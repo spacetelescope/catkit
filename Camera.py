8bfbff7d1fdf395b42b9c9d68bb50bdcd80363f0
Chris Moriarty <moriarty@stsci.edu>
2017-06-01 11:06:46 -0400

    Added some comments.

--- Camera.py
+++ Camera.py
@@ -1,10 +1,13 @@
 from abc import *
 
+"""Abstract base class for all cameras. Implementations of this class also become context managers."""
+
 
 class Camera(object):
     __metaclass__ = ABCMeta
 
     def __init__(self, config_id, *args, **kwargs):
+        """Opens connection with camera sets class attributes for 'config_id' and 'camera'."""
         self.config_id = config_id
         self.camera = self.initialize(self, *args, **kwargs)
 
@@ -15,13 +18,14 @@ class Camera(object):
     def __exit__(self, type, value, traceback):
         self.close()
 
+    # Abstract Methods.
     @abstractmethod
     def initialize(self, *args, **kwargs):
-        """Opens connection with camera and returns camera object."""
+        """Opens connection with camera and returns the camera manufacturer specific object."""
 
     @abstractmethod
     def close(self):
-        """Close camera connection"""
+        """Close camera connection."""
 
     @abstractmethod
     def take_exposures_fits(self, exposure_length, num_exposures, path, filename, *args, **kwargs):
