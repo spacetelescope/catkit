839ceda4671a0dd8078f5aa26f8cb686105f283c
STSCI\moriarty <moriarty@stsci.edu>
2017-06-20 19:03:56 -0400

    Implemented an interface to the Thorlabs flip motor.  Started migration to the hardware folder.  hardware.py is now testbed.py.
    Former-commit-id: 6a12c18859e7a730d076d2ab8bfd4ed68a71d20e

---
+++ FlipMotor.py
@@ -0,0 +1,41 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+from builtins import *
+from abc import *
+
+"""Interface for a two state flip motor."""
+
+
+class FlipMotor(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, config_id, *args, **kwargs):
+        """Opens connection with the DM and sets class attributes for 'config_id' and 'dm'."""
+        self.config_id = config_id
+        self.serial = None
+        self.motor = self.initialize(self, *args, **kwargs)
+
+    # Implementing context manager.
+    def __enter__(self, *args, **kwargs):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    # Abstract Methods.
+    @abstractmethod
+    def initialize(self, *args, **kwargs):
+        """Creates an instance of the controller library and opens a connection."""
+
+    @abstractmethod
+    def close(self):
+        """Close dm connection safely."""
+
+    @abstractmethod
+    def move_to_position1(self):
+        """Implements a move to position 1."""
+
+    @abstractmethod
+    def move_to_position2(self):
+        """Implements a move to position 2."""
