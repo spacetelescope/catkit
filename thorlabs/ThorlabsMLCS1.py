7a617236f7802f13cb1fb3d75700a5160f268a6b
Chris Moriarty <moriarty@stsci.edu>
2017-07-05 14:18:46 -0400

    Implemented a context manager for the Thorlabs Laser source.
    Former-commit-id: a5799bc0488904eb649361a66b4f887e8c4a758f

--- thorlabs/ThorlabsMLCS1.py
+++ thorlabs/ThorlabsMLCS1.py
@@ -1,65 +1,91 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
+
 from builtins import *
-
 from ctypes import *
-
 import os
-path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib","uart_library_win64.dll")
-print(path)
-uart_dll = cdll.LoadLibrary(bytes(path,"utf-8"))
+import re
+
+from ...interfaces.LaserSource import LaserSource
+from ...config import CONFIG_INI
+
+"""Interface for a laser source."""
 
 
-# LISTS ALL COM DEVICES.
-# buffer = b"0"*255
-# list_code = uart_dll.fnUART_LIBRARY_list(buffer, 255)
-# print(buffer)
+class ThorlabsMLCS1(LaserSource):
 
-#if uart_dll.fnUART_LIBRARY_isOpen(b"COM3") != 0:
-handle = uart_dll.fnUART_LIBRARY_open(b"COM3", 115200, 3)
+    def __init__(self, config_id, *args, **kwargs):
+        """
+        Child constructor to add a few hardware specific class attributes. Still calls the super.
+        """
+        super(ThorlabsMLCS1, self).__init__()
+        self.channel = None
+        self.nominal_current = None
+        self.handle = None
 
-# if uart_dll.fnUART_LIBRARY_isOpen(b"COM3") != 1:
-#     uart_dll.fnUART_LIBRARY_close(handle)
-#     handle = uart_dll.fnUART_LIBRARY_open(b"COM3")
+    def initialize(self, *args, **kwargs):
+        self.channel = CONFIG_INI.getint(self.config_id, "channel")
+        self.nominal_current = CONFIG_INI.getint(self.config_id, "current")
+        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib", "uart_library_win64.dll")
+        self.laser = cdll.LoadLibrary(bytes(path, "utf-8"))
+        self.handle = self.laser.fnUART_LIBRARY_open(b"COM3", 115200, 3)
 
+        # Set the initial current to nominal_current and enable the laser.
+        self.set_current(self.channel, self.nominal_current)
+        self.set_channel_enable(self.channel, 1)
+        self.set_system_enable(1)
 
-#ACTIVE CHANNEL.
-# if handle >= 0:
-#     print("Setting channel to 2")
-#     #SetActiveChannel 2
-#     buf32 = b"0"*32
-#     command = "channel=2\r"
-#     pad_size = 32 - len(command)
-#     padding = "0" * pad_size
-#     command = command + padding
-#     uart_dll.fnUART_LIBRARY_Set(handle, command, 32)
-# else:
-#     print("failed to connect")
+        return self.laser
 
-if handle >= 0:
+    def close(self):
+        """Close laser connection safely"""
+        if self.laser.fnUART_LIBRARY_isOpen(b"COM3") != 1:
+            self.set_channel_enable(self.channel, 0)
+            self.set_system_enable(0)
+            self.laser.fnUART_LIBRARY_close(self.handle)
+        self.handle = None
 
-    # Set Active Channel.
-    print("Activate channel 2")
-    active_command_string = b"channel=2\r"
-    uart_dll.fnUART_LIBRARY_Set(handle, active_command_string, 32)
+    def set_current(self, channel, value):
+        """Sets the current on a given channel."""
+        self.set_active_channel(channel)
+        current_command_string = b"current={}\r".format(value)
+        self.laser.fnUART_LIBRARY_Set(self.handle, current_command_string, 32)
 
-    print("Enable channel")
-    enable_command_string = b"enable=1\r"
-    uart_dll.fnUART_LIBRARY_Set(handle, enable_command_string, 32)
+    def get_current(self, channel):
+        """Returns the value of the laser's current."""
+        self.set_active_channel(channel)
+        command = b"current?\r"
+        buffer = b"0" * 255
+        response = self.laser.fnUART_LIBRARY_Get(self.handle, command, buffer)
 
-    print("Setting Current")
-    current_command_string = b"current=80.0\r"
-    uart_dll.fnUART_LIBRARY_Set(handle, current_command_string, 32)
+        # Use regex to find the float value in the response.
+        return float(re.findall("\d+\.\d+", response))
 
-else:
-    print("failed to connect")
+    def find_com_port(self):
+        """Queries the dll for the com port it is using."""
+        buffer = b"0" * 255
+        self.laser.uart_dll.fnUART_LIBRARY_list(buffer, 255)
+        return buffer.split(",")[0]
 
+    def set_channel_enable(self, channel, value):
+        """
+        Set the laser's channel enable.
+        :param channel: Integer value for channel (1 - 4)
+        :param value: Integer value, 1 is enabled, and 0 is disabled.
+        """
+        self.set_active_channel(channel)
+        enable_command_string = b"enable={}\r".format(value)
+        self.laser.fnUART_LIBRARY_Set(self.handle, enable_command_string, 32)
 
+    def set_system_enable(self, value):
+        """
+        Set the laser's system enable.
+        :param value: Integer value, 1 is enabled, and 0 is disabled.
+        """
+        enable_command_string = b"system={}\r".format(value)
+        self.laser.fnUART_LIBRARY_Set(self.handle, enable_command_string, 32)
 
-
-
-if uart_dll.fnUART_LIBRARY_isOpen(b"COM3") != 1:
-    uart_dll.fnUART_LIBRARY_close(handle)
-
-# print("Closed")
-# print(uart_dll.fnUART_LIBRARY_isOpen(buffer))
+    def set_active_channel(self, channel):
+        # Set Active Channel.
+        active_command_string = b"channel={}\r".format(channel)
+        self.laser.fnUART_LIBRARY_Set(self.handle, active_command_string, 32)
