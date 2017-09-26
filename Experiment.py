a785e74b19458f05c2a977b428186c575e5810eb
Chris Moriarty <moriarty@stsci.edu>
2017-09-26 15:38:49 -0400

    Created an abstract class for safety tests, and implement the power and humidity tests.  Also created a primitive launcher script.

--- Experiment.py
+++ Experiment.py
@@ -6,10 +6,9 @@ from builtins import *
 
 from abc import *
 from multiprocessing import Process
-from ..hardware import testbed
-from ..hardware.thorlabs import ThorlabsTSP01
 from .. config import CONFIG_INI
 from .. import util
+from .SafetyTest import UpsSafetyTest, HumidityTemperatureTest, SafetyException
 import time
 
 """Abstract base class that instills safety monitoring into any class that inherits it."""
@@ -19,10 +18,7 @@ class Experiment(object):
     __metaclass__ = ABCMeta
 
     interval = CONFIG_INI.getint("safety", "check_interval")
-    min_humidity = CONFIG_INI.getint("safety", "min_humidity")
-    max_humidity = CONFIG_INI.getint("safety", "max_humidity")
-    min_temp = CONFIG_INI.getint("safety", "min_temp")
-    max_temp = CONFIG_INI.getint("safety", "max_temp")
+    safety_tests = [UpsSafetyTest(), HumidityTemperatureTest()]
 
     @abstractmethod
     def experiment(self):
@@ -34,11 +30,11 @@ class Experiment(object):
         Do not override.
         """
 
-        # Create a SnmpUPS object to monitor the White UPS.
-        ups = testbed.backup_power()
-
-        # Initialize the safety warning to False.  We will allow one safety failure, but two in a row causes a shutdown.
-        safety_warning = False
+        # Check tests before starting experiment.
+        for safety_test in self.safety_tests:
+            if not safety_test.check():
+                print(safety_test.name + " reports unsafe conditions. Aborting experiment...")
+                raise SafetyException()
 
         # Spin off and start the process to run the experiment.
         experiment_process = Process(target=self.run_experiment)
@@ -46,31 +42,26 @@ class Experiment(object):
 
         while experiment_process.is_alive():
 
-            # Check that the UPS not on battery power.
-            power_ok = ups.is_power_ok()
+            for safety_test in self.safety_tests:
 
-            # Check humidity sensor.
-            temp_ok, humidity_ok = self.__check_temp_humidity()
+                if safety_test.check():
+                    # Check passed, clear any warning that might be set and proceed to sleep until next iteration.
+                    safety_test.warning = False
 
-            if power_ok and temp_ok and humidity_ok:
+                elif safety_test.warning:
+                        # Shut down the experiment (but allow context managers to exit properly).
+                        util.soft_kill(experiment_process)
+                        raise SafetyException()
 
-                # Clear the safety_warning flag, everything is ok.
-                safety_warning = False
+                else:
+                    print("Warning issued for " + safety_test.name +
+                          ". Experiment will be softly killed if safety check fails again.")
+                    safety_test.warning=True
 
-                # Sleep until it is time to check safety again.
-                if not self.__smart_sleep(self.interval, experiment_process):
-                    # Experiment ended before the next check interval, exit the while loop.
-                    break
-
-            # A shut down will occur after the safety check fails twice in a row.
-            elif safety_warning:
-
-                # Shut down the experiment (but allow context managers to exit properly).
-                util.soft_kill(experiment_process)
-
-            else:
-                safety_warning = True
-                print("Safety failure detected, experiment will be softly killed if safety check fails again.")
+            # Sleep until it is time to check safety again.
+            if not self.__smart_sleep(self.interval, experiment_process):
+                # Experiment ended before the next check interval, exit the while loop.
+                break
 
     def run_experiment(self):
         """Wrapper for experiment to catch the softkill function's KeyboardInterrupt signal more gracefully."""
@@ -79,16 +70,6 @@ class Experiment(object):
         except KeyboardInterrupt:
             # Silently catch KeyboardInterrupt exception used to kill experiment.
             pass
-
-    def __check_temp_humidity(self):
-        """
-        Helper function that returns booleans for whether the temperature and humidity are ok.
-        :return: Two booleans: temp_ok, humidity_ok.
-        """
-        temp, humidity = ThorlabsTSP01.get_temp_humidity("thorlabs_tsp01_1")
-        temp_ok = self.min_temp <= temp <= self.max_temp
-        humidity_ok = self.min_humidity <= humidity <= self.max_humidity
-        return temp_ok, humidity_ok
 
     @staticmethod
     def __smart_sleep(interval, process):
---
+++ SafetyTest.py
@@ -0,0 +1,52 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+# noinspection PyUnresolvedReferences
+from builtins import *
+from abc import ABCMeta, abstractmethod
+from ..hardware import testbed
+from .. config import CONFIG_INI
+from ..hardware.thorlabs import ThorlabsTSP01
+
+
+class SafetyTest(object):
+    __metaclass__ = ABCMeta
+
+    name = None
+    warning = False
+
+    @abstractmethod
+    def check(self):
+        """Implement to return a boolean. True means everything is ok, false represents unsafe conditions."""
+
+
+class UpsSafetyTest(SafetyTest):
+
+    name = "White UPS Safety Test"
+
+    # Create a SnmpUPS object to monitor the White UPS.
+    ups = testbed.backup_power()
+
+    def check(self):
+        return self.ups.is_power_ok()
+
+
+class HumidityTemperatureTest(SafetyTest):
+
+    name = "Thorlabs Humidity and Temperature Sensor Safety Test"
+
+    min_humidity = CONFIG_INI.getint("safety", "min_humidity")
+    max_humidity = CONFIG_INI.getint("safety", "max_humidity")
+    min_temp = CONFIG_INI.getint("safety", "min_temp")
+    max_temp = CONFIG_INI.getint("safety", "max_temp")
+
+    def check(self):
+        temp, humidity = ThorlabsTSP01.get_temp_humidity("thorlabs_tsp01_1")
+        temp_ok = self.min_temp <= temp <= self.max_temp
+        humidity_ok = self.min_humidity <= humidity <= self.max_humidity
+        return temp_ok and humidity_ok
+
+
+class SafetyException(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
