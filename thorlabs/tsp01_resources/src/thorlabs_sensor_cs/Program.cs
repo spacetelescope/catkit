1fccbff3a1011b098a4acc98e3196030a98febac
Makidon Lab <makidon-lab-FAKE@stsci.edu>
2017-09-29 16:29:30 -0400

    Editted C# throlabs code. Needs to be compiled.

--- thorlabs/tsp01_resources/src/thorlabs_sensor_cs/Program.cs
+++ thorlabs/tsp01_resources/src/thorlabs_sensor_cs/Program.cs
@@ -13,19 +13,35 @@ namespace thorlabs_sensor_cs
         {
             string serial = args[0];
             string usb_address = "USB0::0x1313::0x80F8::" + serial + "::INSTR";
-            TLTSP tsp = new TLTSP(usb_address, true, true);
+            try
+            {
+                TLTSP tsp = new TLTSP(usb_address, true, true);
 
-            // Internal temp is internal to the usb sensor.  External is what we need.
-            Double internal_temp;
-            tsp.measTemperature(1, out internal_temp);
+                // Internal temp is internal to the usb sensor.  External is what we need.
+                Double internal_temp;
+                tsp.measTemperature(1, out internal_temp);
 
-            Double humidity;
-            tsp.fetchHumidity(out humidity);
+                Double humidity;
+                tsp.fetchHumidity(out humidity);
 
-            Double external_temp;
-            tsp.getTemperatureData(2, 0, out external_temp);
-            string str = "temp=" + external_temp.ToString("0.##") + " humidity=" + humidity.ToString("0.##");
-            Console.WriteLine(str);
+                Double external_temp;
+                tsp.getTemperatureData(2, 0, out external_temp);
+                string str = "temp=" + external_temp.ToString("0.##") + " humidity=" + humidity.ToString("0.##");
+                Console.WriteLine(str);
+                tsp.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                Console.Write("Device {0:D}: ", cnt);
+                Console.WriteLine("{0:S} could not be open", resourceString);
+                Console.WriteLine("Connect the TSP01 device(s) and try again");
+                Console.WriteLine();
+                Console.WriteLine("Press any Key to finish");
+                Console.ReadKey();
+                return;
+            }
         }
     }
 }
