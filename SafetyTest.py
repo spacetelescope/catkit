a3eff764498dfe5200d70c5d11973bca401f5296
STSCI\moriarty <moriarty@stsci.edu>
2017-10-02 11:52:24 -0400

    Added a check for the Thorlabs GUI into the humidity safety test.

--- SafetyTest.py
+++ SafetyTest.py
@@ -3,6 +3,8 @@ from __future__ import (absolute_import, division,
 
 # noinspection PyUnresolvedReferences
 from builtins import *
+import psutil
+import time
 from abc import ABCMeta, abstractmethod
 from ..hardware import testbed
 from .. config import CONFIG_INI
@@ -41,6 +43,10 @@ class HumidityTemperatureTest(SafetyTest):
     max_temp = CONFIG_INI.getint("safety", "max_temp")
 
     def check(self):
+        if "TSP01GUI.exe" in (p.name() for p in psutil.process_iter()):
+            print("Close the Thorlabs GUI and run again. It interferes with our code.")
+            return False
+
         temp, humidity = ThorlabsTSP01.get_temp_humidity("thorlabs_tsp01_1")
         temp_ok = self.min_temp <= temp <= self.max_temp
         humidity_ok = self.min_humidity <= humidity <= self.max_humidity
