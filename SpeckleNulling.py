cd2b5c870d1638c295ee4f06b169f6a5c45ccaa5
Makidon Lab <makidon-lab-FAKE@stsci.edu>
2017-10-04 13:18:19 -0400

    Updated experiment launcher.

--- SpeckleNulling.py
+++ SpeckleNulling.py
@@ -19,7 +19,7 @@ class SpeckleNulling(Experiment):
     name = "Speckle Nulling"
 
     def __init__(self,
-                 num_iterations=10,
+                 num_iterations=200,
                  bias=True,
                  flat_map=False,
                  path=None,
