696fab3e3f822543a2370802a989ad3689b2fb47
Makidon Lab <makidon-lab-FAKE@stsci.edu>
2017-11-02 11:19:53 -0400

    Fixed bug with process_distance that wasn't catching the correct exception.  Also, added a check that the find_peaks finds 8 peaks for coron.

--- Calibration.py
+++ Calibration.py
@@ -131,9 +131,9 @@ class Calibration(Experiment):
     def process_distance(self):
         try:
             data = self.data
-        except NameError:
+        except (NameError, AttributeError):
             data = calibration_take_data.take_cal_data(FpmPosition.coron, self.flat_shape, 1,
-                                                       quantity(1, units.millisecond), num_exposures=5)
+                                                       quantity(1, units.millisecond), num_exposures=1)[0]
         centroid, coords = calibration_util.find_center(data, coron=True, return_coords=True)
         dist = calibration_util.find_average_distance_to_center(centroid, coords)
 
