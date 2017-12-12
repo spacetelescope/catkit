61099dd527097450e4c16bd733eb57a64e505f5b
Makidon Lab <makidon-lab-FAKE@stsci.edu>
2017-12-12 12:11:40 -0500

    Made the 4d interface cleaner, with default time-stamp paths, and created a windows permissions setting function.  Untested.

--- FourDTechnology/Accufiz.py
+++ FourDTechnology/Accufiz.py
@@ -4,19 +4,24 @@ from __future__ import (absolute_import, division,
 # noinspection PyUnresolvedReferences
 from builtins import *
 
+import h5py
 import time
 import requests
 import os
+import math
+import numpy as np
+from astropy.io import fits
+from scipy import ndimage
+from glob import glob
 
 from ...interfaces.FizeauInterferometer import FizeauInterferometer
-
 from ...config import CONFIG_INI
-from glob import glob
+from ... import util
 
 
 class Accufiz(FizeauInterferometer):
 
-    def initialize(self, *args, **kwargs):
+    def initialize(self, mask="dm2_detector.mask", *args, **kwargs):
         """Opens connection with interferometer and returns the camera manufacturer specific object."""
         orig = time.time()
         ip = CONFIG_INI.get(self.config_id, "ip")
@@ -27,7 +32,7 @@ class Accufiz(FizeauInterferometer):
         requests.get(set_timeout_string)
 
         # Set the Mask.
-        filemask = self.__get_mask_path()
+        filemask = self.__get_mask_path(mask)
         typeofmask = "Detector"
         parammask = {"maskType": typeofmask, "fileName": filemask}
         set_mask_string = "http://{}/WebService4D/WebService4D.asmx/SetMask".format(ip)
@@ -36,8 +41,17 @@ class Accufiz(FizeauInterferometer):
     def close(self):
         """Close interferometer connection?"""
 
-    def take_measurement(self, num_frames, path, filename, *args, **kwargs):
-        orig = time.time()
+    def take_measurement(self,
+                         num_frames=2,
+                         path=None,
+                         filename=None):
+
+        if path is None:
+            central_store_path = CONFIG_INI.get("optics_lab", "data_path")
+            path = util.create_data_path(initial_path=central_store_path, suffix="4d", permissions=True)
+
+        if filename is None:
+            filename = "4d_measurement"
 
         """Takes exposures and should be able to save fits and simply return the image data."""
         ip = CONFIG_INI.get(self.config_id, "ip")
@@ -45,29 +59,53 @@ class Accufiz(FizeauInterferometer):
 
         measres = requests.post('http://{}/WebService4D/WebService4D.asmx/AverageMeasure'.format(ip), data=parammeas)
 
-        pathfile = os.path.join(path, filename)  # pathdir + inputfile
-
-        pathfile = pathfile.replace('\\',
-                                    '\\\\')  ### This line is here because when sent through webservice slashes tend to disappear. If we sent in parameter a path with only one slash, they disappear
+        pathfile = os.path.join(path, filename)
 
         #  This line is here because when sent through webservice slashes tend
         #  to disappear. If we sent in parameter a path with only one slash,
         #  they disappear
+        pathfile = pathfile.replace('\\',
+                                    '/')
+
+        # pathfile = pathfile.replace('/',
+        #                             '\\\\')
 
         paramsave = {"fileName": pathfile}
 
         if 'success' in measres.text:
-            r = requests.post("http://192.168.192.131/WebService4D/WebService4D.asmx/SaveMeasurement", data=paramsave)
+            r = requests.post("http://{}/WebService4D/WebService4D.asmx/SaveMeasurement".format(ip), data=paramsave)
             if glob(pathfile + '.h5'):
-                #print('SUCCESS IN SAVING ' + pathfile + '.h5 : ' + num_frames + 'frames in ' + str(time.time() - orig))
                 print('SUCCESS IN SAVING ' + pathfile)
+                return self.__convert_h5_to_fits(path)
             else:
                 print("FAIL IN SAVING MEASUREMENT " + pathfile + ".h5")
         else:
             print("FAIL IN MEASUREMENT " + pathfile + ".h5")
 
     @staticmethod
-    def __get_mask_path():
+    def __get_mask_path(mask):
         script_dir = os.path.dirname(__file__)
-        return os.path.join(script_dir, "dm2_detector.mask")
+        return os.path.join(script_dir, mask)
+
+    @staticmethod
+    def __convert_h5_to_fits(path):
+        os.chdir(path)
+
+        for file in glob('*.h5'):
+            pathfile = file
+            pathdifits = file[:-3] + '.fits'
+
+            maskinh5 = np.array(h5py.File(pathfile, 'r').get('measurement0').get('Detectormask'))
+            image0 = np.array(h5py.File(pathfile, 'r').get('measurement0').get('genraw').get('data')) * maskinh5
+
+            fits.PrimaryHDU(maskinh5).writeto(pathdifits, overwrite=True)
+
+            radiusmask = np.int(np.sqrt(np.sum(maskinh5) / math.pi))
+            center = ndimage.measurements.center_of_mass(maskinh5)
+
+            image = np.clip(image0, -10, +10)[np.int(center[0]) - radiusmask:np.int(center[0]) + radiusmask - 1,
+                    np.int(center[1]) - radiusmask: np.int(center[1]) + radiusmask - 1]
+
+            fits.PrimaryHDU(image).writeto(pathdifits, overwrite=True)
+            return glob('*.fits')
 
