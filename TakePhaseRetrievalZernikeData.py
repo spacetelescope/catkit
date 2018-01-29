f613c750c92dc48ac2c2162ae53dc3241ba19c9a
Makidon Lab <makidon-lab-FAKE@stsci.edu>
2018-01-29 17:08:53 -0500

    Phase retrieval launcher updated with new zernike experiment.

--- TakePhaseRetrievalZernikeData.py
+++ TakePhaseRetrievalZernikeData.py
@@ -14,7 +14,7 @@ from .. import util
 from ..hardware.boston import DmCommand
 
 
-class TakePhaseRetrievalData(Experiment):
+class TakePhaseRetrievalZernikeData(Experiment):
     name = "Take Phase Retrieval Zernike Data"
 
     def __init__(self,
@@ -46,18 +46,7 @@ class TakePhaseRetrievalData(Experiment):
 
     def experiment(self):
         if self.path is None:
-            self.path = util.create_data_path(suffix="phase_retrieval_zernike")
-
-        # DM1 Off, DM2 Flat
-        take_phase_retrieval_data(self.exposure_time,
-                                  self.num_exposures,
-                                  self.step,
-                                  os.path.join(self.path, "dm1_off_dm2_flat"),
-                                  self.camera_type,
-                                  position_list=self.position_list,
-                                  dm1_command=flat_command(bias=False, flat_map=False, dm_num=1),
-                                  dm2_command=flat_command(bias=False, flat_map=True, dm_num=2),
-                                  **self.kwargs)
+            self.path = util.create_data_path(suffix="phase_retrieval_zernikes")
 
         # All pure zernikes at 4 different amplitudes.
         path_list = ["z:/Testbeds/hicat_dev/data_vault/dm2_calibration/2018-01-21T15-13-01_4d_zernike_loop_spherical/",
@@ -70,15 +59,15 @@ class TakePhaseRetrievalData(Experiment):
 
         p2v_strings = ["20_nm_p2v", "40_nm_p2v", "80_nm_p2v", "160_nm_p2v"]
         for zernike_path in path_list:
-            zernike_name = os.path.basename(zernike_path).split("_")[-1]
+            zernike_name = zernike_path.split("_")[-1]
 
             for p2v_string in p2v_strings:
-                command_path = glob(zernike_path + "/p2v_string/*.fits")[0]
+                command_path = glob(os.path.join(zernike_path, p2v_string,"*.fits"))[0]
                 dm2_command = DmCommand.load_dm_command(command_path, as_volts=True, dm_num=2)
                 take_phase_retrieval_data(self.exposure_time,
                                           self.num_exposures,
                                           self.step,
-                                          os.path.join(self.path, zernike_name + "_" + p2v_string),
+                                          os.path.join(self.path, zernike_name, p2v_string),
                                           self.camera_type,
                                           position_list=self.position_list,
                                           dm1_command=flat_command(bias=False, flat_map=True, dm_num=1),
