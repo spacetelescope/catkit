a422a26571182eae5a44aa56afbad54fa0e27747
Makidon Lab <makidon-lab-FAKE@stsci.edu>
2018-01-31 13:32:09 -0500

    MTF experiment now applies flat to both DMs.  Lyot sampling value updated in ini.

--- TakeMtfData.py
+++ TakeMtfData.py
@@ -39,9 +39,15 @@ class TakeMtfData(Experiment):
             util.setup_hicat_logging(self.path, "mtf_calibration")
 
         # Create a flat dm command.
-        flat_command_object, flat_file_name = flat_command(flat_map=self.flat_map,
+        flat_command_object1, flat_file_name = flat_command(flat_map=self.flat_map,
                                                            bias=self.bias,
-                                                           return_shortname=True)
+                                                           return_shortname=True,
+                                                           dm_num=1)
+
+        flat_command_object2, flat_file_name = flat_command(flat_map=self.flat_map,
+                                                           bias=self.bias,
+                                                           return_shortname=True,
+                                                           dm_num=2)
         direct_exp_time = self.exposure_time
         num_exposures = self.num_exposures
 
@@ -51,7 +57,7 @@ class TakeMtfData(Experiment):
 
             with testbed.dm_controller() as dm:
                 # Flat.
-                dm.apply_shape(flat_command_object, 1)
+                dm.apply_shape_to_both(flat_command_object1, flat_command_object2)
                 cal_file_path = testbed.run_hicat_imaging(direct_exp_time, num_exposures, FpmPosition.direct,
                                                           path=self.path, exposure_set_name="direct",
                                                           filename=flat_file_name, camera_type=self.camera_type)
