b61e34c96aa5f018b4cc6f9b190ba98631efdba1
Keira Brooks <kbrooks@stsci.edu>
2018-03-26 12:42:10 -0400

    Updated TakCheckerboardData.py to include reference image

--- TakeCheckerboardData.py
+++ TakeCheckerboardData.py
@@ -16,20 +16,22 @@ from ..hardware import testbed
 from ..hardware.FourDTechnology.Accufiz import Accufiz
 from ..config import CONFIG_INI
 from .. import util
-from ..hicat_types import units, quantity
+from ..hicat_types import units, quantity, FpmPosition
 
 
-class TakeDm4dCheckerboardData(Experiment):
-    name = "Take Dm 4d Checkerboard Data"
+class TakeCheckerboardData(Experiment):
+    name = "Take Checkerboard Data"
 
     def __init__(self,
                  amplitude=quantity(800, units.nanometer),
+                 exposure_time=quantity(250, units.microsecond),
                  num_frames=2,
                  path=None,
                  camera_type="imaging_camera",
                  **kwargs):
 
         self.amplitude = amplitude
+        self.exp_time = exposure_time
         self.num_frames = num_frames
         self.path = path
         self.camera_type = camera_type
@@ -41,42 +43,55 @@ class TakeDm4dCheckerboardData(Experiment):
             central_store_path = CONFIG_INI.get("optics_lab", "data_path")
             self.path = util.create_data_path(initial_path=central_store_path,
                                               suffix="checkerboard_" + self.camera_type)
+        dm_num = 1
+        fpm_position = FpmPosition.direct
 
         with testbed.dm_controller() as dm:
 
             # Reference flat image.
-
+            flat_dm_command = flat_command(bias=False, flat_map=True)
+            dm.apply_shape_to_both(flat_dm_command, flat_dm_command)
+            reference_path = testbed.run_hicat_imaging(self.exp_time, num_exposures=1,
+                                                       fpm_position=fpm_position,
+                                                       path=self.path,
+                                                       filename="reference",
+                                                       camera_type=self.camera_type,
+                                                       pipeline=True,
+                                                       **self.kwargs)
 
             # Generate the 16 permutations of checkerboards, and add the commands to a list.
             for i in range(0, 4):
                 for j in range(0, 4):
                     file_name = "checkerboard_{}_{}_{}nm".format(i, j, self.amplitude)
-                    command = checkerboard_command(dm_num=1, offset_x=i, offset_y=j,
+                    command = checkerboard_command(dm_num=dm_num, offset_x=i, offset_y=j,
                                                    amplitude=self.amplitude,
                                                    bias=False, flat_map=True)
-                    dm.apply_shape(command, 1)
-                    with Accufiz("4d_accufiz", mask=mask) as four_d:
-                        image_path = four_d.take_measurement(path=os.path.join(self.path, file_name),
-                                                             filename=file_name,
-                                                             rotate=self.rotate,
-                                                             fliplr=self.fliplr)
+                    dm.apply_shape(command, dm_num)
+                    image_path = testbed.run_hicat_imaging(self.exp_time,
+                                                           self.num_frames,
+                                                           fpm_position,
+                                                           path=self.path,
+                                                           filename=file_name,
+                                                           camera_type=self.camera_type,
+                                                           pipeline=True,
+                                                           **self.kwargs)
 
-                        # Open fits files and subtract.
-                        reference = fits.getdata(reference_path)
-                        image = fits.getdata(image_path)
+                    # Create metadata.
+                    metadata = [MetaDataEntry("offset_x", "offset_x", i, "Checkerboard offset x-axis")]
+                    metadata.append(MetaDataEntry("offset_y", "offset_y", j, "Checkerboard offset y-axis"))
+                    metadata.append(MetaDataEntry("amplitude",
+                                                  "amp",
+                                                  self.amplitude.to(units.nanometer).m,
+                                                  "Amplitude in nanometers"))
 
-                        # Create metadata.
-                        metadata = [MetaDataEntry("offset_x", "offset_x", i, "Checkerboard offset x-axis")]
-                        metadata.append(MetaDataEntry("offset_y", "offset_y", j, "Checkerboard offset y-axis"))
-                        metadata.append(MetaDataEntry("amplitude",
-                                                      "amp",
-                                                      self.amplitude.to(units.nanometer).m,
-                                                      "Amplitude in nanometers"))
+                    reference = fits.getdata(reference_path)
+                    image = fits.getdata(image_path)
+                    # Subtract the reference from image.
+                    util.write_fits(reference - image,
+                                    os.path.join(self.path, file_name + "_subtracted"),
+                                    metadata=metadata)
 
+                    # Save the DM_Command used.
+                    command.export_fits(os.path.join(self.path, file_name))
 
-        files_path = glob(os.path.join(self.path, file_name.split("_")[0] + "*_subtracted.fits"))
-        dm_calibration_util.create_actuator_index(self.dm_num, path=self.path,
-                                                  files=files_path,
-                                                  reffiles=reference_path,
-                                                  show_plot=self.show_plot,
-                                                  overwrite_csv=self.overwrite_csv)
+            return self.path
