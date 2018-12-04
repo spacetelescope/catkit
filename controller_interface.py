4f8d7116f49569092b449a49b01aed87574e806f
Jules Fowler <jfowler@stsci.edu>
2018-12-04 14:07:23 -0500

    Adding fleshed out controller interface and gitignore.

---
+++ controller_interface.py
@@ -0,0 +1,195 @@
+## -- IMPORTS
+
+import struct
+import time
+
+import usb.core
+import usb.util
+
+
+## -- FUNCTIONS and FIDDLING
+
+def build_message(cmd_func, cmd_type, chan, value=None):
+    """ Function to build read/write message.
+
+    Parameters
+    ----------
+    cmd_func : str
+        "loop", "p/i/d_gain" for what kind of adjustment.
+    cmd_type : str
+        "read" or "write" for whether we're writing a command or 
+        reading back the value.
+    chan : int
+        The channel we're working on (1, 2, or 3).
+    value : int/float, optional
+        If writing a command should be int (for the loop) or 
+        a float for the P/I/D gain. Otherwise None.
+
+    Returns
+    -------
+    msg : bytes
+        The message out.
+    """
+
+
+    # Initialize command dictionary
+    cmd_dict = {'loop': 84, 'p_gain': 720, 'i_gain': 728, 'd_gain': 730}
+
+    # Figure out address 
+    addr = 11830000 + 1000*chan + cmd_dict[cmd_func]
+    addr = '0x{}'.format(addr)
+    addr = struct.pack('<Q', int(addr, 16))
+
+    # Now build up the command
+    if cmd_type == "read":
+        if value != None:
+            print('You specified a value but nothing will happen to it.')
+        
+        # [cmd type] [address] [number of reads] [end card]
+        msg = b'\xa4' + addr[:4] + b'\x02\x00\x00\x00\x55'
+    
+    if cmd_type == "write":
+        assert value != None, "Value is required."
+        
+        if cmd_func == "loop":
+            assert value in [1, 0], "1 or 0 value is required for loop."
+
+            # Convert to hex
+            val = struct.pack('<I', value)
+
+        if cmd_func in ["p_gain", "i_gain", "d_gain"]:
+            
+            val = struct.pack('<d', float(value))
+
+        msg = []
+        msg.append(b'\xa2' + addr[:4] + val[:4] + b'\x55')
+        msg.append(b'\xa3' + val[4:] + b'\x55')
+    
+    return msg
+
+
+
+class Controller:
+
+    
+    def __init__(self):
+        self.dev = usb.core.find()
+        #assert self.dev != None, "Go get the device sorted you knucklehead."
+        #self.dev.set_configuration()
+        self.history = {}
+
+
+    def check_config(self):
+        for cfg in self.dev:
+            print(cfg)
+    
+    
+    def build_message(self, cmd_key, cmd_type, chan, value=None):
+        
+        cmd_dict = {'loop': 84, 'p_gain': 720, 'i_gain': 728, 'd_gain': 730}
+
+        addr = 11830000 + 1000*chan + cmd_dict[cmd_key]
+        addr = '0x{}'.format(addr)
+        addr = struct.pack('<Q', int(addr, 16))
+
+        # Now build message or messages []
+
+        if cmd_type == 'read':
+            if value != None:
+                print('You specified a value but nothing will happen to it.')
+            
+            message.append(b'\xa4' + addr[:4] + b'\x02\x00\x00\x00\x55')
+
+        if cmd_type == 'write':
+            assert value != None, "Value is required."
+
+            if cmd_key == 'loop':
+                assert value in [1,0], "1 or 0 value is required for loop."
+
+                # Convert to hex
+                val = struct.pack('<I', value)
+                message.append(b'\xa2' + addr[:4] + val + '\x55')
+
+            if cmd_key = ['p_gain', 'i_gain', 'd_gain']:
+                
+                
+                # Convert to hex double (64 bit)
+                val = struct.pack('<d', float(value))
+
+                message.append(b'\xa4' + addr[:4] + val[:4] + '\x55')
+                message.append(b'\xa3' + val[4:] + '\x55')
+
+        
+        self.history[str(time.time())] = message
+        
+        return message
+
+    
+    def send_message(self, msg):
+        
+        self.dev.write(0x02, msg, 100)
+        
+
+    def read_response(self, timer=False):
+        
+        resp = ''
+        tries = 0
+        
+        
+        start = time.time()
+        while len(resp) < 4 and tries < 10:
+            resp = self.dev.read(0x81, 100, 2000)
+        
+        time_elapsed = time.time() - start
+
+        addr = rep[3:7]
+        address = struct.unpack('<I', addr)
+        val = resp[7:-1]
+        if len(val) == 4:
+            value = struct.unpack('<I', val)
+        elif len(val) == 8:
+            value = struct.unpack('<d', val)
+        else:
+            print("Something is wrong.")
+            value = None
+        
+        if timer:
+            return value, time_elapsed, tries
+        else:
+            return value
+
+
+    def check_status(self, chan):
+        
+        read_msg = {'p_gain': build_message('p_gain', 'read', chan),
+                    'i_gain': build_message('i_gain', 'read', chan),
+                    'd_gain': build_message('d_gain', 'read', chan),
+                    'loop': build_message('loop', 'read', chan)}
+        
+        value_dict = {}
+        print("For channel {}.".format(chan))
+        for key in read_msg:
+            self.send_message(read_msg[key])
+            value = self.read_response()
+            print("For parameter : {} the value is {}".format(key, value)
+            value_dict[key] = value
+
+
+        return value_dict 
+
+
+## -- MAIN with ex
+
+if __name__ == "__main__":
+    dev = usb.core.find()
+    assert dev != None, "Turn on the device you knucklehead."
+    
+    read_msg = build_message('loop', 'read', 1)
+    cmd_msg = build_message('loop', 'write', 1, 1)
+
+    dev.write(0x02, read_msg, 100)
+
+
+    # This usually takes three tries actually?
+    dev.read(0x81, 100, 1000)
+
