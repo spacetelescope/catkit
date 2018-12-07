4e48ae3392439966ec069952b396e274cdf3e92e
Jules Fowler <jfowler@stsci.edu>
2018-12-07 15:07:45 -0500

    Beefing up my error handling and adding in some logging.

--- controller_interface.py
+++ controller_interface.py
@@ -1,15 +1,41 @@
 ## -- IMPORTS
 
+import functools
 import logging
 import struct
 import time
-import warning
+import warnings
 
 import usb.core
+from usb.core import USBError
 import usb.util
 
 
 ## -- FUNCTIONS and FIDDLING
+
+# Decorator with error handling
+def usb_except(function):
+    """Decorator that catches PyUSB errors."""
+
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except USBError as e:
+            args[0].logger.error("There's a timeout or a busy resource.")
+            args[0].logger.error("We'll try to reinstantiate the object.")
+            try:
+                usb.util.dispse_resource(args[0].dev)
+                args[0].dev.clear_halt()
+                args[0].dev.reset()
+                args[0].__init__()
+                args[0].logger.error("We re-initialized the device and it worked!")
+
+            except USBError as e:
+                args[0].logger.error("Alas we are powerless for this reason : {}".format(e))
+                args[0].logger.error("You need to restart the physical controller and start over.")
+        
+    return wrapper
 
 
 class Controller:
@@ -25,27 +51,53 @@ class Controller:
 
     """
 
-    
+    @usb_except
     def __init__(self):
 
-        """ Initial function to find device, set the config, make sure 
+        """ Initial function to configure logging, find device, 
+        set the config, make sure 
         it exists, and start a record of the command history."""
+        
 
+        # Set up logging
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+        fh = logging.FileHandler('controller_interface_log.txt')
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+        self.logger.info('Logging starting up...')
+        
+        # Instantiate the device
         self.dev = usb.core.find()
         if self.dev == None:
-            raise Error("Go get the device sorted you knucklehead.")
+            logger.error('There was no device.')
+            raise NameError("Go get the device sorted you knucklehead.")
+        self.logger.info('Controller instantiated.')
         self.dev.set_configuration()
         self.history = {}
-
+    
+    @usb_except
     def check_config(self):
         """Checks the feasible configurations for the device."""
         
         for cfg in self.dev:
-            print(cfg)
+            self.logger.info('Device config : ')
+            self.logger.info(cfg)
         
+    @usb_except
     def build_message(self, cmd_key, cmd_type, chan, value=None):
         """Builds the message to send to the controller. The messages
-        must be 10 or 6 bytes, in significance increasing order -- which 
+        must be 10 or 6 bytes, in significance increasing order (little 
+        endian) -- which 
         for some godawful reason means backwards but sorted into bytes. 
 
         The format of the message to write to a new location is :
@@ -94,29 +146,35 @@ class Controller:
             message.append(b'\xa4' + addr[:4] + b'\x02\x00\x00\x00\x55')
 
         if cmd_type == 'write':
-            assert value != None, "Value is required."
+            if value == None:
+                self.logger.error('There was no value set for this command.')
+                raise NameError("Value is required.")
 
             if cmd_key == 'loop':
-                assert value in [1,0], "1 or 0 value is required for loop."
+                if value not in [1,0]:
+                    self.logger.error('Loop requires a 1/0 value.')
+                    raise ValueError("1 or 0 value is required for loop.")
 
                 # Convert to hex
                 val = struct.pack('<I', value)
                 message.append(b'\xa2' + addr[:4] + val + b'\x55')
 
             if cmd_key in ['p_gain', 'i_gain', 'd_gain']:
-                
+                if type(value) not in [int, float]:
+                    self.logger.error('Gain requires float/int value.')
+                    raise TypeError("Int or float value is required for gain.")
                 
                 # Convert to hex double (64 bit)
                 val = struct.pack('<d', float(value))
 
                 message.append(b'\xa2' + addr[:4] + val[:4] + b'\x55')
                 message.append(b'\xa3' + val[4:] + b'\x55')
-
         
         self.history[str(time.time())] = message
         
         return message
 
+    @usb_except
     def send_message(self, msg):
         """Send the message to the controller.
 
@@ -153,7 +211,6 @@ class Controller:
         resp = ''
         tries = 0
         
-        
         start = time.time()
         while len(resp) < 4 and tries < 10:
             resp = self.dev.read(0x81, 100, 1000) 
@@ -168,11 +225,16 @@ class Controller:
         else:
             value = struct.unpack('<d', val)
         
-        if timer:
+        # Value will be a 1D tuple and we want the value
+        value = value[0]
+
+
+        if return_timer:
             return value, time_elapsed, tries
         else:
             return value
-
+    
+    @usb_except
     def check_status(self, chan):
         """Checks the status of the loop, and p/i/d_gain for 
         the specified channel.
@@ -194,15 +256,16 @@ class Controller:
                     'loop': self.build_message('loop', 'read', chan)}
         
         value_dict = {}
-        print("For channel {}.".format(chan))
+        self.logger.info("For channel {}.".format(chan))
         for key in read_msg:
             self.send_message(read_msg[key])
             value = self.read_response(key=='loop')
-            print("For parameter : {} the value is {}".format(key, value))
+            self.logger.info("For parameter : {} the value is {}".format(key, value))
             value_dict[key] = value
 
         return value_dict 
 
+    @usb_except
     def command(self, cmd_key, chan, value):
         """Function to send a command to the controller and read back 
         to make sure it matches. 
@@ -222,11 +285,15 @@ class Controller:
 
         self.send_message(write_message)
         self.send_message(read_message)
-        set_value, time_elapsed, tries = self.read_response(cmd_key=='loop', timer=True)
+        set_value, time_elapsed, tries = self.read_response(cmd_key=='loop',return_timer=True)
         
-        print('It took {} seconds and {} tries for the message to return.'.format(time_elapsed, tries))
-        print(set_value, value)
-        #assert set_value == value, "The value set does not match the command sent."
+        self.logger.info('It took {} seconds and {} tries for the message to return.'.format(time_elapsed, tries))
+        if value == set_value:
+            self.logger.info('Command successful: {} == {}.'.format(value, set_value))
+        else:
+            self.logger.info('Command NOT successful : {} != {}.'.format(value, set_value))
+        
+
 
 ## -- MAIN with ex
 if __name__ == "__main__":
