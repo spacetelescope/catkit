2efe35a917965d53b82525ded573f6779318e1f9
Jules Fowler <jfowler@stsci.edu>
2019-02-22 10:38:29 -0500

    VERY rough draft of ripping out the hicat safeteys.

---
+++ simplEST_take_exposure.py
@@ -0,0 +1,22 @@
+
+import matplotlib.pyplot as plt
+import pint.UnitRegistry.Quantitity as quantity
+import zwoasi
+
+# Pick out the right camera
+camera_name = 'ZWhatever...'
+cameras_found = zwoasi.list_cameras()
+camera_index = cameras_found.index(camera_name)
+
+camera = zwoasi.Camera(camera_index)
+
+# Set the image specifics (bare minimum)
+poll = quantity(0.1, units.second)
+exposure_time = quantity(1000, units.microsecond)
+
+# Take the image
+image = camera.capture(initial_sleep=exposure_time.to(units.second).magnitude, poll=poll.magnitude)
+plt.imshow(image)
+plt.savefig('test_img.png')
+
+
