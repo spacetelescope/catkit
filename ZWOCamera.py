da381b9400d6ae96ba612dbac9ad5ecd5594c835
Jules Fowler <jfowler@stsci.edu>
2019-02-27 13:13:51 -0500

    Moved the logging.

--- ZWOCamera.py
+++ ZWOCamera.py
@@ -1,19 +1,19 @@
+## -- IMPORTS 
 
 import matplotlib.pyplot as plt
 import pint
 
 import zwoasi
 
-## -- CLASS DEF
+## -- CLASS DEFINITION
 
 class ZWOCamera:
     """Class for the ZWOCamera. """
     
     def __init__(self):
-        """ Init funciton to set up logging and instantiate the camera."""
+        """ Init function to set up logging and instantiate the camera."""
 
-        # 3 logging 5 me
-        
+        # Camera set up
         try:
             if zwoasi.get_num_cameras() == 0:
                 cam_lib_file = 'C:/Users/RMOLStation1s/piezo_tiptilt/hicat-package/hicat/hardware/zwo/lib/windows/ASICamera2.dll'
@@ -21,6 +21,20 @@ class ZWOCamera:
             self.camera = zwoasi.Camera(0)
         except  Exception:
                 raise
+
+        # Logging
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        log_file = os.path.join('.', 'controller_interface_log_{}.txt'.format(
+                                str(datetime.datetime.now()).replace(' ', '_').replace(':', '_')))
+        fh = logging.FileHandler(filename=log_file)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+
+        ch = logging.StreamHandler()
 
 
     def __enter__(self):
@@ -48,7 +62,6 @@ class ZWOCamera:
         units = pint.UnitRegistry()
         quantity = units.Quantity
  
-        
         # Set up exposure and poll time with units.
         exposure_time = quantity(exp_time, units.microseconds)
         # This one helps to avoid the camera from crashing or something?
@@ -60,11 +73,16 @@ class ZWOCamera:
                     poll=poll.magnitude)
  
         # Save it
-        plt.imshow(image)
+        v_min = np.median(image) - 3*np.std(image)
+        v_max = np.median(image) + 3*np.stdimage)
+        plt.imshow(image, vmin=v_min, vmax=v_max, cmap='grey')
+        plt.colorbar()
         plt.savefig(output_name)
+        plt.clf()
         print('Image saved to {}.'.format(output_name))
 
     def close_camera(self):
         """Closes the camera if you didn't use context managers."""
         self.camera.close()
+
 
