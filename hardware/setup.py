89d5e77a6ef87589b77ccd1193fdf735c6d1492b
Jules Fowler <jfowler@stsci.edu>
2019-05-22 16:03:48 -0400

    Updated setup for hardware.

--- hardware/setup.py
+++ hardware/setup.py
@@ -1,1 +1,13 @@
-### I AM AN EMPTY SHELL OF A PACKAGE
+import os
+from setuptools import setup
+
+setup(name='hardware',
+        version=0.1, 
+        description='Ripping out hicat-pacakge hardware.',
+        url='https://github.com/spacetelescope/instrument-interface-library/...', 
+        author='Optics Lab Team',
+        license='MIT',
+        packages=['hardware'],
+        zip_safe=False)
+
+
