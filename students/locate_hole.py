8e0d6b2132d893c8551a15370f27274756baf0bd
Jules Fowler <jfowler@stsci.edu>
2019-06-27 14:34:09 -0400

    Some file wrangling.

--- students/locate_hole.py
+++ students/locate_hole.py
@@ -1,26 +1,21 @@
 ###IMPORTS
-#Config imports
-from __future__ import (absolute_import, division, unicode_literals)
-from builtins import *
 import configparser
 import os
+import time
 
-#Camera imports
-import ZWOCamera
 
-#Centroid imports
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
 from photutils import centroid_1dg
 from photutils import centroid_2dg
-import time
 
-
+from interfaces import zwo_camera
 
 config_file_name = "config.ini"
 override_file_name = "config_local.ini"
 
+# unused
 def get_config_ini_path():
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), config_file_name)
 
@@ -40,42 +35,49 @@ def load_config_ini():
         config.read(os.path.join(code_directory, config_file_name))
     return config
 
-### SET Constants
-CONFIG_INI = load_config_ini()
+def locate_hole():
+    """ Locate the hole for the FPM. """
+
+    CONFIG_INI = load_config_ini()
 	
 
-#Locate Holes
-zwo_cam_tac = ZWOCamera.ZWOCamera()
-zwo_cam_tac.open_camera("ZWO ASI290MM(1)")
-zwo_cam = ZWOCamera.ZWOCamera()
-zwo_cam.open_camera("ZWO ASI290MM(2)")
+    #Locate Holes
+    zwocam_tac = zwo_camera.ZWOCamera()
+    zwocam_tac.open_camera("ZWO ASI290MM(1)")
+    zwocam = zwo_camera.ZWOCamera()
+    zwocam.open_camera("ZWO ASI290MM(2)")
 
-print("\nShine a light into the back of the FPM.\n")
-input("Press enter when ready to take an image.\n")
-image = zwo_cam.take_exposure(exp_time=30000)
-zwo_cam.plot_image(image, output_name='sci_cam_hole.png')
-sci_cam_x, sci_cam_y = centroid_1dg(image)
-print(sci_cam_x)
-print(sci_cam_y)
-CONFIG_INI.set('Camera', 'sci_cam_x', str(sci_cam_x))
-CONFIG_INI.set('Camera', 'sci_cam_y', str(sci_cam_y))
-
-
-print("\nShine a light into the front of the FPM.\n")
-input("Press enter when ready to take an image.\n")
-image2 = zwo_cam_tac.take_exposure(exp_time=10000)
-zwo_cam_tac.plot_image(image2, output_name='ta_cam_hole.png')
-ta_cam_x, ta_cam_y = centroid_2dg(image2)
-print(ta_cam_x)
-print(ta_cam_y)
-CONFIG_INI.set('Camera', 'ta_cam_x',str(ta_cam_x))
-CONFIG_INI.set('Camera', 'ta_cam_y', str(ta_cam_y))
+    print("\nShine a light into the back of the FPM.\n")
+    input("Press enter when ready to take an image.\n")
+    image = zwocam.take_exposure(exp_time=30000)
+    zwocam.plot_image(image, output_name='sci_cam_hole.png')
+    sci_cam_x, sci_cam_y = centroid_1dg(image)
+    print(sci_cam_x)
+    print(sci_cam_y)
+    CONFIG_INI.set('Camera', 'sci_cam_x', str(sci_cam_x))
+    CONFIG_INI.set('Camera', 'sci_cam_y', str(sci_cam_y))
 
 
-with open('config.ini','w') as configfile:
+    print("\nShine a light into the front of the FPM.\n")
+    input("Press enter when ready to take an image.\n")
+    image2 = zwocam_tac.take_exposure(exp_time=10000)
+    zwocam_tac.plot_image(image2, output_name='ta_cam_hole.png')
+    ta_cam_x, ta_cam_y = centroid_2dg(image2)
+    print(ta_cam_x)
+    print(ta_cam_y)
+    CONFIG_INI.set('Camera', 'ta_cam_x',str(ta_cam_x))
+    CONFIG_INI.set('Camera', 'ta_cam_y', str(ta_cam_y))
+
+
+    with open('config.ini','w') as configfile:
 	CONFIG_INI.write(configfile)
 	configfile.close()
 
-zwo_cam_tac.close()
-zwo_cam.close()
-print("Hole locations saved to config file.\n")
+    zwocam_tac.close()
+    zwocam.close()
+    print("Hole locations saved to config file.\n")
+
+## -- RUN
+if __name__ == "__main__":
+    locate_hole()
+
