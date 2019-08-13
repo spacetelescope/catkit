59b417a94fa755766ca4958ae71c07b4a8b4d941
Jules Fowler <jfowler@stsci.edu>
2019-08-13 10:36:34 -0400

    Looking to a time when this code is more collaborative.

--- setup.py
+++ setup.py
@@ -3,21 +3,8 @@ from setuptools import setup
 
 setup(name='interfaces',
       version='0.1',
-      description='Pure Python hardware interfaces for the Optics Lab.',
+      description='Python API wrapping hardware and device drivers for the Optics Lab.',
       url='https://github.com/spacetelescope/instrument-interface-library',
-      author='Jules Fowler',
-      license='MIT',
-      packages=['interfaces'],
-      zip_safe=False)
+      author='Makidon Optics Lab',
+      packages=['interfaces'])
 
-directories = ['logs', 'images']
-for directory in directories:
-    if os.path.exists(directory) == False:
-        os.makedirs(directory)
-    else:
-        print('The {} directory is already in place.'.format(directory))
-
-import interfaces
-cmd = 'export INTERFACES="{}"'.format(interfaces.__path__[0].replace('interfaces', ''))
-print("Add this line to your .bashrc :")
-print(cmd)
