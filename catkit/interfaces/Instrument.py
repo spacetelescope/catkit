7cadb6fce0934e662150c16c6f5c43707b8f5ce7
James Noss <jnoss@stsci.edu>
2019-09-24 10:44:17 -0400

    Improvements to Instrument interface (implemented by BostonDmController)
    Resolves HICAT-585
    
    - [x] Add __del__() to close connection.
    - [x] Improve close behavior.
    - [x] Only connect within ``__enter()__`` to force use of ``with``.
    - [x] Add ref to connection library as instrument member.
    
    Implemented by the DM classes as an initial starting place and example.
    
    All other hardware classes need to inherit Instrument (as they needed to all along).
    
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/interfaces/Instrument.py
+++ catkit/interfaces/Instrument.py
@@ -6,31 +6,64 @@ class Instrument(ABC):
     """Generic interface to any instrument, implements a context manager."""
     log = logging.getLogger(__name__)
 
+    instrument_lib = None
+
     def __new__(cls, config_id):
-        return super(Instrument, cls).__new__(cls)
+        return super().__new__(cls)
 
     def __init__(self, config_id, *args, **kwargs):
-        """Opens connection with the Instrument and sets class attributes for 'config_id'"""
-
+        self.instrument = None
+        self.__keep_alive = False  # Back door - DO NOT USE!!!
         self.config_id = config_id
-        self.socket_id = None
-        self.instrument = self.initialize(*args, **kwargs)
-        self.log.info("Initialized " + config_id)
+        self.socket_id = None  # Legacy - we will remove at some point.
+        self.initialize(*args, **kwargs)  # This should NOT open a connection!
+        self.log.info("Initialized '{}' (but connection is not open)".format(config_id))
 
     # Context manager Enter function, gets called automatically when the "with" statement is used.
-    def __enter__(self, *args, **kwargs):
+    def __enter__(self):
+        # Attempt to force the use of ``with`` by only opening here and not in __init__().
+        self.__open()
         return self
 
     # Context manager Exit function, gets called automatically the code exits the context of the "with" statement.
     def __exit__(self, exception_type, exception_value, exception_traceback):
-        self.close()
-        self.instrument = None
-        self.log.info("Safely closed connection to " + self.config_id)
+        try:
+            if not self.__keep_alive:
+                self.__close()
+        finally:
+            # Reset, single use basis only.
+            self.__keep_alive = False
+
+    def __del__(self):
+        self.__close()
+
+    def __open(self):
+        # __func() can't be overridden without also overriding those that call it.
+        try:
+            self.instrument = self._open()
+            self.log.info("Opened connection to '{}'".format(self.config_id))
+        except Exception:
+            self.__close()
+            raise
+
+    def __close(self):
+        # __func() can't be overridden without also overriding those that call it.
+        try:
+            if self.instrument:
+                self.close()
+                self.log.info("Safely closed connection to '{}'".format(self.config_id))
+        finally:
+            self.instrument = None
 
     @abstractmethod
     def initialize(self, *args, **kwargs):
-        """Implement this function to return an object that can control the instrument."""
+        """Implement this function to initialize class BUT MUST NEVER open a connection to the instrument."""
+
+    @abstractmethod
+    def _open(self):
+        """Open connection. MUST return an object connected to the instrument,
+        that can be assigned to self.instrument."""
 
     @abstractmethod
     def close(self):
-        """Close connection safely."""
+        """Close connection to self.instrument. Must be a NOOP if self.instrument is None"""
