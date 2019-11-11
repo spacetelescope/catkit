f0bba60c28804d82d0be9f744a2d8781e18a63f5
Jules Fowler <jfowler@stsci.edu>
2019-11-11 15:11:41 -0500

    Stripped down Camera now that Instrument does most of the heavy lifting.

--- catkit/interfaces/Camera.py
+++ catkit/interfaces/Camera.py
@@ -1,37 +1,12 @@
-import logging
-
 from abc import ABC, abstractmethod
 
+from catkit.interfaces.Instrument import Instrument
 """Abstract base class for all cameras. Implementations of this class also become context managers."""
 
 
-class Camera(ABC):
-    log = logging.getLogger(__name__)
-
-    def __init__(self, config_id, *args, **kwargs):
-        """Opens connection with camera sets class attributes for 'config_id' and 'camera'."""
-        self.config_id = config_id
-        self.camera = self.initialize(self, *args, **kwargs)
-        self.log.info("Opened connection to camera: " + self.config_id)
-
-    # Implementing context manager.
-    def __enter__(self, *args, **kwargs):
-        return self
-
-    def __exit__(self, exception_type, exception_value, exception_traceback):
-        self.close()
-        self.camera = None
-        self.log.info("Safely closed camera: " + self.config_id)
-
-    # Abstract Methods.
-    @abstractmethod
-    def initialize(self, *args, **kwargs):
-        """Opens connection with camera and returns the camera manufacturer specific object."""
-
-    @abstractmethod
-    def close(self):
-        """Close camera connection."""
+class Camera(Instrument, ABC):
 
     @abstractmethod
     def take_exposures(self, exposure_length, num_exposures, path=None, filename=None, *args, **kwargs):
         """Takes exposures and should be able to save fits and simply return the image data."""
+
