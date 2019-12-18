1e7b44fe0d8031a411eda43b89a78cb6d01a2fdf
Iva Laginja <laginja.iva@gmail.com>
2019-12-18 12:36:33 -0500

    Adding experiment class file and module

---
+++ MTFSampling.py
@@ -0,0 +1,9 @@
+import logging
+import numpy as np
+
+from hicat.experiments.Experiment import Experiment
+
+
+class MTFSampling(Experiment):
+
+    pass
--- modules/mtf_sampling.py
+++ modules/mtf_sampling.py
@@ -1,32 +1,38 @@
 """
 module for pixel sampling determination
 """
-from glob import glob
-import logging
 import os
 
 from astropy.io import fits
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import numpy as np
+import hicat.util
 
 
-def collect_final_images(path):
-	results = glob(os.path.join(path, "*_cal.fits"))
-	im = str(results[0])
-	return im
+def mtf_sampling(dirpath, im_path, threshold):
+	"""
+	Calculate the MTF pixel sampling using the equivalent area method.
+	    Inputs
+	        dirpath: string, path to the to the directory for the output MTF data
+	        im_path: string, path to the focused input image
+	        threshold: int, the background threshold used to calculate the MTF  support
+	    Output
+	        sampling: float, the MTF sampling in pixels per lambda/D
+	"""
+	mtf_dir = hicat.util.create_data_path(dirpath, suffix='mtf_diagnostics')
+	os.makedirs(mtf_dir, exist_ok=True)
 
-
-def mtf_sampling(path,threshold):
-	mtf_dir = 'mtf_diagnostics'
-	os.makedirs(os.path.join(path, mtf_dir), exist_ok=True)
-
-	im_path = collect_final_images(path)
 	psf = fits.getdata(im_path)
-	imsize = psf.shape[1]
+	full_imsize = psf.shape[1]
+	psf_sub = psf[int(full_imsize/4):int(full_imsize/1.4), int(full_imsize/4):int(full_imsize/1.4)]
+	imsize = psf_sub.shape[1]
+
+	# Save cropped image to diagnostics
+	hicat.util.write_fits(psf_sub, os.path.join(mtf_dir, 'cropped_image.fits'))
 
 	# Calculate the OTF
-	otf = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(psf)))
+	otf = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(psf_sub)))
 	mtf = np.abs(otf)
 	mtfmax = np.max(mtf)
 	mtf = mtf / mtfmax
@@ -34,7 +40,7 @@ def mtf_sampling(path,threshold):
 	plt.clf()
 	plt.imshow(mtf)
 	plt.title('Modulation transfer function (MTF)')
-	plt.savefig(os.path.join(path, mtf_dir, 'MTF.pdf'))
+	plt.savefig(os.path.join(mtf_dir, 'MTF.pdf'))
 
 	bg_zone = mtf[1:int(imsize/8), 1:int(imsize/8)]
 	med = np.median(bg_zone)
@@ -45,17 +51,28 @@ def mtf_sampling(path,threshold):
 
 	plt.clf()
 	plt.imshow(mask)
-	plt.savefig(os.path.join(path, mtf_dir, 'mtf_support.pdf'))	
-	mtf_masked = mtf*mask
+	plt.savefig(os.path.join(mtf_dir, 'mtf_support.pdf'))
+	mtf_masked = mtf * mask
 	plt.clf()
 	plt.imshow(mtf_masked, norm=LogNorm())
 	plt.title('Modulation transfer function (MTF) Masked')
-	plt.savefig(os.path.join(path, mtf_dir, 'mtf_masked.pdf'))
+	plt.savefig(os.path.join(mtf_dir, 'mtf_masked.pdf'))
 
-	area = np.count_nonzero(mtf_masked)
+	# Crop the MTF in case there are those weird side spots showing up
+
+	mtf_masked_cropped = mtf_masked[int(imsize/7):int(imsize/1.2), int(imsize/7):int(imsize/1.2)]
+
+	plt.clf()
+	plt.imshow(mtf_masked_cropped, norm=LogNorm())
+	plt.title('Modulation transfer function (MTF) Masked and cropped')
+	plt.savefig(os.path.join(dirpath, mtf_dir, 'mtf_masked_cropped.pdf'))
+
+	area = np.count_nonzero(mtf_masked_cropped)
 	cutoff_eq = np.sqrt(area/np.pi)
 	sampling = float(imsize) / float(cutoff_eq)
 
+	with open(os.path.join(mtf_dir, 'sampling_params.txt'), 'w') as f:
+		f.write('sampling: {}\n'.format(sampling))
+		f.write('threshold: {}'.format(threshold))
+
 	return sampling
-
-
