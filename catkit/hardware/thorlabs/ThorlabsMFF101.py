88571aaa40268798aed17edde3f0cf291672f7e8
Jules Fowler <jfowler@stsci.edu>
2019-12-24 19:01:26 -0600

    Initial work retooling flipmount hardware in new style.

--- catkit/hardware/thorlabs/ThorlabsMFF101.py
+++ catkit/hardware/thorlabs/ThorlabsMFF101.py
@@ -15,11 +15,15 @@ class ThorlabsMFF101(FlipMotor):
     log = logging.getLogger(__name__)
 
     def initialize(self, *args, **kwargs):
-        """Creates an instance of the controller library and opens a connection."""
-
+        """Creates an instance of the controller library."""
+        
+        self.config_id = config_id
+        self.serial = None
+        self.log.info("Opened connection to flip motor " + config_id)
         serial = CONFIG_INI.get(self.config_id, "serial")
         self.serial = serial
-
+    
+    def _open(self):
         # noinspection PyArgumentList
         motor = ftd2xx.openEx(bytes(serial, 'utf-8'))
         motor.setBaudRate(115200)
@@ -30,26 +34,30 @@ class ThorlabsMFF101(FlipMotor):
         motor.resetDevice()
         motor.setFlowControl(constants.FLOW_RTS_CTS, 0, 0)
         motor.setRts()
-        return motor
+        self.instrument = motor
+        self.motor = self.instrument
 
-    def close(self):
+        return self.instrument
+
+    def _close(self):
         """Close dm connection safely."""
-        self.motor.close()
-
+        self.instrument.close()
+        self.log.info("Safely closed connection to flip motor " + self.config_id)
+    
     def move_to_position1(self):
         """Implements a move to the "up" position."""
         self.log.info("Moving to 'up' position")
         up_command = b"\x6A\x04\x00\x01\x21\x01"
-        self.motor.write(up_command)
+        self.instrument.write(up_command)
         time.sleep(1)
 
     def move_to_position2(self):
         """Implements a move to "down" position """
         self.log.info("Moving to 'down' position")
         down_command = b"\x6A\x04\x00\x02\x21\x01"
-        self.motor.write(down_command)
+        self.instrument.write(down_command)
         time.sleep(1)
 
     def blink_led(self):
         self.log.info(".blink.")
-        self.motor.write(b"\x23\x02\x00\x00\x21\x01")
+        self.instrument.write(b"\x23\x02\x00\x00\x21\x01")
