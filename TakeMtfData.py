61537baf70d8436bccdaa84a32d912c7af2f6505
Heather Kurtz <hkurtz@stsci.edu>
2020-02-27 17:07:01 -0500

    mtf sampling added curve fit

--- TakeMtfData.py
+++ TakeMtfData.py
@@ -60,5 +60,5 @@ class TakeMtfData(Experiment):
                                                           filename=flat_file_name, camera_type=self.camera_type,
                                                           simulator=False,
                                                           **self.kwargs)
-        ps_wo_focus, ps_w_focus, focus = mtf_sampling(cal_file_path, self.mtf_snr_threshold)
-        self.log.info("ps_wo_focus=" + str(ps_wo_focus) + " ps_w_focus=" +str(ps_w_focus) + " focus=" +str(focus) )
+        ps_w_focus = mtf_sampling(cal_file_path, self.mtf_snr_threshold)
+        self.log.info( " ps_w_focus=" +str(ps_w_focus))
--- modules/mtf_sampling.py
+++ modules/mtf_sampling.py
@@ -18,34 +18,51 @@ from matplotlib.colors import LogNorm
 import numpy as np
 from scipy.optimize import curve_fit
 
-from catkit.hardware.boston.commands import flat_command
-import hicat.util
-from hicat.hardware import testbed
-from catkit.catkit_types import *
-from hicat.config import CONFIG_INI
+#import hicat.util
+#from hicat.hardware import testbed
+#from hicat.config import CONFIG_INI
 
 
 def collect_final_images(path):
-	results = [y for x in os.walk(path) for y in glob(os.path.join(x[0], "*_cal.fits"))]
-	for img in results:
-		copyfile(img, os.path.join(path, os.path.basename(img)))
+	results = glob(os.path.join(path, "*_cal.fits"))
+	im = (str(results[0]))
+	return(im)
+	#for img in results:
+	#	copyfile(img, os.path.join(path, os.path.basename(img)))
 
-def otf(nu, nuc):
+def otf_calc(nu, nuc):
 	#nu is the fequency and nuc is te cut off frequesncy. I'm not sure where to get this info
 	#nuc = 2.   # I want a single variable function so I am hacking this. We never change it anyway.
-	otf_analytical = (2*np.pi) * (np.arccos(np.abs(nu/nuc)) - np.abs(nu/nuc) * np.sqrt(1 - (nu/nuc)**2))
+	otf_analytical = (2*np.pi) * (np.arccos(np.abs(nu/nuc)) - np.abs(nu/nuc) * np.sqrt((1 - (nu/nuc))**2))
 	return otf_analytical
 
-
-def otf_analytical_fit(cutoff_eq):
-	px_marg_err = 3.    # the margin around cutoff_eq (either side) we want to fit
-	px_step = 0.05        # steps for the fit
+def otf_analytical_fit(cutoff_eq,mtf):
+	px_marg_err = 2.    # the margin around cutoff_eq (either side) we want to fit
+	px_step_size = 0.07        # steps for the fit
 	
 	# Create an array for the frequency region we want to fit
-	fit_array = np.arange(cutoff_eq-px_marg_err, cutoff_eq+px_marg_err, px_step)
+	fit_x = np.arange(cutoff_eq-px_marg_err, cutoff_eq+px_marg_err, px_step_size)
+	fit_y = np.arange(cutoff_eq-px_marg_err, cutoff_eq+px_marg_err, px_step_size)
+	X,Y = np.meshgrid(fit_x, fit_y)
 	
+	ravel_grid = np.vstack((X.ravel(), Y.ravel()))
+	ravel_grid.astype(float)
+	#print(ravel_grid)
+	#mtf_ravel = mtf.ravel()
+	#Z = otf_calc(X, Y)
+	#print(Z.shape)
+	small_mtf = mtf[105:605,105:605]
+	test = np.ravel(small_mtf)#.ravel()
+	#print((2*np.pi)* np.arccos(np.abs(X/test)) - np.abs(X/test) * np.arccos(np.sqrt(1 - (X/test)**2)))
+	test.astype(float)
+	data = np.abs(X.ravel()/Y.ravel())#otf_calc(X,Y)
+	print(max(data))
 	# Make a least-squares fit
-	one, two = curve_fit(otf, mask, mtf_masked)
+	one, two = curve_fit(otf_calc, X.ravel(), Y.ravel())
+	calc = otf_calc(X.ravel(), small_mtf.ravel())
+	print(calc)
+	plt.plot(calc)
+	plt.show()
 	return one, two
 
 
@@ -56,7 +73,9 @@ def mtf_sampling(path,threshold):
 
 	# Import the image
 	img = collect_final_images(path)
-	psf = fits.getdata(img[0])
+	hdu = fits.open(img, mode='readonly')
+	psf = hdu[0].data
+	#psf = fits.getdata(img)
 	# Get image size
 	imsize = psf.shape[1]
 	# Check where the PSF maximum ist
@@ -104,11 +123,15 @@ def mtf_sampling(path,threshold):
 	sampling = float(imsize) / float(cutoff_eq)
 
 	#this section does the analytical fitting. It still needs work IDK nu, not sure the otf_analytical_fit will work or the output.
-	otf_analytical = otf(nu, cutoff_eq)
-	one, two = otf_analytical_fit(cutoff_eq)
+	#nu=2
+	#otf_analytical = otf_calc(nu, cutoff_eq)
+	one, two = otf_analytical_fit(cutoff_eq,mtf)
+	print(one, two)
+
 	otf_min = np.min(one, two)
 
 	lam_d_wo_defocus = float(imsize) / float(otf_min)
+
 
 
 	#print('The sampling for image "' + filename + '" is: ' + str(sampling))
@@ -133,4 +156,14 @@ def mtf_sampling(path,threshold):
 	#sampling = float(imsize) / float(mtf_min)
 	#print('The sampling for image "{}" is: {}'.format(filename, sampling))
 
-	return(ps_wo_focus, ps_w_focus, focus)
+	return(lam_d_wo_defocus)
+
+
+
+
+def main():
+	path = '/astro/opticslab1/hicat_data/2019-07-18T14-00-23_mtf_calibration/direct'
+	threshold = 25
+	mtf_sampling(path,threshold)
+main()
+
