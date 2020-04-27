a69f7f7772363f7eeb8053b1a52b7898708b70c5
Keira Brooks <kbrooks@stsci.edu>
2020-04-27 09:53:26 -0400

    Remove most dependencies on mapping for IrisAO command creation

--- catkit/hardware/iris_ao/segmented_dm_command.py
+++ catkit/hardware/iris_ao/segmented_dm_command.py
@@ -56,25 +56,55 @@ class SegmentedDmCommand(object):
         self.filename_flat = CONFIG_INI.get(config_id, 'flat_file_ini') #format is .ini
 
         # Define aperture - full aperture or subaperture
-        # If you are not using the full aperture, must include which segments are used
         try:
             self.segments_in_pupil = json.loads(CONFIG_INI.get(config_id, 'active_segment_list'))
-            self.number_segments_in_pupil = CONFIG_INI.getint(config_id, 'active_number_of_segments')
-            if self.segments_in_pupil[0] != 1 and convert_to_native_mapping:
-                self._shift_center = True # Pupil is centered elsewhere, must shift
+            self.number_segments_in_pupil = CONFIG_INI.getint(config_id,
+                                                              'active_number_of_segments')
+            if len(self.segments_in_pupil) != self.number_segments_in_pupil:
+                raise ValueError("The array length active_segment_list does not match the active_number_of_segments in the config.ini. Please update your config.ini.")
         except NoOptionError:
-            self.segments_in_pupil = iris_util.iris_pupil_numbering()
+            self.segments_in_pupil = iris_util.iris_pupil_naming()
 
         if data is None:
             # If no data given, return dictionary of zeros
-            data = iris_util.create_zero_dictionary(self.number_segments_in_pupil,
-                                                    seglist=self.segments_in_pupil)
+            data = iris_util.create_zero_array(self.number_segments_in_pupil)
 
-        self.data = data
         self.apply_flat_map = apply_flat_map
+        self.data = None
+        self.command = None
 
-        if self._shift_center:
-            self.data = shift_command(self.data, self.segments_in_pupil)
+
+    def read_new_command(self, segment_values):
+        """
+        Read a new command and assign to the attribute 'data'
+
+        :param segment_values: str, array. Can be .PTT111, .ini files or array where the first
+                               element of the array is the center of the pupil and subsequent
+                               elements continue up and clockwise around the pupil (see README
+                               for more information) of the form {seg: (piston, tip, tilt)}
+        """
+        self.data = self.read_command(segment_values)
+
+
+    def read_command(self, segment_values):
+        """
+        Read a command from one of the allowed formats
+
+        :param segment_values: str, array. Can be .PTT111, .ini files or array where the first
+                               element of the array is the center of the pupil and subsequent
+                               elements continue up and clockwise around the pupil (see README
+                               for more information) of the form {seg: (piston, tip, tilt)}
+        """
+        ptt_arr, segment_names = iris_util.read_segment_values(segment_values)
+        if segment_names is not None:
+            command_array = []
+            for seg_name in self.segments_in_pupil:
+                ind = np.where(segment_names == seg_name)[0][0]
+                command_array.append(ptt_arr[ind])
+        else:
+            command_array = ptt_arr
+
+        return command_array
 
 
     def get_data(self):
@@ -88,76 +118,40 @@ class SegmentedDmCommand(object):
         """
         # Apply Flat Map
         if self.apply_flat_map:
-            self.add_map(self.filename_flat, apply_shift=False)
+            self.add_map(self.filename_flat)
+        #TODO should this be saved as an attribute?
+        command_dict = {seg: ptt for seg, ptt in zip(self.segments_in_pupil, self.data)}
 
-        return self.data
+        return command_dict
 
 
-    def add_map(self, segment_values_to_add, apply_shift=True):
+    def update_one_segment(self, segment_ind, ptt_tuple):
+        """ Update the value of one segment. This will be ADDED to the current value
+
+        :param segment_ind: int, the index of the segment in the pupil to be updated
+                            (see README for relationship between index and segment)
+        :param ptt_tuple: tuple with three values for piston, tip, and tilt
+
+        """
+        command_array = iris_util.create_zero_array(self.number_segments_in_pupil)
+        command_array[segment_ind] = ptt_tuple
+
+        self.add_map(command_array)
+
+
+    def add_map(self, segment_values_to_add):
         """
         Add a command to the one already loaded.
 
-        Will shift the new command if you are using a shifted pupil. Updates self.data
-        with combined commands.
-
-        Will not shift the flat (flat=True) since the flat is segment-specific.
-
         :param new_command: str or array (.PTT111 or .ini file, or array from POPPY)
-        :param flat: bool, only True if the map being added is the flat (so that it is not shifted)
         """
         original_data = self.get_data()
-        data_to_add, _ = iris_util.read_segment_values(segment_values_to_add)
+        new_command = self.read_command(segment_values_to_add)
 
-        if self._shift_center and apply_shift:
-            data_to_add = shift_command(data_to_add, self.segments_in_pupil,
-                                        iris_util.iris_pupil_numbering())
-
-        # Do magic adding only if segment exists in both
-        combined_data = {seg: tuple(np.asarray(original_data.get(seg, (0., 0., 0.))) + np.asarray(data_to_add.get(seg, (0., 0., 0.)))) for seg in set(original_data) & set(data_to_add)}
-
-        self.data = combined_data
+        self.data = original_data + new_command
 
 
-def shift_command(command_to_shift, to_pupil, from_pupil=None):
-    """
-    If using a custom pupil, you must shift the numbering from centering in the center
-    of the Iris AO, to the center of your pupil.
-
-    This function will shift the pupil from the "from_pupil" (generally centered on the
-    full Iris AO) to the "to_pupil" (generally a custom pupil with a custom center)
-
-    Different pupil numbering systems include:
-    Full Iris: <= 37 segments centered on 1 and numbered from 1-19 (and until 37)
-               for overall Iris AO pupil. This is the default "from_pupil"
-               if None is given. This numbering is given by iris_util.iris_pupil_numbering
-    Custom: <=19 segments centered on first segment number in "segments_in_pupil" in
-            the config file and numbered specifically for a custom pupil, defining
-            the custom pupil as part of the DM
-    Poppy: POPPY uses a different numbering scheme given by iris_util.poppy_numbering
-
-    :param command_to_shift: dict, wavefront map to be shifted, Iris AO format
-    :param to_pupil: list, of segments in the pupil that you are moving to, starting
-                     at the center and then continuing counter clockwise
-    :param from_pupil: list, segments in the pupil with numbering system you are moving from.
-                       If None (default) the Iris AO numbering will be used
-
-    :return: dict, command shifted to expected center
-    """
-    if from_pupil is None:
-        from_pupil = iris_util.iris_pupil_numbering()
-
-    # Match lengths of arrays
-    from_pupil, to_pupil = iris_util.match_lengths(from_pupil, to_pupil)
-
-    mapping = iris_util.map_to_new_center(to_pupil, from_pupil)
-
-    # Create the new map with the mapping of the input
-    shifted_map = iris_util.remap_dictionary(command_to_shift, mapping)
-
-    return shifted_map
-
-
-def load_command(segment_values, segment_mapping=None, apply_flat_map=True, config_id='iris_ao'):
+def load_command(segment_values, apply_flat_map=True, config_id='iris_ao'):
     """
     Loads the segment_values from a file, array, or dictionary and returns a
     SegmentedDmCommand object.
@@ -171,12 +165,10 @@ def load_command(segment_values, segment_mapping=None, apply_flat_map=True, conf
 
     :return: SegmentedDmCommand object representing the command dictionary.
     """
-    data, convert_to_native_mapping = iris_util.read_segment_values(segment_values,
-                                                                    segment_mapping)
-    return SegmentedDmCommand(data,
-                              convert_to_native_mapping=convert_to_native_mapping,
-                              apply_flat_map=apply_flat_map,
-                              config_id=config_id)
+    dm_command_obj = SegmentedDmCommand(apply_flat_map=apply_flat_map, config_id=config_id)
+    dm_command_obj.read_new_command(segment_values)
+
+    return dm_command_obj
 
 
 ## POPPY
@@ -200,29 +192,34 @@ class PoppySegmentedCommand():
         self.flat_to_flat = CONFIG_INI.getfloat('iris_ao', 'flat_to_flat_mm')  # [mm]
         self.gap = CONFIG_INI.getint('iris_ao', 'gap_um')  # [um]
         self.num_segs_in_pupil = CONFIG_INI.getint('iris_ao', 'active_number_of_segments')
+        self.outer_ring_corners = CONFIG_INI.getboolean('iris_ao', 'include_outer_ring_corners')
         self.wavelength = (CONFIG_INI.getint('thorlabs_source_mcls1', 'lambda_nm')*u.nm).to(u.m)
 
         self.radius = (self.flat_to_flat/2*u.mm).to(u.m)
         self.num_terms = (self.num_segs_in_pupil - 1) * 3
+        self.num_rings = get_num_rings(self.num_segs_in_pupil, self.outer_ring_corners)
 
         self.global_coefficients = global_coefficients
 
         # Create the specific basis for this pupil
         self.basis = self.create_ptt_basis()
+        self.segment_list = get_segment_list(self.num_rings, center_segment=True, outer_corners=True)
 
         # Create array of coefficients
         self.array_of_coefficients = self.get_array_from_global()
 
 
     def create_ptt_basis(self):
+        #TODO: add segment list
         """
         Create the basis needed for getting the per/segment coeffs back
 
         :return: Poppy Segment_PTT_Basis object for the specified pupil
         """
-        pttbasis = poppy.zernike.Segment_PTT_Basis(rings=get_num_rings(self.num_segs_in_pupil),
+        pttbasis = poppy.zernike.Segment_PTT_Basis(rings=self.num_rings,
                                                    flattoflat=self.flat_to_flat,
-                                                   gap=self.gap)
+                                                   gap=self.gap,
+                                                   segmentlist=self.segment_list)
         return pttbasis
 
 
@@ -256,7 +253,6 @@ class PoppySegmentedCommand():
         coeff_array = poppy.zernike.opd_expand_segments(wavefront, nterms=self.num_terms,
                                                         basis=self.basis)
         coeff_array = np.reshape(coeff_array, (self.num_segs_in_pupil - 1, 3))
-
         center_segment = np.array([0.0, 0.0, 0.0]) # Add zeros for center segment
         coeff_array = np.vstack((center_segment, coeff_array))
 
@@ -270,78 +266,93 @@ class PoppySegmentedCommand():
         :return: np.ndarray of coefficients for piston, tip, and tilt, for your pupil
         """
         wavefront = self.create_wavefront_from_global(self.global_coefficients)
-
         coeffs_array = self.get_coeffs_from_pttbasis(wavefront)
+        coeffs_array = round_array(coeffs_array)
 
         return coeffs_array
 
 
-    def to_dictionary(self, map_to_iris=True):
-        """
-        Read in an array produced by POPPY. Each entry in array is a tuple of (piston, tip, tilt)
-        values for the segment number that corresponds with the index in the array.
+def round_array(ptt_arr, decimals=3):
+    """
+    Make sure that the PTT coefficients in the array are rounded to decimals
 
-        This function will convert the values in this array from si units to um and mrad, as
-        expected by IrisAO .
-
-        :param map_to_iris: bool, if True, you are putting this command directly on the hardware.
-                            If False, the command will remain in the Poppy numbering
-
-        :return: dict, command in the form of a dictionary of the form {seg: (piston, tip, tilt)}
-                 with units of ([um], [mrad], [mrad])
-        """
-        # Give seglist so that you guarentee you start at 0
-        dictionary = iris_util.create_dict_from_array(self.array_of_coefficients,
-                                                      seglist=np.arange(len(self.array_of_coefficients)))
-        # Convert from meters and radians (what Poppy outputs) to um and mrad.
-        dictionary = iris_util.convert_dict_from_si(dictionary)
-
-        # Round to 3 decimal points after zero.
-        command_dict = {seg: (np.round(ptt[0], 3), -1*np.round(ptt[2], 3),
-                              np.round(ptt[1], 3)) for seg, ptt in list(dictionary.items())}
-
-        # Re map to Iris AO
-        if map_to_iris:
-            to_pupil = iris_util.iris_pupil_numbering()[:self.num_segs_in_pupil]
-            from_pupil = poppy_numbering()
-            command_dict = shift_command(command_dict, to_pupil, from_pupil)
-
-        return command_dict
+    :param ptt_arr: ndarray or list, of tuples existing of piston, tip, tilt,
+                    values for each segment in a pupil
+    """
+    return [(np.round(ptt[0], decimals), -1*np.round(ptt[2], decimals),
+             np.round(ptt[1], decimals)) for ptt in ptt_arr]
 
 
 def poppy_numbering():
     """
     Numbering of the pupil in POPPY. Specifically for a 37 segment Iris AO
     """
-    return [0,   # Ring 0
-            1, 6, 5, 4, 3, 2,  # Ring 1
-            7, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8,  # Ring 2
-            19, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20]  # Ring 3
+    return np.arange(163)
 
 
-def get_num_rings(number_segments_in_pupil):
+def get_num_rings(number_segments_in_pupil, outer_ring_corners, max_rings=7):
     """
-    Get the number of rings of segments from using the number_segments_in_pupil parameter
-    This is specific for a segmented DM with 37 segments therefore:
-      - 37 segments = 3 rings
-      - 19 segments = 2 rings
-      - 7 segments = 1 ring
-      - 1 segment = 0 rings
+    Get the number of rings based on the number of specified segments using the
+    number_segments_in_pupil and include_outer_ring_corners parameters. This can
+    be used for a pupil of up to 7 rings (the max allowed in the PTT489 IrisAO model).
+    Note that for the PTT489 model the 7th ring does not include the corner segments
+    which you will need to make clear.
 
     :param num_segs_in_pupil: int, the number of segments in the pupil.
-
     :return: num_rings: int, the number of full rings of hexagonal segments in the pupil
     """
     # seg_nums: number of segments in a pupil of the corresponding # of rings
-    seg_nums = np.array([37, 19, 7, 1])
-    ring_nums = np.array([3, 2, 1, 0])
+    seg_nums = [1,] # account for center segment
+    for i in (np.arange(max_rings)+1):
+        seg_nums.append(seg_nums[i-1]+i*6)
+
+    if not outer_ring_corners:
+        seg_nums = [num-6 if num>6 else num for num in seg_nums]
 
     if number_segments_in_pupil not in seg_nums:
-        raise Exception("Invalid number of segments for number_segments_in_pupil.")
+        raise Exception("Invalid number of segments give number_segments_in_pupil and include_outer_ring_corners parameters.")
 
+    # The number of rings
+    ring_nums = np.arange(max_rings+1)
     num_rings = [rings for segs, rings in zip(seg_nums,
                                               ring_nums) if number_segments_in_pupil == segs][0]
     return num_rings
+
+
+def number_segments(num_rings, center_segment=True, outer_corners=False):
+    """
+    Determine number of segments in an aperture based on number of rings,
+    existence of a center segment, and if it includes the corners on the outer-most ring
+
+    :param rings: int, number of
+
+    """
+    num_segs = int(center_segment)
+    for ring in np.arange(num_rings)+1:
+        num_segs += 6*ring
+
+    if not outer_corners:
+        num_segs -= 6
+
+    return num_segs
+
+def get_segment_list(num_rings, center_segment=True, outer_corners=True):
+    """
+    regardless of if the aperture has a center segment, we still want to include it?
+    """
+    num_segs = number_segments(num_rings, center_segment=True, outer_corners=True)
+    seglist = np.arange(num_segs)
+
+    if not outer_corners:
+        inner_segs = seglist[:(num_segs-6*num_rings)]
+        outer_segs = seglist[(num_segs-6*num_rings):]
+        outer_segs = np.delete(outer_segs, np.arange(0, outer_segs.size, num_rings)) # delete corner segs
+
+        seglist = np.unique(np.concatenate((inner_segs, outer_segs)))
+
+    if not center_segment:
+        seglist = seglist[1:]
+    return seglist
 
 
 def get_wavefront_from_coeffs(basis, coeff_array):
@@ -411,20 +422,19 @@ def display(segment_values, instrument_fov, active_segment_list=None,
     # read_segment_values returns the data and whether or not to convert into
     #native/Iris mapping, in this case we want to know if it's in centered mapping
     # convert_to_native_mapping = centered_mapping
-    data, centered_mapping = iris_util.read_segment_values(segment_values,
-                                                           segment_mapping)
+    data, centered_mapping = iris_util.read_segment_values(segment_values)
     # Shift TO Poppy from the Iris AO
     if not active_segment_list:
-        active_segment_list = iris_util.iris_pupil_numbering()
-    command = data if centered_mapping else shift_command(data,
-                                                          poppy_numbering()[:len(active_segment_list)],
-                                                          active_segment_list)
+        active_segment_list = iris_util.iris_pupil_naming()
+    # command = data if centered_mapping else shift_command(data,
+    #                                                       poppy_numbering()[:len(active_segment_list)],
+    #                                                       active_segment_list)
     iris = poppy.dms.HexSegmentedDeformableMirror(name='Iris DM',
                                                   rings=num_rings,
                                                   flattoflat=flat_to_flat,
                                                   gap=gap)
 
-    iris = deploy_global_wf(iris, command) # takes pupil and dictionary
+    iris = deploy_global_wf(iris, data) # takes pupil and dictionary
 
     if wavefront:
         plt.figure()
