a32958340842aca28f48f26d54b5b69be47e2cce
Matthew Maclay <mmaclay@stsci.edu>
2020-06-11 13:54:44 -0400

    first revisions from Marshall's feedback

--- modules/contrast_statistics.py
+++ modules/contrast_statistics.py
@@ -2,18 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas
 import os
+import logging
 
-
-def calculate_iteration_of_convergence(metrics_data, slope_threshold=0.0008):
+def calculate_iteration_of_convergence(filepath, slope_threshold=0.00008):
     """
-    Calculate the iteration at which the contrast converges. Uses a slope estimate first. If this fails to converge,
-    it simply returns n/2 (iteration at the halfway point) and throws a warning.
+    Calculate the iteration at which the contrast converges. Fits a 5th order polynomial and uses the first derivative
+    to infer a slope estimate first. The absolute value of the slope must be below a set parameter, slope_threshold.
+    If this fails to converge, it simply returns n/2 (iteration at the halfway point) and throws a warning.
     :param metrics_data: The csv or df of data including iteration number and mean contrast (may inc. humidity & temp)
     :param slope_threshold: Threshold that slope must be below to be considered 'converged'
     :return: number of the iteration at which the slope *first* crosses the slope_threshold.
     """
-    if type(metrics_data) == str:
-        metrics_data = pandas.read_csv(metrics_data)
+    if type(filepath) == str:
+        metrics_data = pandas.read_csv(filepath)
 
     contrast_fit = np.polyfit(metrics_data['iteration'], np.log(metrics_data[' mean image contrast']), 5)
     fit_1d = np.poly1d(contrast_fit)
@@ -23,37 +24,40 @@ def calculate_iteration_of_convergence(metrics_data, slope_threshold=0.0008):
 
     # Warning fix this: selects last half of data if no convergence
     if len(convergence_metrics) == 0:
-        print("Warning: Iterations do not converge to required slope threshold. Selecting last half of data.")
         iteration_of_convergence = int(metrics_data['iteration'].iloc[-1] / 2)
+        logging.warning("Iterations do not converge to required slope threshold. Selecting last half of data, iteration"
+                        f" {iteration_of_convergence}.")
+        warning_flag = True
     elif len(convergence_metrics) >= 1:
         iteration_of_convergence = convergence_metrics['iteration'].iloc[0]
+        warning_flag = False
         print(f"Slope threshold reached at iteration {iteration_of_convergence}")
 
-    return iteration_of_convergence
+    return iteration_of_convergence, warning_flag
 
 
 def ecdf(data):
-    """ Compute ECDF """
+    """ Compute Empirical Cumulative Distribution Function """
     x = np.sort(data)
     n = x.size
     y = np.arange(1, n + 1) / n
     return x, y
 
 
-def calculate_confidence_interval(metrics_data, filepath='', iteration_of_convergence=None, generate_plots=True):
+def calculate_confidence_interval(filepath, iteration_of_convergence=None, generate_plots=True):
     """
-    Calculates the conrtrast, c, where with 90% confidence, actual measured contrast will be below (better) than c.
+    Calculates the contrast, c, where with 90% confidence, actual measured contrast will be below (better) than c.
     Using analytical assumption that mean contrast, µ + 1.28(sigma) is 90% confidence level.
-    :type filepath: str
-    :param metrics_data: The csv or df of data including iteration number and mean contrast (may inc. humidity & temp)
+    :param filepath: str path to The csv or df of data including iteration number and mean contrast
     :param iteration_of_convergence: Default=None, calculated in calculate_iteration_of_convergence
     :return: contrast value, c, that the actual measured contrast will be below (better) than c 90% of the time.
     """
-    if type(metrics_data) == str:
-        metrics_data = pandas.read_csv(metrics_data)
+
+    if type(filepath) == str:
+        metrics_data = pandas.read_csv(filepath)
 
     if iteration_of_convergence == None:
-        iteration_of_convergence = calculate_iteration_of_convergence(metrics_data)
+        iteration_of_convergence, warning_flag = calculate_iteration_of_convergence(filepath)
     elif type(iteration_of_convergence) == int:
         print(f"Implementing user-specified convergence point at iteration {iteration_of_convergence}")
 
@@ -65,8 +69,12 @@ def calculate_confidence_interval(metrics_data, filepath='', iteration_of_conver
 
     if generate_plots:
         fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(20, 4))
-        fig.suptitle(os.path.split(filepath)[-1])
+        fig.suptitle(os.path.split(filepath)[-2])
+        if warning_flag:
+            fig.suptitle('WARNING: CONVERGENCE CRITERIA QUESTIONABLE  '+os.path.split(filepath)[-2]+
+                         '  WARNING: CONVERGENCE CRITERIA QUESTIONABLE', c='r')
         ax1.axvline(iteration_of_convergence, label='Point of Convergence', c='g')
+
         ax1.set_yscale('log')
         ax1.plot(metrics_data['iteration'], metrics_data[' mean image contrast'],c='b', marker='o', alpha = 0.6)
         ax1.plot(converged_metrics['iteration'], converged_metrics[' mean image contrast'], c='g', marker='o',
@@ -76,6 +84,7 @@ def calculate_confidence_interval(metrics_data, filepath='', iteration_of_conver
         ax1.set_xlabel('Iteration')
         ax1.set_ylabel('Contrast')
         ax1.set_title('Contrast by Iteration')
+
         ax1.grid(True, which='both')
         ax1.legend()
 
@@ -97,13 +106,11 @@ def calculate_confidence_interval(metrics_data, filepath='', iteration_of_conver
         ax3.set_xlabel('Contrast')
         ax3.set_ylabel('Counts')
         ax3.set_title(f'Distribution of Contrast: {n_samples} iterations')
-
         plt.ticklabel_format(axis="x", style="sci", scilimits=(0, 0))
         ax3.legend()
 
 
         ecdf_x, ecdf_y = ecdf(converged_metrics[' mean image contrast'])
-
         ax4.plot(ecdf_x,ecdf_y,alpha=0.8)
         ax4.axvline(confidence_interval, label=f'90% CI: {confidence_interval:.3}', c='k', alpha=0.7, linestyle='-.')
         ax4.axvline(mean, label=f'Mean: {mean:.3}', c='k', linestyle='-')
@@ -115,6 +122,6 @@ def calculate_confidence_interval(metrics_data, filepath='', iteration_of_conver
         ax4.legend()
 
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.4, hspace=None)
-        fig.savefig(f'{filepath}contrast_metrics.pdf', dpi=300, bbox_inches='tight')
-
+        fig.savefig(os.path.join(os.path.split(filepath)[-2],'contrast_metrics.pdf'), dpi=300, bbox_inches='tight')
+        # print(os.path.join(os.path.split(filepath)[-2],'contrast_metrics.pdf'))
     return confidence_interval
