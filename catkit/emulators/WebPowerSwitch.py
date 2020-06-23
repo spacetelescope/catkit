42bd16b9083564fd66ab1a481ca46d2c0a5c0116
James Noss <jnoss@stsci.edu>
2020-06-22 20:13:00 -0400

    Post review changes
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/emulators/WebPowerSwitch.py
+++ catkit/emulators/WebPowerSwitch.py
@@ -1,10 +1,18 @@
+import requests
+
 from catkit.interfaces.Instrument import SimInstrument
 import catkit.hardware.WebPowerSwitch
 
 
-class WebPowerSwitchRequestsEmulator:
+class WebPowerSwitchRequestsEmulator():
+
+    def __init__(self, status_code=200):
+        self.status_code = status_code
+
     def get(self, url, params=None, **kwargs):
-        pass
+        resp = requests.Response()
+        resp.status_code = self.status_code
+        return resp
 
 
 class WebPowerSwitch(SimInstrument, catkit.hardware.WebPowerSwitch.WebPowerSwitch):
--- catkit/emulators/tests/test_web_power_switch.py
+++ catkit/emulators/tests/test_web_power_switch.py
@@ -1,6 +1,6 @@
-import itertools
 import os
 import pytest
+from requests import HTTPError
 
 from catkit.config import load_config_ini
 from catkit.emulators.WebPowerSwitch import WebPowerSwitch
@@ -8,8 +8,6 @@ from catkit.emulators.WebPowerSwitch import WebPowerSwitch
 
 config_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.ini")
 config = load_config_ini(config_filename)
-
-BOOL_VALUES = (True, False)
 
 
 @pytest.mark.usefixtures("dummy_config_ini", "dummy_testbed_state")
@@ -36,14 +34,14 @@ def test_individual_off():
     switch.turn_off("dm1_outlet")
 
 
-@pytest.mark.parametrize("on", BOOL_VALUES)
+@pytest.mark.parametrize("on", (True, False))
 @pytest.mark.usefixtures("dummy_config_ini", "dummy_testbed_state")
 def test_switch(on):
     switch = WebPowerSwitch(config_id="web_power_switch")
     switch.switch(outlet_id=None, on=on, all=True)
 
 
-@pytest.mark.parametrize("on", BOOL_VALUES)
+@pytest.mark.parametrize("on", (True, False))
 @pytest.mark.usefixtures("dummy_config_ini", "dummy_testbed_state")
 def test_switch_lists(on):
     outlet_ids = ("dm1_outlet", "motor_controller_outlet")
@@ -56,3 +54,21 @@ def test_outlet_list():
     switch = WebPowerSwitch(config_id="web_power_switch", outlet_list={"switch_1": 1, "switch_2": 2})
     switch.switch(outlet_id="switch_2", on=True)
     switch.turn_on("dm1_outlet")
+
+
+@pytest.mark.parametrize("status_code", (400, 500))
+def test_http_error(status_code):
+    switch = WebPowerSwitch(config_id="web_power_switch",
+                            outlet_list={"switch_1": 1, "switch_2": 2},
+                            status_code=status_code)
+    with pytest.raises(HTTPError, match=str(status_code)):
+        switch.switch(outlet_id="switch_2", on=True)
+
+
+@pytest.mark.parametrize("status_code", (100, 300))
+def test_runtime_error(status_code):
+    switch = WebPowerSwitch(config_id="web_power_switch",
+                            outlet_list={"switch_1": 1, "switch_2": 2},
+                            status_code=status_code)
+    with pytest.raises(RuntimeError, match=str(status_code)):
+        switch.switch(outlet_id="switch_2", on=True)
