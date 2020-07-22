c90544e7a9f2e71507854bcbfecfcbb242a7bf57
James Noss <jnoss@stsci.edu>
2020-07-22 16:29:16 -0400

    Final comparison to existing SimZwocamera
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/emulators/ZwoCamera.py
+++ catkit/emulators/ZwoCamera.py
@@ -1,11 +1,11 @@
 import numpy as np
 
+import zwoasi
+
 from hicat.config import CONFIG_INI
 from hicat.hardware import testbed_state
 import hicat.simulators
-import zwoasi
 
-from catkit.catkit_types import units, quantity
 from catkit.interfaces.Instrument import SimInstrument
 import catkit.hardware.zwo.ZwoCamera
 
@@ -35,27 +35,14 @@ class PoppyZwoEmulator(ZwoASI):
 
         self.config_id = config_id
         self.image_type = None
+        self.control_values = {}
         self.camera_mappings = self.get_camera_mappings()
+        self.photometry_config_key = None
 
         if self.config_id not in self.camera_mappings:
             raise ValueError(f"Unknown camera for simulations: {self.config_id}")
 
         self.camera_purpose = self.camera_mappings[self.config_id]["purpose"]
-
-        if self.camera_purpose == 'imaging_camera':
-            hicat.simulators.optics_simulator.detector = 'imager'
-            self.photometry_config_key = 'total_direct_photometry_cts_per_microsec'
-        elif self.camera_purpose == 'pupil_camera':
-            hicat.simulators.optics_simulator.detector = 'pupil_camera'
-            self.photometry_config_key = 'total_pupil_direct_photometry_cts_per_microsec'
-        elif self.camera_purpose == 'phase_retrieval_camera':
-            hicat.simulators.optics_simulator.detector = 'pr_camera'
-            self.photometry_config_key = 'total_prcam_direct_photometry_cts_per_microsec'
-        elif self.camera_purpose == 'zernike_camera':
-            hicat.simulators.optics_simulator.detector = 'zernike_wfs_camera'
-            self.photometry_config_key = 'total_zernike_direct_photometry_cts_per_microsec'
-        else:
-            raise NotImplementedError(f"Unknown camera for simulations: {self.camera_purpose}")
 
     def init(self, library_file=None):
         pass
@@ -79,7 +66,10 @@ class PoppyZwoEmulator(ZwoASI):
         return {'BandWidth': {'MinValue': None, 'ControlType': None, 'DefaultValue': None}}
 
     def set_control_value(self, control_type, value, auto=False):
-        pass
+        accepted_types = (int,)
+        if value is not None and not isinstance(value, accepted_types):
+            raise ValueError(f"Expected type {accepted_types} got '{type(value)}'")
+        self.control_values[control_type] = value
 
     def stop_video_capture(self):
         pass
@@ -90,33 +80,38 @@ class PoppyZwoEmulator(ZwoASI):
     def set_image_type(self, image_type):
         self.image_type = image_type
 
-    def capture(self, initial_sleep, poll):
-        """ Get a simulated image capture from the simulator
-
-        :param exposure_time: exposure time in microseconds
-        """
-        # exposure_time == initial sleep? Who did this...
-        # it looks like the og call was in seconds, does poppy want
-        # microseconds? 
-        #image = self.camera.capture(initial_sleep=exposure_time.to(units.second).magnitude, poll=poll.magnitude)
-        exposure_time = initial_sleep.to(units.microseconds)
+    def capture(self, initial_sleep=0.01, poll=0.01, buffer=None, filename=None):
+        """ Get a simulated image capture from the simulator """
+        if self.camera_purpose == 'imaging_camera':
+            hicat.simulators.optics_simulator.detector = 'imager'
+            self.photometry_config_key = 'total_direct_photometry_cts_per_microsec'
+        elif self.camera_purpose == 'pupil_camera':
+            hicat.simulators.optics_simulator.detector = 'pupil_camera'
+            self.photometry_config_key = 'total_pupil_direct_photometry_cts_per_microsec'
+        elif self.camera_purpose == 'phase_retrieval_camera':
+            hicat.simulators.optics_simulator.detector = 'pr_camera'
+            self.photometry_config_key = 'total_prcam_direct_photometry_cts_per_microsec'
+        elif self.camera_purpose == 'zernike_camera':
+            hicat.simulators.optics_simulator.detector = 'zernike_wfs_camera'
+            self.photometry_config_key = 'total_zernike_direct_photometry_cts_per_microsec'
+        else:
+            raise NotImplementedError(f"Unknown camera for simulations: {self.camera_purpose}")
 
         # Here's the actual PSF calculation! Retrieve the simulated image from the optical system mode. This will do a
         # propagation up to the detector plane.
-        self.log.info("HICAT_SIM: Simulating image capture for "+self.camera_purpose)
+        self.log.info(f"HICAT_SIM: Simulating image capture for {self.camera_purpose}")
         image_hdulist = hicat.simulators.optics_simulator.calc_psf(apply_pipeline_binning=False)
         self.log.info("HICAT_SIM: Simulation complete for image.")
         testbed_state._simulation_latest_image = image_hdulist # Save so we can later copy some of the FITS header keywords
 
-        counts_per_microsec = CONFIG_INI.getfloat(
-            'photometry', self.photometry_config_key, fallback=90000)
+        counts_per_microsec = CONFIG_INI.getfloat('photometry', self.photometry_config_key, fallback=90000)
 
         # Adjust count rate to compensate for poppy normalizing to 1.0 in entrance pupil by default.
-        exit_pupil_flux_correction_factor =  CONFIG_INI.getfloat(
-            'data_simulator', 'simulator_direct_exit_vs_entrance_pupil_flux')
+        exit_pupil_flux_correction_factor =  CONFIG_INI.getfloat('data_simulator', 'simulator_direct_exit_vs_entrance_pupil_flux')
         counts_per_microsec /= exit_pupil_flux_correction_factor
 
         # Apply flux normalization and exposure time scaling to the output image
+        exposure_time = self.control_values[self.instrument_lib.ASI_EXPOSURE]
         image = image_hdulist[0].data * counts_per_microsec * exposure_time
 
         return image.astype(np.dtype(np.int32))
@@ -130,7 +125,6 @@ class PoppyZwoEmulator(ZwoASI):
         # I *think* this can just return a dict with milquetoast values for this
         # took said milquetoast values from the __setup_control_values sim
         # function here
-
         return {'MaxWidth': 4096, 'MaxHeight': 4096} 
         pass
 
@@ -145,7 +139,7 @@ class PoppyZwoEmulator(ZwoASI):
         # runs zwolib.ASISetROIFormat(id_, width, height, bins, image_type)
         # set_roi_start_position --> _set_start_position :
         # runs zwolib.ASISetStartPos(id_, start_x, start_y)
-        pass # according to Marshall
+        pass  # according to Marshall
 
 
 class ZwoCamera(SimInstrument,  catkit.hardware.zwo.ZwoCamera.ZwoCamera):
