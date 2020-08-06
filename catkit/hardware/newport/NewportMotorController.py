078c2fdec67e502f48d1e1c30ce2a86ba297f38c
Makidon Lab <makidonlabs@gmail.com>
2020-08-06 00:51:45 -0400

    Update axes labels used in NewportMotorController.__update_testbed_state for the relabeling in HICAT-910. --Marshall

--- catkit/hardware/newport/NewportMotorController.py
+++ catkit/hardware/newport/NewportMotorController.py
@@ -166,9 +166,9 @@ class NewportMotorController(MotorController):
     def __update_testbed_state(self, motorid):
         if self.use_testbed_state:
             position = self.get_position(motorid)
-            if motorid == "motor_lyot_stop_x":
+            if motorid == "motor_lyot_stop_y":
                 ini_value = CONFIG_INI.getfloat(motorid, "in_beam")
                 testbed_state.lyot_stop = True if np.isclose(ini_value, position, atol=.001) else False
-            elif motorid == "motor_FPM_Y":
+            elif motorid == "motor_FPM_X":
                 ini_value = CONFIG_INI.getfloat(motorid, "default_coron")
                 testbed_state.coronograph = True if np.isclose(ini_value, position, atol=.001) else False
