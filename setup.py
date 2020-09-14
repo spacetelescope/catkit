725ea1666813030db39fb7e5c95bb1b71f5ed409
Iva Laginja <iva.laginja@lam.fr>
2020-09-14 15:51:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='catkit',
-      version='0.5.2',
+      version='0.13.0',
       description='Python API wrapping hardware and device drivers for the Optics Lab.',
       url='https://github.com/spacetelescope/instrument-interface-library',
       author='Makidon Optics Lab',
