f76f0bc527524709e2f7a6cf748309cc6426ea39
James Noss <jnoss@stsci.edu>
2020-09-16 15:36:56 -0400

    Bump version from v0.13.0 -> v0.14.0
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='catkit',
-      version='0.13.0',
+      version='0.14.0',
       description='Python API wrapping hardware and device drivers for the Optics Lab.',
       url='https://github.com/spacetelescope/instrument-interface-library',
       author='Makidon Optics Lab',
