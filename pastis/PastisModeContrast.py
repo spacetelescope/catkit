eb042b47e855e57497f0bdf0ac749113bb191301
Iva Laginja <laginja.iva@gmail.com>
2020-09-21 15:46:42 +0200

    add rest of mode contrast experiment; IrisAO stuff missing

--- pastis/PastisModeContrast.py
+++ pastis/PastisModeContrast.py
@@ -1,20 +1,35 @@
+import os
+from astropy.io import fits
+import astropy.units as u
+import numpy as np
+
 from hicat.experiments.pastis.PastisExperiment import PastisExperiment
 from hicat.hardware import testbed_state
+
+from pastis.pastis_analysis import cumulative_contrast_matrix, modes_from_file
+from pastis.plotting import plot_contrast_per_mode, plot_cumulative_contrast_compare_accuracy
 
 
 class PastisModeContrast(PastisExperiment):
 
-    def __init__(self, pastis_modes, individual, probe_filename, dm_map_path, color_filter, nd_direct, nd_coron,
+    name = 'PASTIS Mode Contrast'
+
+    def __init__(self, pastis_results_path, pastis_matrix_path, individual, c_target, probe_filename, dm_map_path, color_filter, nd_direct, nd_coron,
                  num_exposures, exposure_time_coron, exposure_time_direct, auto_expose, file_mode, raw_skip,
                  align_lyot_stop=True, run_ta=True):
         super().__init__(probe_filename, dm_map_path, color_filter, nd_direct, nd_coron, num_exposures,
                          exposure_time_coron, exposure_time_direct, auto_expose, file_mode, raw_skip,
                          align_lyot_stop, run_ta)
 
-        self.pastis_modes = pastis_modes
         self.individual = individual
+        self.c_target = c_target
 
-        self.contrast_list = []
+        # Read PASTIS matrix, modes and mode weights from file
+        self.pastis_modes, self.eigenvalues = modes_from_file(pastis_results_path)
+        self.mode_weights = np.loadtxt(os.path.join(pastis_results_path, 'results', f'mode_requirements_{c_target}_uniform.txt'))
+        self.pastis_matrix = fits.getdata(os.path.join(pastis_matrix_path, 'pastis_matrix.fits'))
+
+        self.measured_contrast = []
 
     def experiment(self):
         # Run flux normalization
@@ -25,5 +40,52 @@ class PastisModeContrast(PastisExperiment):
         self.log.info('Measuring reference PSF (direct) and coronagraph floor')
         self.measure_coronagraph_floor()
 
+        # Target contrast needs to be above contrast floor
+        if self.c_target <= self.coronagraph_floor:
+            raise ValueError(f"Coronagraph floor ({self.coronagraph_floor}) cannot be above target contrast ({self.c_target}).")
+
         # Access testbed devices and set experiment path
         devices = testbed_state.devices.copy()    # TODO: Is this how I will access the IrisDM?
+
+        # Loop over all modes
+        for maxmode in range(self.pastis_modes.shape[0]):
+            initial_path = os.path.join(self.output_path, f'mode_{maxmode}')
+
+            # Apply each mode individually or cumulatively?
+            if self.individual:
+                opd = self.pastis_modes[:, maxmode] * self.mode_weights[maxmode]
+            else:
+                opd = np.nansum(self.pastis_modes[:, :maxmode + 1] * self.mode_weights[:maxmode + 1], axis=1)
+            opd *= u.nm  # the package is currently set up to spit out the modes in units of nm
+
+            # Convert this to IrisAO command
+
+            # Apply this to IrisAO
+
+            # Take coro images
+            pair_image, header = self.take_exposure(devices, 'coron', self.wvln, initial_path, dark_zone_mask=self.dark_zone)
+            pair_image /= self.direct_max
+
+            # Measure mean contrast
+            self.measured_contrast.append(np.mean(pair_image[self.dark_zone]))
+
+        # Save the measured contrasts to file
+        if self.individual:
+            filename = f'individual_mode_contrasts_{self.c_target}.txt'
+        else:
+            filename = f'cumul_contrast_accuracy_{self.c_target}.txt'
+        np.savetxt(os.path.join(self.output_path, filename), self.measured_contrast)
+
+    def post_experiment(self, *args, **kwargs):
+
+        # Calculate contrast from PASTIS propagation
+        self.pastis_contrast = cumulative_contrast_matrix(self.pastis_modes, self.mode_weights, self.pastis_matrix,
+                                                          self.coronagraph_floor, self.individual)
+
+        # Plot the results
+        if self.individual:
+            plot_contrast_per_mode(self.measured_contrast, self.coronagraph_floor, self.c_target,
+                                   nmodes=len(self.pastis_contrast), out_dir=self.output_path, save=True)
+        else:
+            plot_cumulative_contrast_compare_accuracy(self.pastis_contrast, self.measured_contrast,
+                                                      out_dir=self.output_path, c_target=self.c_target, save=True)
