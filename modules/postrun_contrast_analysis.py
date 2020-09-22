0e87696e733570ee53d872eddc622579a0a92407
Makidon Lab <makidonlabs@gmail.com>
2020-09-22 13:35:12 -0400

    'docstring'

--- modules/postrun_contrast_analysis.py
+++ modules/postrun_contrast_analysis.py
@@ -1,7 +1,14 @@
 from hicat.experiments.modules import contrast_statistics
+'''
+Quick Launcher to be run after a strokemin run. This allows users to specify a convergence iteration and generate
+separate plots. Include these plots in the science slides.
 
-filepath = '/Users/mmaclay/hicat_data/simulations/2020-09-17T15-57-35_broadband_stroke_minimization/metrics.csv'
-iteration_of_convergence = 12
+runfolder: type=str, the name of the run's folder eg. 2020-07-15T17-27-04_broadband_stroke_minimization
+iteration_of_convergence, type=int, the iteration after which the contrast appears to have converged.
+'''
 
+runfolder = "2020-07-15T17-27-04_broadband_stroke_minimization"
+iteration_of_convergence = 15
 
+filepath = str("C:/Users/HICAT/hicat_data/"+str(runfolder)+"/metrics.csv")
 contrast_statistics.calculate_confidence_interval(filepath, iteration_of_convergence=iteration_of_convergence)
