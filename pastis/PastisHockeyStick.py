97a38d89dd901df89dc8d5d9560aa6a41346ccb0
Iva Laginja <laginja.iva@gmail.com>
2020-09-23 18:38:09 +0200

    add docstring

--- pastis/PastisHockeyStick.py
+++ pastis/PastisHockeyStick.py
@@ -20,6 +20,26 @@ class PastisHockeyStick(PastisExperiment):
     def __init__(self, rms_range, no_realizations, pastis_matrix_path, probe_filename, dm_map_path, color_filter, nd_direct, nd_coron,
                  num_exposures, exposure_time_coron, exposure_time_direct, auto_expose, file_mode, raw_skip,
                  align_lyot_stop=True, run_ta=True):
+        """
+        Measure the contrast of random WFE maps that are scaled to a range of global WFE rms values.
+        
+        :param rms_range: list or array of global WFE rms calues to measure contrast for
+        :param no_realizations: int, number of realizations of each global WFE rms value requested
+        :param pastis_matrix_path: str, full path to PASTIS matrix, including filename
+        :param probe_filename: str, path to probe file, used only to get DH geometry
+        :param dm_map_path: str, path to folder that contains DH solution
+        :param color_filter: str, wavelength for color flipmount
+        :param nd_direct: str, ND filter choice for direct images
+        :param nd_coron: str, ND filter choice for coronagraphic images
+        :param num_exposures: int, number of exposures for each image acquisition
+        :param exposure_time_coron: float, exposure time for coron mode in microseconds
+        :param exposure_time_direct: float, exposure time for direct mode in microseconds
+        :param auto_expose: bool or {catkit.catkit_types.FpmPosition: bool}, flag to enable auto exposure time correction
+        :param file_mode: bool, If true files will be written to disk otherwise only final results are saved
+        :param raw_skip: int, Skips x writing-files for every one taken. raw_skip=math.inf will skip all and save no raw image files.
+        :param align_lyot_stop: bool, whether to automatically align the Lyot stop before the experiment or not
+        :param run_ta: bool, whether to run target acquisition. Will still just measure TA if False.
+        """
         super().__init__(probe_filename, dm_map_path, color_filter, nd_direct, nd_coron, num_exposures,
                          exposure_time_coron, exposure_time_direct, auto_expose, file_mode, raw_skip,
                          align_lyot_stop, run_ta)
