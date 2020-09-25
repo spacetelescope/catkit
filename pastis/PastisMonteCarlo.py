8eb75a9ba0b37e34150e5f6fb236c7f52adf1aeb
Iva Laginja <laginja.iva@gmail.com>
2020-09-25 14:50:13 +0200

    Add docstring

--- pastis/PastisMonteCarlo.py
+++ pastis/PastisMonteCarlo.py
@@ -16,6 +16,27 @@ class PastisMonteCarlo(PastisExperiment):
     def __init__(self, segments, pastis_results_path, n_repeat, c_target, probe_filename, dm_map_path, color_filter,
                  nd_direct, nd_coron, num_exposures, exposure_time_coron, exposure_time_direct, auto_expose, file_mode,
                  raw_skip, align_lyot_stop=True, run_ta=True):
+        """
+        Run a Monte Carlo analysis on the average contrast in DH, either from random PASTIS mode weights or segment weights.
+
+        :param segments: bool, if True, will draw random segment weights, if False, will draw random mode weights
+        :param pastis_results_path: str, path to the overall PASTIS data directory, without the 'results' at the end
+        :param n_repeat: int, number of random realizations of WFE to be drawn
+        :param c_target: float, target contrast for which the mode weights have been calculated
+        :param probe_filename: str, path to probe file, used only to get DH geometry
+        :param dm_map_path: str, path to folder that contains DH solution
+        :param color_filter: str, wavelength for color flipmount
+        :param nd_direct: str, ND filter choice for direct images
+        :param nd_coron: str, ND filter choice for coronagraphic images
+        :param num_exposures: int, number of exposures for each image acquisition
+        :param exposure_time_coron: float, exposure time for coron mode in microseconds
+        :param exposure_time_direct: float, exposure time for direct mode in microseconds
+        :param auto_expose: bool or {catkit.catkit_types.FpmPosition: bool}, flag to enable auto exposure time correction
+        :param file_mode: bool, If true files will be written to disk otherwise only final results are saved
+        :param raw_skip: int, Skips x writing-files for every one taken. raw_skip=math.inf will skip all and save no raw image files.
+        :param align_lyot_stop: bool, whether to automatically align the Lyot stop before the experiment or not
+        :param run_ta: bool, whether to run target acquisition. Will still just measure TA if False.
+        """
 
         if segments:
             self.name = 'PASTIS Monte Carlo Segments'
