2ca6016cb38135c7433047f770c39d081062a898
Iva Laginja <laginja.iva@gmail.com>
2020-09-25 13:55:20 +0200

    adjust MC experiment to be able to run both on modes and segments

--- pastis/PastisMonteCarloModes.py
+++ pastis/PastisMonteCarloModes.py
@@ -13,28 +13,41 @@ from pastis.plotting import plot_monte_carlo_simulation
 
 class PastisMonteCarloModes(PastisExperiment):
 
-    name = "PASTIS Monte Carlo Modes"
-
-    def __init__(self, pastis_results_path, n_repeat, c_target, probe_filename, dm_map_path, color_filter, nd_direct, nd_coron,
+    def __init__(self, segments, pastis_results_path, n_repeat, c_target, probe_filename, dm_map_path, color_filter, nd_direct, nd_coron,
                  num_exposures, exposure_time_coron, exposure_time_direct, auto_expose, file_mode, raw_skip,
                  align_lyot_stop=True, run_ta=True):
+
+        if segments:
+            self.name = 'PASTIS Monte Carlo Modes'
+        else:
+            self.name = 'PASTIS Monte Carlo Segments'
 
         super().__init__(probe_filename, dm_map_path, color_filter, nd_direct, nd_coron, num_exposures,
                          exposure_time_coron, exposure_time_direct, auto_expose, file_mode, raw_skip,
                          align_lyot_stop, run_ta)
 
+        self.segments = segments
+        if segments:
+            self.log.info('Working on MC for SEGMENTS.')
+        else:
+            self.log.info('Working on MC for MODES.')
         self.n_repeat = n_repeat
         self.c_target = c_target
         self.log.info(f'Target contrast: {c_target}')
         self.log.info(f'Will run {n_repeat} iterations.')
 
-        # Read PASTIS matrix, modes and mode weights from file
-        self.pastis_modes, self.eigenvalues = modes_from_file(pastis_results_path)
-        self.mode_weights = np.loadtxt(os.path.join(pastis_results_path, 'results', f'mode_requirements_{c_target}_uniform.txt'))
-        self.log.info(f'PASTIS modes and mode weights read from {pastis_results_path}')
+        # Read PASTIS matrix, modes and mode/segment weights from file
+        if self.segments:
+            self.pastis_modes, self.eigenvalues = modes_from_file(pastis_results_path)
+            self.mode_weights = np.loadtxt(os.path.join(pastis_results_path, 'results', f'mode_requirements_{c_target}_uniform.txt'))
+            self.log.info(f'PASTIS modes and mode weights read from {pastis_results_path}')
+        else:
+            self.segment_weights = np.loadtxt(os.path.join(pastis_results_path, f'segment_requirements_{c_target}.txt'))
+            self.segment_weights *= u.nm
+            self.log.info(f'Segment weights read from {pastis_results_path}')
 
         self.measured_contrast = []
-        self.random_mode_weight_sets = []
+        self.random_weights = []
 
     def experiment(self):
 
@@ -59,30 +72,42 @@ class PastisMonteCarloModes(PastisExperiment):
         # Instantiate a connection to the IrisAO
         iris_dm = pastis_functions.IrisAO()
 
-        # Loop over all modes
+        # Loop over all modes/segments
         for rep in range(self.n_repeat):
-            self.log.info(f'Random mode set realization {rep + 1}/{self.n_repeat}')
 
-            # Create a random set of mode weights
-            modes_random_state = np.random.RandomState()
-            rand = modes_random_state.normal(0, 1, self.nb_seg)
-            random_weights = self.mode_weights * rand
-            self.random_mode_weight_sets.append(random_weights)
+            # Define a numpy random state
+            mc_random_state = np.random.RandomState()
 
-            # Sum up all modes with randomly scaled sigmas to make total OPD
-            opd = np.nansum(self.pastis_modes[:, :] * random_weights, axis=1)
-            opd *= u.nm
+            if self.segments:
+                self.log.info(f'Random segment set realization {rep + 1}/{self.n_repeat}')
+
+                # Draw a normal distribution for each segment, where the stddevs are the segment weights
+                random_opd = mc_random_state.normal(0, self.segment_weights) * u.nm
+                self.random_weights.append(random_opd)
+
+            else:
+                self.log.info(f'Random mode set realization {rep + 1}/{self.n_repeat}')
+
+                # Draw a normal distribution for each mode, where the stddevs are the mode weights
+                random_weights = mc_random_state.normal(0, self.mode_weights)
+                self.random_weights.append(random_weights)
+
+                # Sum up all modes with randomly scaled weights to make total random OPD
+                random_opd = np.nansum(self.pastis_modes[:, :] * random_weights, axis=1)
+                random_opd *= u.nm
 
             # Convert this to IrisAO command - a list of 37 tuples of 3 (PTT)
             # TODO: make it such that we can pick between piston, tip and tilt (will require extra keyword "zernike")
             command_list = []
             for seg in range(self.nb_seg):
-                command_list.append((opd[seg], 0, 0))
-            #opd_command = segmented_dm_command.load_command(command_list, apply_flat_map=True, dm_config_id='iris_ao')
-            opd_command = None
+                command_list.append((random_opd[seg], 0, 0))
+            #random_opd_command = segmented_dm_command.load_command(command_list, apply_flat_map=True, dm_config_id='iris_ao')
+            random_opd_command = None
 
             # Apply this to IrisAO
-            iris_dm.apply_shape(opd_command)
+            iris_dm.apply_shape(random_opd_command)
+
+            # TODO: save random OPD command on IrisAO as WFE map
 
             # Take coro images
             pair_image, header = self.take_exposure(devices, 'coron', self.wvln, initial_path,
@@ -92,21 +117,27 @@ class PastisMonteCarloModes(PastisExperiment):
             # Measure mean contrast
             self.measured_contrast.append(np.mean(pair_image[self.dark_zone]))
 
-        # Save the measured contrasts and random mode sets to file
-        np.savetxt(os.path.join(self.output_path, f'mc_modes_contrasts_{self.c_target}.txt'), self.measured_contrast)
-        np.savetxt(os.path.join(self.output_path, f'mc_mode_reqs_{self.c_target}.txt'), self.random_mode_weight_sets)
+        # Save the measured contrasts and random weights to file
+        if self.segments:
+            filename_contrasts = f'mc_segments_contrasts_{self.c_target}.txt'
+            filename_weights = f'mc_segment_req_maps_{self.c_target}.txt'
+        else:
+            filename_contrasts = f'mc_modes_contrasts_{self.c_target}.txt'
+            filename_weights = f'mc_mode_reqs_{self.c_target}.txt'
+        np.savetxt(os.path.join(self.output_path, filename_contrasts), self.measured_contrast)
+        np.savetxt(os.path.join(self.output_path, filename_weights), self.random_weights)
 
     def post_experiment(self, *args, **kwargs):
 
         # Calculate the empirical mean and standard deviation of the distribution
-        mean_modes = np.mean(self.measured_contrast)
-        stddev_modes = np.std(self.measured_contrast)
-        self.log.info(f'Mean of the Monte Carlo result modes: {mean_modes}')
-        self.log.info(f'Standard deviation of the Monte Carlo result modes: {stddev_modes}')
+        mean_empirical = np.mean(self.measured_contrast)
+        stddev_empirical = np.std(self.measured_contrast)
+        self.log.info(f'Mean of the Monte Carlo result: {mean_empirical}')
+        self.log.info(f'Standard deviation of the Monte Carlo result: {stddev_empirical}')
         with open(os.path.join(self.output_path, f'statistical_contrast_empirical_{self.c_target}.txt'), 'w') as file:
-            file.write(f'Empirical, statistical mean: {mean_modes}')
-            file.write(f'\nEmpirical variance: {stddev_modes**2}')
+            file.write(f'Empirical, statistical mean: {mean_empirical}')
+            file.write(f'\nEmpirical variance: {stddev_empirical**2}')
 
         # Plot histogram
         plot_monte_carlo_simulation(self.measured_contrast, out_dir=self.output_path, c_target=self.c_target,
-                                    segments=False, stddev=stddev_modes, save=True)
+                                    segments=self.segments, stddev=stddev_empirical, save=True)
