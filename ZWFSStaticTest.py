b29cc3aa554e76ae8d2355cc152ee1a4b8fac152
Raphaël P <raphael.pourcelot@oca.eu>
2020-10-02 17:30:57 +0200

    Corrected recentering issue. Working test with F shape for orientation

--- ZWFSStaticTest.py
+++ ZWFSStaticTest.py
@@ -2,9 +2,11 @@ from hicat.experiments.Experiment import HicatExperiment
 from hicat.control import zwfs
 import logging
 
+import numpy as np
+from astropy.io import fits
 
 class ZWFSStaticTest(HicatExperiment):
-    name = "Take ZWFS single measurement"
+    name = "Take ZWFS static test"
     log = logging.getLogger(__name__)
 
     def __init__(self,
@@ -31,8 +33,19 @@ class ZWFSStaticTest(HicatExperiment):
 
     def experiment(self):
 
+        # Define F shape for DM1
+        f_shape = np.zeros((34, 34))
+        f_shape[5:29, 12:16] = 1
+        f_shape[5:9, 12:26] = 1
+        f_shape[16:20, 12:23] = 1
+
+        f_shape *= 1e-8
+
         zernike_sensor = zwfs.ZWFS(self.instrument)
         zernike_sensor.calibrate(output_path=self.output_path)
-        zernike_sensor.make_reference_opd(self.wave)
-        zopd = zernike_sensor.perform_zwfs_measurement(self.wave, output_path=self.output_path, differential=True)
-        zernike_sensor.save_list(zopd, 'ZWFS_OPD', self.output_path)
+        zernike_sensor.make_reference_opd(self.wave, dm1_shape=f_shape)
+        zopd = zernike_sensor.perform_zwfs_measurement(self.wave, output_path=self.output_path,
+                                                       differential=True, dm1_shape=f_shape)
+
+        zernike_sensor.save_list(zopd, 'ZWFS_F_opd_differential', self.output_path)
+
