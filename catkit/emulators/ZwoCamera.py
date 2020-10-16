c726268ccc0e05e2c9c9b97369c62cbd826e2917
James Noss <jnoss@stsci.edu>
2020-10-16 16:58:09 -0400

    Move stuff back to hicat
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/emulators/ZwoCamera.py
+++ catkit/emulators/ZwoCamera.py
@@ -1,15 +1,9 @@
-import copy
+import abc
 import logging
 
-import numpy as np
-
 import zwoasi
-
 from hicat.config import CONFIG_INI, CONFIG_MODES
-from hicat.hardware import testbed_state
-import hicat.simulators
 
-from catkit.interfaces.Instrument import SimInstrument
 import catkit.hardware.zwo.ZwoCamera
 
 
@@ -17,14 +11,10 @@ import catkit.hardware.zwo.ZwoCamera
 ZwoASI = type("ZwoASI", (), zwoasi.__dict__)
 
 
-class PoppyZwoEmulator(ZwoASI):
-    """ Class to emulate *only our usage* of the zwoasi library. """
+class ZwoEmulator(ZwoASI):
+    """ Class to emulate of the zwoasi library. """
 
-    implemented_camera_purposes = ("imaging_camera",
-                                   "phase_retrieval_camera",
-                                   "pupil_camera",
-                                   "target_acquisition_camera",
-                                   "zernike_wfs_camera")
+    implemented_camera_purposes = None
 
     @classmethod
     def get_camera_mappings(cls):
@@ -42,51 +32,12 @@ class PoppyZwoEmulator(ZwoASI):
         self.config_id = config_id
         self.image_type = None
         self.control_values = {}
-        self.camera_mappings = self.get_camera_mappings()
-        self.photometry_config_key = None
 
+        self.camera_mappings = self.get_camera_mappings()
         if self.config_id not in self.camera_mappings:
             raise ValueError(f"Unknown camera for simulations: {self.config_id}")
 
         self.camera_purpose = self.camera_mappings[self.config_id]["purpose"]
-
-        # helper function for repetitive config flag retrieval:
-        def get_camera_config_flag(label):
-            return CONFIG_INI.getboolean('data_simulator',
-                                         f'simulate_{self.camera_purpose}_{label}',
-                                         fallback=False)
-
-        self.simulate_background = get_camera_config_flag('background')
-        self.simulate_photon_noise = get_camera_config_flag('photon_noise')
-        self.simulate_read_noise = get_camera_config_flag('read_noise')
-        self.simulate_image_jitter = get_camera_config_flag('image_jitter')
-        self.simulate_readout_quantization = get_camera_config_flag('readout_quantization')
-
-        self.simulate_background_rate = CONFIG_INI.getfloat('data_simulator',
-                                                            f'simulate_{self.camera_purpose}_background_counts_per_microsec',
-                                                            fallback=0.0)
-        self.simulate_jitter_sigma = CONFIG_INI.getfloat('data_simulator',
-                                                         f'simulate_{self.camera_purpose}_jitter_sigma_pixels',
-                                                         fallback=0.0)
-        if self.simulate_photon_noise or self.simulate_read_noise:
-            gain_db = CONFIG_INI.getint(self.config_id, 'gain')
-            if gain_db != 0:
-                raise NotImplementedError(
-                    "SimZwoCamera noise model does not yet support camera gain settings other than 0 db")
-            else:
-                # Values from https://astronomy-imaging-camera.com/manuals/ASI178%20Manual%20EN.pdf section 5 plots
-                self.gain_e_per_count = 0.9 / 4  # electrons/ADU, from ZWO documentation for ASI178.
-                self.readnoise_counts = 2.2 * 4  # readnoise in ADU
-                # Note, factor of 4 is for 14 bit readout into 16 bits, skipping lowest 2 bits
-        else:
-            self.gain_e_per_count = self.readnoise_counts = 0
-
-        self.log.info(f"Simulated {self.camera_purpose} noise model: Background {self.simulate_background}; "
-                      f"photon noise {self.simulate_photon_noise}, gain {self.gain_e_per_count} e-/count; "
-                      f"read noise {self.simulate_read_noise}, {self.readnoise_counts} counts; "
-                      f"jitter {self.simulate_image_jitter}, {self.simulate_jitter_sigma} pixels")
-        self._reuse_cached_psf_prior_to_adding_noise = False
-        self._force_reuse_cached_psf_prior_to_adding_noise = False  # external override option, used for TA camera efficient sims path
 
     def init(self, library_file=None):
         pass
@@ -124,106 +75,9 @@ class PoppyZwoEmulator(ZwoASI):
     def set_image_type(self, image_type):
         self.image_type = image_type
 
-    @staticmethod
-    def reuse_psf(pre_call_reuse, post_call_reuse):
-        def decorator(func):
-            def wrapper(self, *args, **kwargs):
-                # Simulation of multiple noisy exposures can take a shortcut, only performing the optical
-                # propagation sim once, and then just adding different noise realizations each subsequent time.
-                # To have this work properly, we have to flag here that the images after the first should
-                # reuse the prior fourier optics sim.
-
-                if pre_call_reuse is not None:
-                    if isinstance(self, PoppyZwoEmulator):
-                        self._reuse_cached_psf_prior_to_adding_noise = pre_call_reuse
-                    elif isinstance(self, ZwoCamera):
-                        self.instrument_lib._reuse_cached_psf_prior_to_adding_noise = pre_call_reuse
-                    else:
-                        raise NotImplementedError
-
-                ret = func(self, *args, **kwargs)
-
-                if post_call_reuse is not None:
-                    if isinstance(self, PoppyZwoEmulator):
-                        self._reuse_cached_psf_prior_to_adding_noise = post_call_reuse
-                    elif isinstance(self, ZwoCamera):
-                        self.instrument_lib._reuse_cached_psf_prior_to_adding_noise = post_call_reuse
-                    else:
-                        raise NotImplementedError
-
-                return ret
-            return wrapper
-        return decorator
-
-    @reuse_psf.__func__(pre_call_reuse=None, post_call_reuse=True)
+    @abc.abstractmethod
     def capture(self, initial_sleep=0.01, poll=0.01, buffer=None, filename=None):
-        """ Get a simulated image capture from the simulator """
-        if self.camera_purpose == 'imaging_camera':
-            hicat.simulators.optics_simulator.detector = 'imager'
-            self.photometry_config_key = 'total_direct_photometry_cts_per_microsec'
-        elif self.camera_purpose == 'target_acquisition_camera':
-            hicat.simulators.optics_simulator.detector = 'target_acquisition_camera'
-            self.photometry_config_key = 'total_direct_photometry_cts_per_microsec'
-        elif self.camera_purpose == 'pupil_camera':
-            hicat.simulators.optics_simulator.detector = 'pupil_camera'
-            self.photometry_config_key = 'total_pupil_direct_photometry_cts_per_microsec'
-        elif self.camera_purpose == 'phase_retrieval_camera':
-            hicat.simulators.optics_simulator.detector = 'pr_camera'
-            self.photometry_config_key = 'total_prcam_direct_photometry_cts_per_microsec'
-        elif self.camera_purpose == 'zernike_wfs_camera':
-            hicat.simulators.optics_simulator.detector = 'zernike_wfs_camera'
-            self.photometry_config_key = 'total_zernike_direct_photometry_cts_per_microsec'
-        else:
-            raise NotImplementedError(f"Unknown camera for simulations: {self.camera_purpose}")
-
-        # Here's the actual PSF calculation! Retrieve the simulated image from the optical system mode. This will do a
-        # propagation up to the detector plane.
-        if self._reuse_cached_psf_prior_to_adding_noise or self._force_reuse_cached_psf_prior_to_adding_noise:
-            self.log.info("HICAT_SIM: Reusing cached prior sim; adding independent noise realization.")
-            image_hdulist = copy.deepcopy(testbed_state._simulation_latest_image)
-        else:
-            image_hdulist = hicat.simulators.optics_simulator.calc_psf(apply_pipeline_binning=False)
-            self.log.info("HICAT_SIM: Simulation complete for image.")
-            testbed_state._simulation_latest_image = image_hdulist  # Save so we can later copy some of the FITS header keywords
-            # or reuse, for efficient simulation of multiple exposures
-
-        counts_per_microsec = CONFIG_INI.getfloat('photometry', self.photometry_config_key, fallback=90000)
-
-        # Adjust count rate to compensate for poppy normalizing to 1.0 in entrance pupil by default.
-        exit_pupil_flux_correction_factor = CONFIG_MODES.getfloat(
-            testbed_state.current_mode, 'simulator_direct_exit_vs_entrance_pupil_flux')
-        counts_per_microsec /= exit_pupil_flux_correction_factor
-
-        # Apply flux normalization and exposure time scaling to the output image
-        exposure_time = self.control_values[self.ASI_EXPOSURE]
-        image = image_hdulist[0].data * counts_per_microsec * exposure_time
-
-        ### Noise Simulations (Optional)
-        # Noise terms may be turned on or off individually using config settings.
-        sim_zwo_random_state = np.random.RandomState()
-
-        if self.simulate_background:
-            image += self.simulate_background_rate * exposure_time
-        if self.simulate_photon_noise:
-            image = sim_zwo_random_state.poisson(image * self.gain_e_per_count) / self.gain_e_per_count
-        if self.simulate_read_noise:
-            image += sim_zwo_random_state.randn(*image.shape) * self.readnoise_counts
-        if self.simulate_image_jitter:
-            # Simulate image jitter. For now just with integer pixel shifts so this is fast.
-            # FIXME get actual statistics on what the jitter is. This is a complete handwave!
-            shift_vec = sim_zwo_random_state.normal(self.simulate_jitter_sigma, size=2).round().astype(int)
-            image = hicat.cross_correlation.roll_image(image, shift_vec)
-
-        if self.simulate_readout_quantization:
-            # Note, this camera writes 14 bits readout into 16, leaving the lowest two bits always 0, such that the minimum step is 4 counts
-            # Simulate camera saturation: clip maximum value to the largest possible 16 bit unsigned number
-            image = np.clip(image.astype(np.int32), a_min=0, a_max=(2 ** 16 - 1))
-
-            # Simulate quantization. i.e. the last two bits are always 0. Simulate this by bitwise masking out last two bits here
-            image = image.astype(np.int32) & ~np.int32(1) & ~np.int32(2)
-
-        # cast return to float32, consistent with cast for ZwoCamera
-        return image.astype(np.float32)
+        pass
 
     def close(self):
         pass
@@ -250,25 +104,3 @@ class PoppyZwoEmulator(ZwoASI):
         # runs zwolib.ASISetStartPos(id_, start_x, start_y)
         pass  # according to Marshall
 
-
-class ZwoCamera(SimInstrument,  catkit.hardware.zwo.ZwoCamera.ZwoCamera):
-
-    instrument_lib = PoppyZwoEmulator
-
-    @classmethod
-    def load_asi_lib(cls):
-        pass
-
-    @PoppyZwoEmulator.reuse_psf(pre_call_reuse=False, post_call_reuse=False)
-    def just_take_exposures(self, exposure_time, num_exposures,
-                            extra_metadata=None,
-                            subarray_x=None, subarray_y=None, width=None, height=None, gain=None, full_image=None,
-                            bins=None):
-        return super().just_take_exposures(exposure_time=exposure_time,
-                                           num_exposures=num_exposures,
-                                           extra_metadata=extra_metadata,
-                                           subarray_x=subarray_x, subarray_y=subarray_y,
-                                           width=width, height=height,
-                                           gain=gain,
-                                           full_image=full_image,
-                                           bins=bins)
