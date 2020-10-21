fe9f0d9f78922f32c904e54998ecb144f8383ed3
James Noss <jnoss@stsci.edu>
2020-10-21 13:58:28 -0400

    nanometers -> nanometer
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/hardware/boston/commands.py
+++ catkit/hardware/boston/commands.py
@@ -39,7 +39,7 @@ def flat_command(bias=False,
         return dm_command_object
 
 
-def poke_command(actuators, amplitude=quantity(700, units.nanometers), bias=False,
+def poke_command(actuators, amplitude=quantity(700, units.nanometer), bias=False,
                  flat_map=True, return_shortname=False, dm_num=1):
     """
     Creates a DmCommand object that pokes actuators at a given amplitude.
@@ -81,7 +81,7 @@ def poke_command(actuators, amplitude=quantity(700, units.nanometers), bias=Fals
         return dm_command_object
 
 
-def poke_letter_f_command(amplitude=quantity(250, units.nanometers), bias=False, flat_map=True, dm_num=1):
+def poke_letter_f_command(amplitude=quantity(250, units.nanometer), bias=False, flat_map=True, dm_num=1):
     """
     Creates the letter F in normal orientation when viewed in DS9.
     """
@@ -104,7 +104,7 @@ def poke_letter_f_command(amplitude=quantity(250, units.nanometers), bias=False,
     return DmCommand(data, dm_num, flat_map=flat_map, bias=bias)
 
 
-def checkerboard_command(amplitude=quantity(250, units.nanometers), bias=False, flat_map=True,
+def checkerboard_command(amplitude=quantity(250, units.nanometer), bias=False, flat_map=True,
                          dm_num=1, offset_x=0, offset_y=3, step=4):
     """
     Creates a checkerboard pattern DM command.  Useful for phase retrieval or 4D images. Default values
