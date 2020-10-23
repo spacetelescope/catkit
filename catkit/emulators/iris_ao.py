e294a95641fc4edd75dbfeebe209f646969bd799
Iva Laginja <laginja.iva@gmail.com>
2020-10-23 14:37:05 +0200

    rework the iris_ao emulator with closer to how I think it actually works

--- catkit/emulators/iris_ao.py
+++ catkit/emulators/iris_ao.py
@@ -10,106 +10,79 @@ from catkit.interfaces.Instrument import SimInstrument
 
 
 class PoppyIrisAO(poppy.dms.HexSegmentedDeformableMirror):
+    """
+    Adding attributes to describe a typical IrisAO deformable mirror.
+    TODO: drop this one into hicat_sim.py
+    """
     def __init__(self, **super_kwargs):
 
-        # TODO: do the thing where we use the negative flatmap to simulate the non-flattened DM surface
+        # TODO: do the thing where we use the negative flatmap to simulate the non-flattened DM surface?
 
         super().__init__(**super_kwargs)
 
 
 class FakeStdin:
     """Because maybe this will work"""
-    def __init__(self):
-        self.stdin = FakeWriteAndFlush()
+    def __init__(self, iris_dm, config_id, filename_ptt_dm):
+        self.iris_dm = iris_dm
+        self.config_id = config_id
+        self.filename_ptt_dm = filename_ptt_dm
+
+        self.stdin = FakeWriteAndFlush(iris_dm, config_id, filename_ptt_dm)
 
 
 class FakeWriteAndFlush:
+
+    def __init__(self, iris_dm, config_id, filename_ptt_dm):
+        self.iris_dm = iris_dm
+        self.config_id = config_id
+        self.filename_ptt_dm = filename_ptt_dm
+
     def write(self, string):
-        pass
+        """
+        There are two options:
+        b'config\n': which applies a command to the IrisAO
+        b'quit\n': closes the connection to the IrisAO
+        """
+
+        if string == b'config\n':
+            # Read ConfigPTT.ini
+            number_of_segments = util.iris_num_segments(self.config_id)
+            data = util.read_ini(self.filename_ptt_dm, number_of_segments)   # returns a dict, the command to be sent to the DM
+
+            # Put it on the iris_dm
+            # Setting the simulated IrisAO means setting each actuator individually
+            for seg, values in data.items():
+                self.iris_dm.set_actuator(seg-1, values[0]*u.um, values[1]*u.mrad, values[2]*u.mrad)   #TODO: double-check the -1 here, meant to correct for different segment names
+
+        elif string == b'quit\n':
+            # When we're done, leave the simulated DMs in a flat state, to avoid persistent
+            # state between different simulation calls.
+            # This intentionally differs from hardware behavior in which an unpowered DM is non-flat.
+            self.iris_dm.flatten()
+            self._close_iris_controller_testbed_state()
+
+    @staticmethod
+    def _close_iris_controller_testbed_state():
+        testbed_state.iris_command_object = None
 
     def flush(self):
+        """ This doesn't need to do anything when in simulation, but we need to have it."""
         pass
 
 
 class PoppyIrisaoEmulator:
 
-    def __init__(self, iris_dm):
+    def __init__(self, iris_dm, config_id, filename_ptt_dm):
         self.log = logging.getLogger(f"{self.__module__}.{self.__class__.__qualname__}")
         self.iris_dm = iris_dm
-        self.command = None
+        self.config_id = config_id
+        self.filename_ptt_dm = filename_ptt_dm
 
     def Popen(self, cmd, stdin, stdout, stderr, cwd, bufsize, creationflags):
         """ Something something, open a connection?"""
-        self.instrument = FakeStdin()
+        self.instrument = FakeStdin(self.iris_dm, self.config_id, self.filename_ptt_dm)
         return self.instrument
-
-    def send_data(self, data):
-        """
-        Setting the simulated IrisAO means setting each actuator individually
-
-        :param data: dict, the command to be sent to the DM
-        """
-        for seg, values in data.items():
-            self.iris_dm.set_actuator(seg, values[0]*u.um, values[1]*u.mrad, values[2]*u.mrad)
-
-    def zero(self, return_zeros=False):
-        """Put zeros on the DM. This does not correspond to a flattened DM.
-
-        :return: If return_zeros=True, return a dictionary of zeros
-        """
-        zero_list = util.create_zero_list(util.iris_num_segments(self.config_id))
-        zeros = util.create_dict_from_list(zero_list, util.iris_pupil_naming(self.config_id))
-        self.send_data(zeros)
-
-        # Update the testbed state
-        self._update_iris_state(zeros)
-
-        if return_zeros:
-            return zeros
-
-    def _close(self):
-        """ Close connection to (simulated) DM hardware """
-        # When we're done, leave the simulated DMs in a flat state, to avoid persistent
-        # state between different simulation calls.
-        # This intentionally differs from hardware behavior in which an unpowered DM is non-flat.
-        self.iris_dm.flatten()
-        self._close_iris_controller_testbed_state()
-
-    def apply_shape(self, dm_shape, dm_num=1):
-        """
-        Apply a command object to the Iris AO after adding the flatmap from the configfile.
-        The units of said SegmentedDmCommand object are mrad for tip/tilt, um for piston.
-
-        :param dm_shape: instance of SegmentedDmCommand class
-        :param dm_num: int, this must always be 1 since only one DM can be controlled
-                       with this controller.
-        """
-        if dm_num != 1:
-            raise NotImplementedError("You can only control one Iris AO at a time")
-
-        # Use DmCommand class to format the single command correctly.
-        command = dm_shape.to_command()
-
-        # Send array to DM.
-        self.send_data(command)
-
-        # Update the dm_command class attribute.
-        self.command = command
-
-        # Update the testbed_state.
-        self._update_iris_state(dm_shape)
-
-    def apply_shape_to_both(self, dm1_shape=None, dm2_shape=None):
-        """Method only used by the BostonDmController"""
-        raise NotImplementedError("apply_shape_to_both is not implemented for the Iris AO")
-
-    @staticmethod
-    def _update_iris_state(command_object):
-        testbed_state.iris_command_object = command_object
-
-    @staticmethod
-    def _close_iris_controller_testbed_state():
-        testbed_state.iris_command_object = None
 
 
 class PoppyIrisAOController(SimInstrument, IrisAoDmController):
