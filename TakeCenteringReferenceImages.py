76c239b2031672b6469f2c6c408d8c0e1c8b40ca
Marshall Perrin <mperrin@stsci.edu>
2020-10-26 14:26:42 -0400

    improve centering of reference image creation in sim mode

--- TakeCenteringReferenceImages.py
+++ TakeCenteringReferenceImages.py
@@ -1,4 +1,8 @@
+import os
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import astropy.io.fits as fits
 
 from catkit.catkit_types import ImageCentering, units, quantity
 from catkit.hardware.boston.commands import flat_command
@@ -6,6 +10,7 @@ from hicat.experiments.Experiment import Experiment
 from hicat.experiments.modules.general import take_exposures
 from hicat.hardware import testbed_state
 from hicat.hardware.testbed import move_filter
+from hicat.config import CONFIG_INI
 
 
 class TakeCenteringReferenceImages(Experiment):
@@ -58,12 +63,46 @@ class TakeCenteringReferenceImages(Experiment):
 
     def experiment(self):
 
-        # Pick centering method based on coronagraph mode
-        if testbed_state.current_mode == 'clc2':
-            centering_method = ImageCentering.satellite_spots
-        if testbed_state.current_mode == 'aplc_v2':
-            centering_method = ImageCentering.custom_apodizer_spots
-        self.log.info(f'Centering method for {testbed_state.current_mode} is {centering_method}')
+        # Pick pipeline centering method based on coronagraph mode, and if in real hardware or not
+        if testbed_state.simulation:
+            # In simulated mode, we will create perfectly centered images by construction
+            # This involves overriding whatever's currently in the config to disable WFE and jitter
+            # This is not at all realistic to the hardware, intentionally.
+            default_values = {}
+            def override_value(key, newval):
+                """ Override one value temporarily"""
+                default_values[key] = CONFIG_INI.get('data_simulator', key)
+                CONFIG_INI.set('data_simulator', key, str(newval))
+            def un_override_values():
+                """ Undo all overrides"""
+                for key in default_values:
+                    CONFIG_INI.set('data_simulator', key, default_values[key])
+            def toggle_simulator_to_reread_config():
+                """ re-init simulator after a config change"""
+                import hicat.simulators
+                hicat.simulators.set_testbed_simulation_mode(False)
+                hicat.simulators.set_testbed_simulation_mode(True)
+
+            override_value('simulate_imaging_camera_image_jitter', False)
+            override_value('power_spectrum_rms_nm', 0)
+            override_value('wfe_tilt_nm', 0)
+            override_value('wfe_focus_nm', 0)
+            override_value('wfe_astig_nm', 0)
+            override_value('wfe_coma_nm', 0)
+            override_value('wfe_trefoil_nm', 0)
+            toggle_simulator_to_reread_config()
+            self.log.info("DISABLING simulated image jitter and WFE")
+
+            centering_method = ImageCentering.off
+            self.log.info(f'Centering method for SIMULATED {testbed_state.current_mode} is {centering_method}')
+        else:
+            # For the actual images on the hardware, we want to make these as precisely centered as possible, so they
+            # can then serve as the references for what a centered image should look like. TBD how best to do this.
+            if testbed_state.current_mode == 'clc2':
+                centering_method = ImageCentering.satellite_spots
+            if testbed_state.current_mode == 'aplc_v2':
+                centering_method = ImageCentering.custom_apodizer_spots
+            self.log.info(f'Centering method for {testbed_state.current_mode} is {centering_method}')
 
         # Set wavelength filter AND set ND filter - this assumes we use the same ND filter for the coro images in all wavelengths
         move_filter(wavelength=int(np.rint(self.color_filter)), nd=self.nd_filter)
@@ -85,3 +124,39 @@ class TakeCenteringReferenceImages(Experiment):
                        auto_expose=self.auto_expose,
                        centering=centering_method,
                        **self.kwargs)
+
+
+        if testbed_state.simulation:
+            # if we're in simulation mode, we added overrides above.
+            # Undo that here to avoid unintentional consequences for any subsequent simulated experiments.
+            un_override_values()
+            self.log.info("Reset simualted image jitter and WFE back to prior levels")
+            toggle_simulator_to_reread_config()
+
+        # Now make a plot to verify the centering is OK
+
+        fig, axes = plt.subplots(figsize=(11,7), ncols=2)
+        im = fits.getdata(os.path.join(self.output_path, 'coron/coron_image_cal.fits'))
+        norm = matplotlib.colors.LogNorm(1, im.max() )
+        cm = matplotlib.cm.get_cmap('viridis')
+        cm.set_bad(cm(0))
+
+        axes[0].imshow(im, norm=norm, cmap=cm)
+        axes[0].set_title(f"New centering image for {self.color_filter}, {'Simulated' if testbed_state.simulation else 'Measured'}")
+
+        axes[1].imshow(im, norm=norm, cmap=cm)
+        axes[1].set_title(f"New centering image for {self.color_filter}, zoom to center")
+        cen = (im.shape[0]-1)/2
+        boxsize=10
+        axes[1].set_xlim(cen-boxsize, cen+boxsize)
+        axes[1].set_ylim(cen-boxsize, cen+boxsize)
+        for ax in axes:
+            ax.axhline(cen, ls='--',color='orange')
+            ax.axvline(cen, ls='--',color='orange')
+        plt.text(0.1, 0.1, self.output_path, color='gray', transform=fig.transFigure)
+        plt.savefig(os.path.join(self.output_path, f"centering_reference_check_{self.color_filter}.pdf"))
+
+
+
+if __name__ == "__main__":
+    TakeCenteringReferenceImages().start()
