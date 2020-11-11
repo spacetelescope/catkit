f5ae88b1b3a74df076b59a05a3b2954a87af51a9
Iva Laginja <laginja.iva@gmail.com>
2020-11-11 21:52:56 +0100

    apply flatmaps to all DMs with same launcher

--- ApplyFlatMap.py
+++ ApplyFlatMap.py
@@ -2,7 +2,8 @@ import logging
 import time
 
 from hicat.experiments.Experiment import Experiment
-from catkit.hardware.boston.commands import flat_command
+from hicat.experiments.modules.iris_ao import flat_command as flat_command_iris
+from catkit.hardware.boston.commands import flat_command as flat_command_bostons
 from hicat.hardware import testbed
 
 
@@ -11,34 +12,42 @@ class ApplyFlatMap(Experiment):
     log = logging.getLogger(__name__)
 
     def __init__(self,
-                 dm1_command_object=flat_command(bias=False, flat_map=True),  # Default flat with bias.
-                 dm2_command_object=flat_command(bias=False, flat_map=True),  # Default flat with bias.
+                 dm1_command_object=flat_command_bostons(bias=False, flat_map=True),  # Default flat with bias.
+                 dm2_command_object=flat_command_bostons(bias=False, flat_map=True),  # Default flat with bias.
+                 iris_ao_command_object=flat_command_iris(),  # Default custom flat map.
                  output_path=None,
                  suffix='apply_flat_map',
                  timeout=600,
                  **kwargs
                  ):
         """
-        Takes a set of data with any camera, any DM command, any exposure time, etc.
+        Applies a flat map to both DM's, and the IrisAO if it is installed, for a long time (default
+        timeout = 10 minutes), or stop it sooner with the stop button.
         :param dm1_command_object: (DmCommand) DmCommand object to apply on DM1.
         :param dm1_command_object: (DmCommand) DmCommand object to apply on DM2.
-        :param timeout:  int. Maximum time in seconds to hold the pattern on the DMs.
+        :param iris_ao_command_object: (SegmentedDmCommand) SegmentedDmCommand to apply on IrisAO
+        :param timeout:  int. Maximum time in seconds to hold the pattern on the DMs. Default 600 sec = 10 min.
 
         """
-        super(ApplyFlatMap, self).__init__(output_path=output_path, suffix=suffix, **kwargs)
+        super().__init__(output_path=output_path, suffix=suffix, **kwargs)
         self.timeout = timeout
         self.dm1_command_object = dm1_command_object
         self.dm2_command_object = dm2_command_object
+        self.iris_ao_command_object = iris_ao_command_object
 
     def experiment(self):
-        with testbed.dm_controller() as dm:
+        with testbed.dm_controller() as dm, \
+             testbed.iris_ao() as iris_ao:
+
             dm.apply_shape_to_both(self.dm1_command_object, self.dm2_command_object)
-            self.log.info("Flat Map applied.")
-            self.log.info(" ** This will loop for up to {} seconds , maintaining the flat map. **".format(self.timeout))
+            iris_ao.apply_shape(self.iris_ao_command_object)
+
+            self.log.info("Flat Maps applied to both Bostons, and IrisAO if it is installed.")
+            self.log.info(f" ** This will loop for up to {self.timeout} seconds , maintaining the flat map. **")
             self.log.info(" ** You must cancel the script to terminate it sooner . ** ")
             self.log.info(" ** I.e. use square 'stop' button in PyCharm. Caution - be careful to single click, not double click it! ** ")
 
             for time_counter in range(self.timeout):
                 time.sleep(1)
 
-            self.log.info("Reached DM pattern hold timeout ({} seconds);  therefore ending script.".format(self.timeout))
+            self.log.info(f"Reached DM pattern hold timeout ({self.timeout} seconds);  therefore ending script.")
