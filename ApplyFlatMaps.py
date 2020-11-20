a22c7b52c77a4b5bfa788027e2479746f899243c
Iva Laginja <laginja.iva@gmail.com>
2020-11-20 17:54:22 +0100

    add parameters to toggle independently between flatmaps on Bostons and IrisAO

--- ApplyFlatMaps.py
+++ ApplyFlatMaps.py
@@ -12,6 +12,8 @@ class ApplyFlatMaps(Experiment):
     log = logging.getLogger(__name__)
 
     def __init__(self,
+                 control_boston_dms=True,
+                 control_iris_ao=True,
                  dm1_command_object=None,  # Will default to flat with bias.
                  dm2_command_object=None,  # Will default to flat with bias.
                  iris_ao_command_object=None,  # Will default to custom flat map.
@@ -23,6 +25,8 @@ class ApplyFlatMaps(Experiment):
         """
         Applies a flat map to both DM's, and the IrisAO if it is installed, for a long time (default
         timeout = 10 minutes), or stop it sooner with the stop button.
+        :param control_boston_dms: (bool) whether to apply shape to both Boston DMs or not, default True
+        :param control_iris_ao: (bool) whether to apply shape to IrisAO DM or not, default True
         :param dm1_command_object: (DmCommand) DmCommand object to apply on DM1.
         :param dm1_command_object: (DmCommand) DmCommand object to apply on DM2.
         :param iris_ao_command_object: (SegmentedDmCommand) SegmentedDmCommand to apply on IrisAO
@@ -30,6 +34,9 @@ class ApplyFlatMaps(Experiment):
 
         """
         super().__init__(output_path=output_path, suffix=suffix, **kwargs)
+        self.control_boston_dms = control_boston_dms
+        self.control_iris_ao = control_iris_ao
+
         self.timeout = timeout
         if dm1_command_object is None:
             self.dm1_command_object = flat_command(bias=False, flat_map=True)
@@ -42,8 +49,10 @@ class ApplyFlatMaps(Experiment):
         with testbed.dm_controller() as dm, \
              testbed.iris_ao() as iris_ao:
 
-            dm.apply_shape_to_both(self.dm1_command_object, self.dm2_command_object)
-            iris_ao.apply_shape(self.iris_ao_command_object)
+            if self.control_boston_dms:
+                dm.apply_shape_to_both(self.dm1_command_object, self.dm2_command_object)
+            if self.control_iris_ao:
+                iris_ao.apply_shape(self.iris_ao_command_object)
 
             self.log.info("Flat Maps applied to both Bostons, and IrisAO if it is installed.")
             self.log.info(f" ** This will loop for up to {self.timeout} seconds , maintaining the flat map. **")
