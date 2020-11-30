8e8cbf471b4becc747a1b7430e84692d5652f63e
James Noss <jnoss@stsci.edu>
2020-11-30 13:31:51 -0500

    Don't require backend lib path for emulator
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/emulators/tests/test_npoint_tiptilt.py
+++ catkit/emulators/tests/test_npoint_tiptilt.py
@@ -15,8 +15,7 @@ product_id = 24596
 Controller = functools.partial(SimNPointTipTiltController,
                                config_id="npoint_tiptilt_lc_400",
                                vendor_id=vendor_id,
-                               product_id=product_id,
-                               library_path=os.path.abspath(__file__))
+                               product_id=product_id)
 
 
 def test_init():
