d49c59e9335c65159408e121358c93d944fde4a3
James Noss <jnoss@stsci.edu>
2020-12-17 17:21:32 -0500

    Rename catkit/testbed/Experiment.py -> catkit/testbed/experiment.py
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/testbed/__init__.py
+++ catkit/testbed/__init__.py
@@ -1,3 +1,3 @@
-from catkit.testbed.Experiment import devices
-from catkit.testbed.Experiment import Experiment
-from catkit.testbed.Experiment import SafetyException
+from catkit.testbed.experiment import devices
+from catkit.testbed.experiment import Experiment
+from catkit.testbed.experiment import SafetyException
