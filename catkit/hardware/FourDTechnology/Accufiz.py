5e259243dab2b0645a5d1155575bbf4af42f139d
James Noss <jnoss@stsci.edu>
2021-01-20 16:44:46 -0500

    HICAT-710 Refactor 4D file paths
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/hardware/FourDTechnology/Accufiz.py
+++ catkit/hardware/FourDTechnology/Accufiz.py
@@ -15,82 +15,81 @@ from catkit.config import CONFIG_INI
 
 class Accufiz(FizeauInterferometer):
 
-    def initialize(self, mask="dm2_detector.mask", *args, **kwargs):
-        """Opens connection with interferometer and returns the camera manufacturer specific object."""
-        orig = time.time()
-        ip = CONFIG_INI.get(self.config_id, "ip")
-        timeout = CONFIG_INI.getint(self.config_id, "timeout")
+    instrument_lib = requests
 
+    def initialize(self, local_path, server_path, mask="dm2_detector.mask", post_save_sleep=1):
+        """
+
+        :param local_path: str, The local path accessible from Python.
+        :param server_path: str, The path accessible from the 4D server.
+        :param mask: str, ?
+        :param post_save_sleep: int, float, Seconds to sleep between saving and checking for success.
+        """
+        self.ip = CONFIG_INI.get(self.config_id, "ip")
+        self.timeout = CONFIG_INI.getint(self.config_id, "timeout")
+        self.html_prefix = f"http://{self.ip}/WebService4D/WebService4D.asmx"
+        self.local_path = local_path
+        self.server_path = server_path
+        self.mask = mask
+        self.post_save_sleep = post_save_sleep
+
+    def _open(self):
         # Set the 4D timeout.
-        set_timeout_string = "http://{}/WebService4D/WebService4D.asmx/SetTimeout?timeOut={}".format(ip, timeout)
-        requests.get(set_timeout_string)
+        self.set_timeout_string = f"{self.html_prefix}/SetTimeout?timeOut={self.timeout}"
+        self.instrument_lib.get(set_timeout_string)
 
         # Set the Mask. This mask has to be local to the 4D computer in this directory.
-        # filemask = os.path.join("c:\\4Sight_masks", mask)
+        # filemask = os.path.join("c:\\4Sight_masks", self.mask)
         # typeofmask = "Detector"
         # parammask = {"maskType": typeofmask, "fileName": filemask}
         # set_mask_string = "http://{}/WebService4D/WebService4D.asmx/SetMask".format(ip)
         # resmask = requests.post(set_mask_string, data=parammask)
 
-    def close(self):
+        return True  # We're "open".
+
+    def _close(self):
         """Close interferometer connection?"""
+        pass
 
     def take_measurement(self,
-                         path,
                          num_frames=2,
                          filename=None,
                          rotate=0,
                          fliplr=False,
                          exposure_set=""):
 
-        if path is None:
-           raise ValueError("Path must be defined.")
+        # Send request to take data.
+        measurement_resp = self.instrument_lib.post(f"{self.html_prefix}/AverageMeasure", data={"count": int(num_frames)})
+        if "success" not in measurement_resp.text:
+            raise RuntimeError(f"{self.config_id}: Failed to take data - {measurement_resp.text}.")
 
         if filename is None:
             filename = "4d_measurement"
 
-        """Takes exposures and should be able to save fits and simply return the image data."""
-        ip = CONFIG_INI.get(self.config_id, "ip")
-        parammeas = {"count": int(num_frames)}
+        server_file_path = os.path.join(self.server_path, filename)
+        local_file_path = os.path.join(self.local_path, filename)
 
-        try_counter = 0
-        tries = 10
-        while try_counter < tries:
-            measres = requests.post('http://{}/WebService4D/WebService4D.asmx/AverageMeasure'.format(ip), data=parammeas)
+        #  This line is here because when sent through webservice slashes tend
+        #  to disappear. If we sent in parameter a path with only one slash,
+        #  they disappear
+        server_file_path = server_file_path.replace('\\', '/')
+        server_file_path = server_file_path.replace('/', '\\\\')
 
-            pathfile = os.path.join(path, filename)
+        # Create dir.
+        local_path_root = os.path.dirname(local_file_path)
+        if not os.path.exists(local_path_root):
+            os.makedirs(local_path_root)
 
-            #  This line is here because when sent through webservice slashes tend
-            #  to disappear. If we sent in parameter a path with only one slash,
-            #  they disappear
-            pathfile = pathfile.replace('\\',
-                                        '/')
+        # Send request to save data.
+        _resp = self.instrument_lib.post(f"{self.html_prefix}/SaveMeasurement", data={"fileName": server_file_path})
+        time.sleep(self.post_save_sleep)
 
-            pathfile = pathfile.replace('/',
-                                        '\\\\')
+        if not glob(f"{local_file_path}.h5"):
+            raise RunTimeException(f"{self.config_id}: Failed to save measurement data to '{local_file_path}'.")
 
-            paramsave = {"fileName": pathfile}
+        self.log.info(f"{self.config_id}: Succeeded to save measurement data to '{local_file_path}'")
 
-            if 'success' in measres.text:
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                r = requests.post("http://{}/WebService4D/WebService4D.asmx/SaveMeasurement".format(ip), data=paramsave)
-                time.sleep(1)
-                if glob(pathfile + '.h5'):
-                    print('SUCCESS IN SAVING ' + pathfile)
-                    return self.__convert_h5_to_fits(path, pathfile, rotate, fliplr)
-                else:
-                    try_counter += 1
-                    print("FAIL IN SAVING MEASUREMENT " + pathfile + ".h5")
-                    if try_counter < tries:
-                        print("Trying again..")
-            else:
-                try_counter += 1
-                print("FAIL IN MEASUREMENT " + pathfile + ".h5")
-                print(measres.text)
-                if try_counter < tries:
-                    print("Trying again..")
-
+        return self.convert_h5_to_fits(local_file_path, rotate, fliplr)
 
     @staticmethod
     def __get_mask_path(mask):
@@ -101,17 +100,16 @@ class Accufiz(FizeauInterferometer):
         return os.path.join(calibration_data_path, mask)
 
     @staticmethod
-    def __convert_h5_to_fits(path, file, rotate, fliplr):
-        os.chdir(path)
+    def convert_h5_to_fits(filepath, rotate, fliplr, wavelength=632.8):
 
-        file = file if file.endswith(".h5") else file + ".h5"
-        pathfile = file
-        pathdifits = file[:-3] + '.fits'
+        filepath = filepath if filepath.endswith(".h5") else f"{filepath}.h5"
 
-        maskinh5 = np.array(h5py.File(pathfile, 'r').get('measurement0').get('Detectormask'))
-        image0 = np.array(h5py.File(pathfile, 'r').get('measurement0').get('genraw').get('data')) * maskinh5
+        fits_filepath = f"{os.path.splitext(filepath)[0]}.fits"
 
-        fits.PrimaryHDU(maskinh5).writeto(pathdifits, overwrite=True)
+        maskinh5 = np.array(h5py.File(filepath, 'r').get('measurement0').get('Detectormask'))
+        image0 = np.array(h5py.File(filepath, 'r').get('measurement0').get('genraw').get('data')) * maskinh5
+
+        fits.PrimaryHDU(maskinh5).writeto(fits_filepath, overwrite=True)
 
         radiusmask = np.int(np.sqrt(np.sum(maskinh5) / math.pi))
         center = ndimage.measurements.center_of_mass(maskinh5)
@@ -122,8 +120,8 @@ class Accufiz(FizeauInterferometer):
         # Apply the rotation and flips.
         image = catkit.util.rotate_and_flip_image(image, rotate, fliplr)
 
-        # Convert waves to nanometers (wavelength of 632.8).
-        image = image * 632.8
+        # Convert waves to nanometers.
+        image = image * wavelength
 
-        fits.PrimaryHDU(image).writeto(pathdifits, overwrite=True)
-        return pathdifits
+        fits.PrimaryHDU(image).writeto(fits_filepath, overwrite=True)
+        return fits_filepath
--- catkit/interfaces/FizeauInterferometer.py
+++ catkit/interfaces/FizeauInterferometer.py
@@ -1,34 +1,11 @@
-
 from abc import ABC, abstractmethod
+
+from catkit.interfaces.Instrument import Instrument
 
 """Abstract base class for all Fizeau Interferometers. Implementations of this class also become context managers."""
 
 
-class FizeauInterferometer(ABC):
-    def __init__(self, config_id, *args, **kwargs):
-        """Opens connection with camera sets class attributes for 'config_id'"""
-        self.config_id = config_id
-        self.interferometer = self.initialize(*args, **kwargs)
-        print("Opened connection to Fizeau Interferometer: " + self.config_id)
-
-    # Implementing context manager.
-    def __enter__(self, *args, **kwargs):
-        return self
-
-    def __exit__(self, exception_type, exception_value, exception_traceback):
-        self.close()
-        self.interferometer = None
-        print("Safely closed Fizeau Interferometer: " + self.config_id)
-
-    # Abstract Methods.
+class FizeauInterferometer(Instrument, ABC):
     @abstractmethod
-    def initialize(self, *args, **kwargs):
-        """Opens connection with interferometer and returns the camera manufacturer specific object."""
-
-    @abstractmethod
-    def close(self):
-        """Close interferometer connection."""
-
-    @abstractmethod
-    def take_measurement(self, num_frames, path, filename):
+    def take_measurement(self, num_frames, filename):
         """Takes exposures and should be able to save fits and simply return the image data."""
