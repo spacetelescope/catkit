e2daae594711366c6d996fc50fc74433b7a11511
James Noss <jnoss@stsci.edu>
2021-02-23 20:55:52 -0500

    CATKIT-80 Refactor MFF101 to use Instrument interface
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/hardware/thorlabs/ThorlabsMFF101.py
+++ catkit/hardware/thorlabs/ThorlabsMFF101.py
@@ -1,44 +1,55 @@
+from enum import Enum
+
 try:
     import ftd2xx
 except Exception as error:  # Raises OSError if it can't open driver lib
     raise ImportError("Missing libftd2xx driver? Download from https://www.ftdichip.com/Drivers/D2XX.htm") from error
-import ftd2xx.defines as constants
-import logging
 from catkit.interfaces.FlipMotor import FlipMotor
-from catkit.config import CONFIG_INI
 import catkit.util
 
 """Implementation of the FlipMotor interface for the Thorlabs MFF101 Flip Mount."""
 
 
 class ThorlabsMFF101(FlipMotor):
-    log = logging.getLogger(__name__)
 
-    def initialize(self, *args, **kwargs):
+    instrument_lib = ftd2xx
+
+    class Command(Enum):
+        MOVE_TO_POSITION_1 = b"\x6A\x04\x00\x01\x21\x01"
+        MOVE_TO_POSITION_2 = b"\x6A\x04\x00\x02\x21\x01"
+        BLINK_LED = b"\x23\x02\x00\x00\x21\x01"
+
+    def initialize(self, serial, in_beam_position):
         """Creates an instance of the controller library and opens a connection."""
 
-        serial = CONFIG_INI.get(self.config_id, "serial")
         self.serial = serial
+        self.in_beam_position = in_beam_position
 
-        # noinspection PyArgumentList
-        motor = ftd2xx.openEx(bytes(serial, 'utf-8'))
-        motor.setBaudRate(115200)
-        motor.setDataCharacteristics(constants.BITS_8, constants.STOP_BITS_1, constants.PARITY_NONE)
-        catkit.util.sleep(.05)
-        motor.purge()
-        catkit.util.sleep(.05)
-        motor.resetDevice()
-        motor.setFlowControl(constants.FLOW_RTS_CTS, 0, 0)
-        motor.setRts()
-        return motor
+    def _open(self):
+        # Open.
+        self.instrument = self.instrument_lib.openEx(self.serial.encode())
 
-    def close(self):
+        # Configure.
+        self.instrument.setBaudRate(115200)
+        self.instrument.setDataCharacteristics(self.instrument_lib.defines.BITS_8,
+                                               self.instrument_lib.defines.STOP_BITS_1,
+                                               self.instrument_lib.defines.PARITY_NONE)
+        catkit.util.sleep(.05)
+        self.instrument.purge()
+        catkit.util.sleep(.05)
+        self.instrument.resetDevice()
+        self.instrument.setFlowControl(self.instrument_lib.defines.FLOW_RTS_CTS, 0, 0)
+        self.instrument.setRts()
+
+        return self.instrument
+
+    def _close(self):
         """Close dm connection safely."""
-        self.motor.close()
+        self.instrument.close()
 
     def move_to_position(self, position_number):
-        """ Calls move_to_position<position_number>. """
-        #return self.__getattribute__(f"move_to_position{position_number}")
+        """ Calls move_to_position<position_number>(). """
+        # return self.__getattribute__(f"move_to_position{position_number}")
         if position_number == 1:
             return self.move_to_position1()
         elif position_number == 2:
@@ -47,23 +58,21 @@ class ThorlabsMFF101(FlipMotor):
             raise NotImplementedError
 
     def move_to_position1(self):
-        """Implements a move to the "up" position."""
-        is_in_beam = CONFIG_INI.getint(self.config_id, 'in_beam_position') == 1
+        """ Implements a move to the "up" position. """
+        is_in_beam = self.in_beam_position == 1
         inout_label = 'in' if is_in_beam else "out of"
         self.log.info(f"Moving to 'up' position, which is {inout_label} beam")
-        up_command = b"\x6A\x04\x00\x01\x21\x01"
-        self.motor.write(up_command)
+        self.instrument.write(self.Command.MOVE_TO_POSITION_1)
         catkit.util.sleep(1)
 
     def move_to_position2(self):
-        """Implements a move to "down" position """
-        is_in_beam = CONFIG_INI.getint(self.config_id, 'in_beam_position') == 2
+        """ Implements a move to "down" position. """
+        is_in_beam = self.in_beam_position == 2
         inout_label = 'in' if is_in_beam else "out of"
         self.log.info(f"Moving to 'down' position, which is {inout_label} beam")
-        down_command = b"\x6A\x04\x00\x02\x21\x01"
-        self.motor.write(down_command)
+        self.instrument.write(self.Command.MOVE_TO_POSITION_2)
         catkit.util.sleep(1)
 
     def blink_led(self):
         self.log.info(".blink.")
-        self.motor.write(b"\x23\x02\x00\x00\x21\x01")
+        self.instrument.write(self.Command.BLINK_LED)
