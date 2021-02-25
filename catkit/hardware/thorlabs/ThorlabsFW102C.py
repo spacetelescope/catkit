101dcfb30fda125f68713e9d1296caab5e0dc1d4
James Noss <jnoss@stsci.edu>
2021-02-25 14:07:49 -0500

    CATKIT-81 Refactor Thorlabs FW102C
    This was already using the instrument base interface.
    
    The commit caches the current filter wheel position.
    
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/hardware/thorlabs/ThorlabsFW102C.py
+++ catkit/hardware/thorlabs/ThorlabsFW102C.py
@@ -14,6 +14,8 @@ class ThorlabsFW102C(FilterWheel):
     def initialize(self, *args, **kwargs):
         """ Initializes class instance, but doesn't -- and shouldn't -- open a connection to the hardware."""
 
+        self.current_position = None
+
         # Determine the os, and load the correct filter ID from the ini file.
         if platform.system().lower() == "darwin":
             self.visa_id = CONFIG_INI.get(self.config_id, "mac_resource_name")
@@ -25,6 +27,8 @@ class ThorlabsFW102C(FilterWheel):
     def _open(self):
         """Open connection. Return an object connected to the instrument hardware.
         """
+        self.current_position = None
+
         rm = self.instrument_lib.ResourceManager('@py')
 
         # These values took a while to figure out; be careful changing them.
@@ -36,6 +40,7 @@ class ThorlabsFW102C(FilterWheel):
 
     def _close(self):
         self.instrument.close()
+        self.current_position = None
 
     def get_position(self):
         _bytes_written = self.instrument.write("pos?")
@@ -43,23 +48,34 @@ class ThorlabsFW102C(FilterWheel):
         if self.instrument.last_status is pyvisa.constants.StatusCode.success:
 
             # First read the echo to clear the buffer.
-            self.instrument.read()
+            try:
+                self.instrument.read()
+                # Now read the filter position, and convert to an integer.
+                self.current_position = int(self.instrument.read())
+            except Exception:
+                self.current_position = None
+                raise
 
-            # Now read the filter position, and convert to an integer.
-            return int(self.instrument.read())
+            return self.current_position
         else:
             raise Exception(f"Filter wheel '{self.config_id}' returned an unexpected response: '{self.instrument.last_status}'")
 
     def set_position(self, new_position):
         command = "pos=" + str(new_position)
-        _bytes_written = self.instrument.write(command)  # bytes_written := len(command) + 1 due to '\r'.
+        try:
+            _bytes_written = self.instrument.write(command)  # bytes_written := len(command) + 1 due to '\r'.
 
-        if self.instrument.last_status is pyvisa.constants.StatusCode.success:
-            self.instrument.read()
-            # Wait for wheel to move. Fairly arbitrary 3 s delay...
-            catkit.util.sleep(3)
-        else:
-            raise Exception(f"Filter wheel '{self.config_id}' returned an unexpected response: '{self.instrument.last_status}'")
+            if self.instrument.last_status is pyvisa.constants.StatusCode.success:
+                self.instrument.read()
+                self.current_position = new_position
+                # Wait for wheel to move. Fairly arbitrary 3 s delay...
+                catkit.util.sleep(3)
+            else:
+                raise Exception(f"Filter wheel '{self.config_id}' returned an unexpected response: '{self.instrument.last_status}'")
+        except Exception:
+            self.current_position = None
+            raise
+
 
     def ask(self, write_string):
         self.instrument.write(write_string)
