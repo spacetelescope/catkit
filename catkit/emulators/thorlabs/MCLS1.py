65ca1b6c139c33eb0f0661a543b76873110876e4
James Noss <jnoss@stsci.edu>
2021-03-04 15:31:50 -0500

    Further refac and added emulator
    Signed-off-by: James Noss <jnoss@stsci.edu>

---
+++ catkit/emulators/thorlabs/MCLS1.py
@@ -0,0 +1,79 @@
+from catkit.hardware.thorlabs.ThorlabsMCLS1 import ThorlabsMCLS1
+
+class MCSL1Emulator:
+    """ Emulates UART comms library specifically for the MCLS1. """
+
+    N_CHANNELS = 4  # The MCLS1 has only 4 channels.
+
+    Command = ThorlabsMCLS1.Command
+
+    def __init__(self, device_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instrument_handle = False
+        self.active_channel = None
+        self.system_enabled = False
+        self.channel_enabled = [False] * self.N_CHANNELS
+        self.current = [0] * self.N_CHANNELS
+        self.port = None
+        self.device_id = device_id
+
+    def fnUART_LIBRARY_open(self, port, *args, **kwargs):
+        self.instrument_handle = True
+        self.port = port
+        return self.instrument_handle
+
+    def fnUART_LIBRARY_isOpen(self, port, *args, **kwargs):
+        self.instrument_handle = True
+        return self.instrument_handle
+
+    def fnUART_LIBRARY_close(self, handle, *args, **kwargs):
+        self.instrument_handle = False
+
+    def fnUART_LIBRARY_Set(self, handle, command, size, *args, **kwargs):
+        if not self.instrument_handle:
+            raise RuntimeError("Connection closed")
+
+        command = command.decode()
+
+        if "=" not in command:
+            raise RuntimeError(f"Expected SET command ('=') but got '{command}'")
+
+        command, value = command.replace(self.Command.TERM_CHAR, '').split("=")
+        command += "="
+        command = self.Command(command)
+
+        if command is self.Command.SET_SYSTEM:
+            self.system_enabled = bool(value)
+        elif command is self.Command.SET_CHANNEL:
+            self.active_channel = int(value)
+        elif command is self.Command.SET_ENABLE:
+            self.channel_enabled[self.active_channel-1] = bool(value)
+        elif command is self.Command.SET_CURRENT:
+            self.current[self.active_channel-1] = float(value)
+        else:
+            raise NotImplementedError
+
+    def fnUART_LIBRARY_Get(self, command, buffer, *args, **kwargs):
+        if not self.instrument_handle:
+            raise RuntimeError("Connection closed")
+
+        command = command.decode().replace(self.Command.TERM_CHAR, '')
+
+        if "?" not in command:
+            raise RuntimeError(f"Expected GET command ('?') but got '{command}'")
+
+        command = self.Command(command)
+
+        if command is self.Command.GET_CURRENT:
+            resp = float(self.current[self.active_channel-1])
+        elif command is self.Command.GET_ENABLE:
+            resp = int(self.channel_enabled[self.active_channel-1])
+        elif command is self.Command.GET_CHANNEL:
+            resp = int(self.active_channel)
+        else:
+            raise NotImplementedError
+
+        return str(resp).encode()
+
+    def fnUART_LIBRARY_list(self, buffer, size, *args, **kwargs):
+        return f"0, {self.device_id}"  # Return port, ID.
--- catkit/hardware/thorlabs/ThorlabsMCLS1.py
+++ catkit/hardware/thorlabs/ThorlabsMCLS1.py
@@ -1,4 +1,5 @@
 from ctypes import cdll
+from enum import Enum
 import os
 import re
 
@@ -14,6 +15,30 @@ UART_lib = cdll.LoadLibrary(os.environ.get('CATKIT_THORLABS_UART_LIB_PATH'))
 class ThorlabsMCLS1(LaserSource):
 
     instrument_lib = UART_lib
+
+    class Command(Enum):
+        TERM_CHAR = "\r"
+        GET_CURRENT = "current?"
+        SET_CURRENT = "current="
+        GET_ENABLE = "enable?"
+        SET_ENABLE = "enable="
+        SET_SYSTEM = "system="
+        SET_CHANNEL = "channel="
+
+        # The following are untested.
+        GET_COMMANDS = "?"
+        GET_ID = "id?"
+        GET_CHANNEL = "channel?"
+        GET_TARGET_TEMP = "target?"
+        SET_TARGET_TEMP = "target="
+        GET_TEMP = "temp?"
+        GET_POWER = "power?"
+        GET_SYSTEM = "system?"
+        GET_SPECS = "specs?"
+        GET_STEP = "step?"
+        SET_STEP = "step="
+        SAVE = "save"
+        GET_STATUS = "statword"
 
     def initialize(self,
                    device_id,
@@ -39,8 +64,8 @@ class ThorlabsMCLS1(LaserSource):
 
         # Set the initial current to nominal_current and enable the laser.
         self.set_current(self.nominal_current, sleep=False)
-        self.set_channel_enable(self.channel, 1)
-        self.set_system_enable(1)
+        self.set_channel_enable(self.channel, True)
+        self.set_system_enable(True)
 
         return self.instrument
 
@@ -48,41 +73,63 @@ class ThorlabsMCLS1(LaserSource):
         """Close laser connection safely"""
         if self.power_off_on_exit:
             if self.instrument_lib.fnUART_LIBRARY_isOpen(self.port.encode()) == 1:
-                self.set_channel_enable(self.channel, 0)
+                self.set_channel_enable(self.channel, False)
 
-                self.log.info("Checking whether to power off laser...")
+                self.log.info("Checking whether other channels enable before powering off laser...")
                 # Check if the other channels are enabled before turning off system enable.
                 turn_off_system_enable = True
                 for i in range(1, 5):
-                    if self.is_channel_enabled(i) == 1:
+                    if self.is_channel_enabled(i):
                         turn_off_system_enable = False
+                        if i == self.channel:
+                            raise RuntimeError(f"Failed to disable channel: '{self.channel}")
+                        break
 
                 if turn_off_system_enable:
-                    self.set_system_enable(0)
+                    self.log.info("Powering off laser (system wide).")
+                    self.set_system_enable(False)
+                else:
+                    self.log.info("Other channels enabled, NOT powering off laser (system wide).")
                 self.instrument_lib.fnUART_LIBRARY_close(self.instrument)
         else:
             self.log.info("Power off on exit is False; leaving laser ON.")
 
-    def set_current(self, value, sleep=True):
-        """Sets the current on a given channel."""
+    def get(self, command, channel=None):
+        self.set_active_channel(channel=channel)
+        command = command + self.Command.TERM_CHAR
+        response_buffer = ("0" * 255)
+        self.instrument_lib.fnUART_LIBRARY_Get(self.instrument, command.encode(), response_buffer.encode())
+        return response_buffer.decode()
 
-        if self.get_current() != value:
+    def set(self, command, value, channel=None):
+        if isinstance(value, bool):
+            value = int(value)
+
+        if command not in (self.Command.SET_CHANNEL, self.Command.SET_SYSTEM):
+            self.set_active_channel(channel=channel)
+
+        # WARNING! The device may have multiple connections and thus a race exits between setting the channel and
+        # then commanding that channel. See HICAT-542.
+
+        command = f"{command}{value}{self.Command.TERM_CHAR}"
+        self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, command.encode(), 32)
+
+    def set_current(self, value, channel=None, sleep=True):
+        """Sets the current on a given channel."""
+        if self.get_current(channel=channel) != value:
             self.log.info("Laser is changing amplitude...")
-            self.set_active_channel(self.channel)
-            current_command_string = f"current={value}\r".encode()
-            self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, current_command_string, 32)
+            self.set(self.Command.SET_CURRENT, value, channel=channel)
             if sleep:
                 catkit.util.sleep(self.sleep_time)
 
-    def get_current(self):
-        """Returns the value of the laser's current."""
-        self.set_active_channel(self.channel)
-        command = "current?\r".encode()
-        response_buffer = ("0" * 255).encode()
-        self.instrument_lib.fnUART_LIBRARY_Get(self.instrument, command, response_buffer)
-        response_buffer = response_buffer.decode()
-        # Use regex to find the float value in the response.
-        return float(re.findall("\d+\.\d+", response_buffer)[0])
+    def get_current(self, channel=None):
+        """ Returns the value of the laser's current. """
+        return float(re.findall("\d+\.\d+", self.get(self.Command.GET_CURRENT, channel=channel))[0])
+
+    @property
+    def current(self):
+        # The laser can handle multiple connections and thus we can't mutex and store state locally so must query the device.
+        return self.get_current(channel=self.channel)
 
     def find_com_port(self):
         """Queries the dll for the list of all com devices."""
@@ -101,37 +148,24 @@ class ThorlabsMCLS1(LaserSource):
         raise IOError(f"{self.config_id}: no port found for '{self.device_id}'")
 
     def set_channel_enable(self, channel, value):
-        """
-        Set the laser's channel enable.
+        """ Set the laser's channel enable.
         :param channel: Integer value for channel (1 - 4)
-        :param value: Integer value, 1 is enabled, and 0 is disabled.
+        :param value: Integer, bool value, 1 is enabled, and 0 is disabled.
         """
-        self.set_active_channel(channel)
-        enable_command_string = f"enable={value}\r".encode()
         self.log.info(f"Laser is enabling channel '{channel}'...")
-        self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, enable_command_string, 32)
-        if value == 1:
+        self.set(self.Command.SET_ENABLE, value, channel=channel)
+        if value:
             catkit.util.sleep(self.sleep_time)
 
     def set_system_enable(self, value):
-        """
-        Set the laser's system enable.
-        :param value: Integer value, 1 is enabled, and 0 is disabled.
-        """
-        enable_command_string = f"system={value}\r".encode()
-        self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, enable_command_string, 32)
-        if value == 1:
+        """ Set the laser's system enable. """
+        self.set(self.Command.SET_SYSTEM, value)
+        if value:
             catkit.util.sleep(self.sleep_time)
 
-    def set_active_channel(self, channel):
-        # Set Active Channel.
-        active_command_string = f"channel={channel}\r".encode()
-        self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, active_command_string, 32)
+    def set_active_channel(self, channel=None):
+        channel = channel if channel else self.channel
+        self.set(self.Command.SET_CHANNEL, value=channel)
 
-    def is_channel_enabled(self, channel):
-        self.set_active_channel(channel)
-        command = "enable?\r".encode()
-        response_buffer = ("0" * 255).encode()
-        self.instrument_lib.fnUART_LIBRARY_Get(self.instrument, command, response_buffer)
-        response_buffer = response_buffer.decode()
-        return bool(int(re.findall("\d+", response_buffer)[0]))
+    def is_channel_enabled(self, channel=None):
+        return bool(int(re.findall("\d+", self.get(self.Command.GET_ENABLE, channel=channel))[0]))
