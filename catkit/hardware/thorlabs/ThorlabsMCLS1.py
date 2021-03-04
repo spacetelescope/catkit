68d81fb88add74738dd4ce854771d498ad959344
James Noss <jnoss@stsci.edu>
2021-03-04 11:36:03 -0500

    CATKIT-84 Refactor Thorlabs MCLS1 (laser)
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/hardware/thorlabs/ThorlabsMCLS1.py
+++ catkit/hardware/thorlabs/ThorlabsMCLS1.py
@@ -1,61 +1,56 @@
 from ctypes import cdll
-import logging
 import os
 import re
 
 from catkit.interfaces.LaserSource import LaserSource
-from catkit.config import CONFIG_INI
 import catkit.util
 
 """Interface for a laser source."""
 
+# Load Thorlabs uart lib, e.g., uart_library_win64.dll.
+UART_lib = cdll.LoadLibrary(os.environ.get('CATKIT_THORLABS_UART_LIB_PATH'))
+
 
 class ThorlabsMCLS1(LaserSource):
-    SLEEP_TIME = 2  # Number of seconds to sleep after turning on laser or changing current.
-    log = logging.getLogger(__name__)
 
-    def __init__(self, config_id, device_id=None, power_off_on_exit=False, *args, **kwargs):
-        """
-        Child constructor to add a few hardware specific class attributes. Still calls the super.
-        """
-        self.channel = None
-        self.nominal_current = None
-        self.handle = None
+    instrument_lib = UART_lib
+
+    def initialize(self,
+                   device_id,
+                   channel,
+                   nominal_current,
+                   power_off_on_exit=False,
+                   sleep_time=2):
+
+        self.channel = channel #CONFIG_INI.getint(self.config_id, "channel")
+        self.nominal_current = nominal_current#CONFIG_INI.getint(self.config_id, "nominal_current")
+        self.sleep_time = sleep_time  # Number of seconds to sleep after turning on laser or changing current.
         self.port = None
         self.power_off_on_exit = power_off_on_exit
-        self.device_id = device_id if device_id else CONFIG_INI.get(config_id, "device_id")
-        self._library_path = None
-        super(ThorlabsMCLS1, self).__init__(config_id, *args, **kwargs)
+        self.device_id = device_id# if device_id else CONFIG_INI.get(config_id, "device_id")
 
-
-    def initialize(self, *args, library_path=None, **kwargs):
-        """Starts laser at the nominal_current value from config.ini."""
-        self.channel = CONFIG_INI.getint(self.config_id, "channel")
-        self.nominal_current = CONFIG_INI.getint(self.config_id, "nominal_current")
-
-        # Load Thorlabs uart lib, e.g., uart_library_win64.dll.
-        self._library_path = os.environ.get('CATKIT_THORLABS_UART_LIB_PATH') if library_path is None else library_path
-        # noinspection PyArgumentList
-        self.laser = cdll.LoadLibrary(self._library_path)
+    def _open(self):
         self.port = self.find_com_port()
-        self.handle = self.laser.fnUART_LIBRARY_open(self.port.encode(), 115200, 3)
-        if self.handle < 0:
-            raise IOError("{} connection failure on port: '{}'".format(self.config_id, self.port))
+        # Open connection (handle).
+        instrument = self.instrument_lib.fnUART_LIBRARY_open(self.port.encode(), 115200, 3)
+        if instrument < 0:
+            raise IOError(f"{self.config_id} connection failure on port: '{self.port}'")
+        self.instrument = instrument
 
         # Set the initial current to nominal_current and enable the laser.
         self.set_current(self.nominal_current, sleep=False)
         self.set_channel_enable(self.channel, 1)
         self.set_system_enable(1)
 
-        return self.laser
+        return self.instrument
 
-    def close(self):
+    def _close(self):
         """Close laser connection safely"""
         if self.power_off_on_exit:
-            if self.laser.fnUART_LIBRARY_isOpen(self.port.encode()) == 1:
+            if self.instrument_lib.fnUART_LIBRARY_isOpen(self.port.encode()) == 1:
                 self.set_channel_enable(self.channel, 0)
 
-                print("Checking to power off laser")
+                self.log.info("Checking whether to power off laser...")
                 # Check if the other channels are enabled before turning off system enable.
                 turn_off_system_enable = True
                 for i in range(1, 5):
@@ -64,11 +59,9 @@ class ThorlabsMCLS1(LaserSource):
 
                 if turn_off_system_enable:
                     self.set_system_enable(0)
-                self.laser.fnUART_LIBRARY_close(self.handle)
+                self.instrument_lib.fnUART_LIBRARY_close(self.instrument)
         else:
-            print("Power off on exit is False; leaving laser ON.")
-
-        self.handle = None
+            self.log.info("Power off on exit is False; leaving laser ON.")
 
     def set_current(self, value, sleep=True):
         """Sets the current on a given channel."""
@@ -76,17 +69,17 @@ class ThorlabsMCLS1(LaserSource):
         if self.get_current() != value:
             self.log.info("Laser is changing amplitude...")
             self.set_active_channel(self.channel)
-            current_command_string = "current={}\r".format(value).encode()
-            self.laser.fnUART_LIBRARY_Set(self.handle, current_command_string, 32)
+            current_command_string = f"current={value}\r".encode()
+            self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, current_command_string, 32)
             if sleep:
-                catkit.util.sleep(self.SLEEP_TIME)
+                catkit.util.sleep(self.sleep_time)
 
     def get_current(self):
         """Returns the value of the laser's current."""
         self.set_active_channel(self.channel)
         command = "current?\r".encode()
         response_buffer = ("0" * 255).encode()
-        self.laser.fnUART_LIBRARY_Get(self.handle, command, response_buffer)
+        self.instrument_lib.fnUART_LIBRARY_Get(self.instrument, command, response_buffer)
         response_buffer = response_buffer.decode()
         # Use regex to find the float value in the response.
         return float(re.findall("\d+\.\d+", response_buffer)[0])
@@ -94,19 +87,18 @@ class ThorlabsMCLS1(LaserSource):
     def find_com_port(self):
         """Queries the dll for the list of all com devices."""
         if not self.device_id:
-            raise ValueError("{}: requires a device ID to find a com port to connect to.".format(self.config_id))
+            raise ValueError(f"{self.config_id}: requires a device ID to find a com port to connect to.")
 
         response_buffer = ("0" * 255).encode()
-        self.laser.fnUART_LIBRARY_list(response_buffer, 255)
+        self.instrument_lib.fnUART_LIBRARY_list(response_buffer, 255)
         response_buffer = response_buffer.decode()
         split = response_buffer.split(",")
         for i, thing in enumerate(split):
-
             # The list has a format of "Port, Device, Port, Device". Once we find device named VCPO, minus 1 for port.
             if thing == self.device_id:
                 return split[i - 1]
 
-        raise IOError("{}: no port found for '{}'".format(self.config_id, self.device_id))
+        raise IOError(f"{self.config_id}: no port found for '{self.device_id}'")
 
     def set_channel_enable(self, channel, value):
         """
@@ -115,31 +107,31 @@ class ThorlabsMCLS1(LaserSource):
         :param value: Integer value, 1 is enabled, and 0 is disabled.
         """
         self.set_active_channel(channel)
-        enable_command_string = "enable={}\r".format(value).encode()
-        self.laser.fnUART_LIBRARY_Set(self.handle, enable_command_string, 32)
+        enable_command_string = f"enable={value}\r".encode()
+        self.log.info(f"Laser is enabling channel '{channel}'...")
+        self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, enable_command_string, 32)
         if value == 1:
-            self.log.info("Laser is enabling channel " + str(channel) + "...")
-            catkit.util.sleep(self.SLEEP_TIME)
+            catkit.util.sleep(self.sleep_time)
 
     def set_system_enable(self, value):
         """
         Set the laser's system enable.
         :param value: Integer value, 1 is enabled, and 0 is disabled.
         """
-        enable_command_string = "system={}\r".format(value).encode()
-        self.laser.fnUART_LIBRARY_Set(self.handle, enable_command_string, 32)
+        enable_command_string = f"system={value}\r".encode()
+        self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, enable_command_string, 32)
         if value == 1:
-            catkit.util.sleep(self.SLEEP_TIME)
+            catkit.util.sleep(self.sleep_time)
 
     def set_active_channel(self, channel):
         # Set Active Channel.
-        active_command_string = "channel={}\r".format(channel).encode()
-        self.laser.fnUART_LIBRARY_Set(self.handle, active_command_string, 32)
+        active_command_string = f"channel={channel}\r".encode()
+        self.instrument_lib.fnUART_LIBRARY_Set(self.instrument, active_command_string, 32)
 
     def is_channel_enabled(self, channel):
         self.set_active_channel(channel)
         command = "enable?\r".encode()
         response_buffer = ("0" * 255).encode()
-        self.laser.fnUART_LIBRARY_Get(self.handle, command, response_buffer)
+        self.instrument_lib.fnUART_LIBRARY_Get(self.instrument, command, response_buffer)
         response_buffer = response_buffer.decode()
         return bool(int(re.findall("\d+", response_buffer)[0]))
--- catkit/interfaces/LaserSource.py
+++ catkit/interfaces/LaserSource.py
@@ -1,50 +1,11 @@
-import logging
-from abc import ABC, abstractmethod
+from abc import abstractmethod
+
+from catkit.interfaces.Instrument import Instrument
 
 """Interface for a laser source."""
 
 
-class LaserSource(ABC):
-    log = logging.getLogger()
-
-    def __init__(self, config_id, *args, **kwargs):
-        """Opens connection with the laser source and sets class attributes for 'config_id'"""
-        self.config_id = config_id
-        self._keep_alive = False
-        self.instrument = None
-
-        # Connect.
-        self.laser = self.initialize(*args, **kwargs)
-        self.instrument = self.laser
-        self.log.info("Opened connection to laser source " + config_id)
-
-    # Implementing context manager.
-    def __enter__(self, *args, **kwargs):
-        return self
-
-    def __exit__(self, exception_type, exception_value, exception_traceback):
-        try:
-            if not self._keep_alive:
-                try:
-                    if self.instrument:
-                        self.close()
-                finally:
-                    self.laser = None
-                    self.instrument = None
-                self.log.info("Safely closed connection to laser source " + self.config_id)
-        finally:
-            # Reset, single use basis only.
-            self._keep_alive = False
-
-    # Abstract Methods.
-    @abstractmethod
-    def initialize(self, *args, **kwargs):
-        """Creates an instance of the laser source dll and sets default current."""
-
-    @abstractmethod
-    def close(self):
-        """Close laser source connection safely."""
-
+class LaserSource(Instrument):
     @abstractmethod
     def set_current(self, value):
         """Sets the current"""
