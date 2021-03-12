42c319fcdbb1318029e09aeb507e6613c9ee5cf1
James Noss <jnoss@stsci.edu>
2021-03-11 20:30:02 -0500

    Release v0.32.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='catkit',
-      version='0.28.2',
+      version='0.32.0',
       description='Python API wrapping hardware and device drivers for the Optics Lab.',
       url='https://github.com/spacetelescope/catkit',
       author='Makidon Optics Lab',
