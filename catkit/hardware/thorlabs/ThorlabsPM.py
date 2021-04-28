a2f426f547e8ee51015652998bd47472b4422375
Emiel Por <epor@stsci.edu>
2021-04-28 00:08:40 -0400

    Autoload the library upon first use, rather than on import.

--- catkit/hardware/thorlabs/ThorlabsPM.py
+++ catkit/hardware/thorlabs/ThorlabsPM.py
@@ -7,30 +7,49 @@ from catkit.config import CONFIG_INI
 from catkit.interfaces.PowerMeter import PowerMeter
 import catkit.util
 
-# Find TLPM_64.dll - the path to which needs to be added to PYTHONPATH.
-# E.g. C:/Program Files/IVI Foundation/VISA/Win64/Bin/TLPM_64.dll.
-TLPM_REQUIRED_LIB = "TLPM_64.dll"  # Yep, this is Windows specific.
-TLPM_LIB_PATH = None
 
-for path in sys.path:
-    TLPM_LIB_PATH = glob.glob(os.path.join(path, TLPM_REQUIRED_LIB))
+class ThorlabsPMInstrumentMeta(type):
+    # Forward any call to a function to the PM library. Autoload the library upon first call.
+    def __getattr__(cls, name):
+        lib = cls.load_library()
 
-    if TLPM_LIB_PATH:
-        break
-
-if not TLPM_LIB_PATH:
-    raise ImportError("TLPM: Failed to locate '{}' - add path to PYTHONPATH".format(TLPM_REQUIRED_LIB))
-
-# Now load the found library.
-try:
-    TLPM_LIB = ctypes.cdll.LoadLibrary(TLPM_LIB_PATH[0])
-except Exception as error:
-    TLPM_LIB = None
-    raise ImportError("TLPM: Failed to import '{}' library @ '{}'".format(TLPM_REQUIRED_LIB, TLPM_LIB_PATH)) from error
+        return getattr(lib, name)
 
 
-class TLPM(PowerMeter):
-    instrument_lib = TLPM_LIB
+class ThorlabsPMInstrument(metaclass=ThorlabsPMInstrumentMeta):
+    _library = None
+
+    @classmethod
+    def load_library(cls):
+        if cls._library is not None:
+            return cls._library
+
+        # Find TLPM_64.dll - the path to which needs to be added to PYTHONPATH.
+        # E.g. C:/Program Files/IVI Foundation/VISA/Win64/Bin/TLPM_64.dll.
+        library_name = "TLPM_64.dll"  # Yep, this is Windows specific.
+        library_path = None
+
+        for path in sys.path:
+            library_path = glob.glob(os.path.join(path, library_name))
+
+            if library_path:
+                break
+
+        if not library_path:
+            raise ImportError("TLPM: Failed to locate '{}' - add path to PYTHONPATH".format(library_name))
+
+        # Now load the found library.
+        try:
+            cls._library = ctypes.cdll.LoadLibrary(library_path[0])
+        except Exception as error:
+            cls._library = None
+            raise ImportError("TLPM: Failed to import '{}' library @ '{}'".format(library_name, library_path)) from error
+
+        return cls._library
+
+
+class ThorlabsPM(PowerMeter):
+    instrument_lib = ThorlabsPMInstrument
 
     _BUFFER_SIZE = 1024
 
@@ -53,21 +72,21 @@ class TLPM(PowerMeter):
         self.device_name = device_names[0]
 
         # int TLPM_init(char *resourceName, bool IDQuery, bool resetDevice, void **vi)
-        status = self.instrument_lib.TLPM_init(self.device_name.encode(), True, True, ctypes.byref(self.instrument))
+        # use ctypes.pointer() to allow the emulator to modify this parameter
+        status = self.instrument_lib.TLPM_init(self.device_name.encode(), True, True, ctypes.pointer(self.instrument))
         if status or self.instrument.value is None:
             raise OSError("TLPM: Failed to connect - '{}'".format(self.get_error_message(status)))
 
         return self.instrument
 
-    @classmethod
-    def get_error_message(cls, status_code):
+    def get_error_message(self, status_code):
         """Convert error status to error message."""
-        error_message = ctypes.create_string_buffer(cls._BUFFER_SIZE)
+        error_message = ctypes.create_string_buffer(self._BUFFER_SIZE)
 
         # int TLPM_errorMessage(void *vi, int statusCode, char description[])
-        status = cls.instrument_lib.TLPM_errorMessage(None, status_code, error_message)
+        status = self.instrument_lib.TLPM_errorMessage(None, status_code, error_message)
         if status:
-            raise OSError("TLPM: Ironically failed to get error message - '{}'".format(cls.get_error_message(status)))
+            raise OSError("TLPM: Ironically failed to get error message - '{}'".format(self.get_error_message(status)))
 
         return error_message.value.decode()
 
@@ -75,32 +94,29 @@ class TLPM(PowerMeter):
     def create(cls, config_id):
         return cls(config_id=config_id, serial_number=CONFIG_INI.get(config_id, "serial_number"))
 
-    @classmethod
-    def find_all(cls):
+    def find_all(self):
         """Find all connected PM devices."""
-
         # First find the total number of connected PM devices.
         device_count = ctypes.c_int(0)
 
         # int TLPM_findRsrc(void *vi, int *resourceCount)
-        status = cls.instrument_lib.TLPM_findRsrc(None, ctypes.byref(device_count))
+        # use ctypes.pointer() to allow the emulator to modify this parameter
+        status = self.instrument_lib.TLPM_findRsrc(None, ctypes.pointer(device_count))
         if status:
-            raise ImportError("TLPM: Failed when trying to find connected devices - '{}'".format(cls.get_error_message(status)))
+            raise ImportError("TLPM: Failed when trying to find connected devices - '{}'".format(self.get_error_message(status)))
 
         # Then get their resource names.
         available_devices = []
 
         for i in range(device_count.value):
-            resource_name = ctypes.create_string_buffer(cls._BUFFER_SIZE)
+            resource_name = ctypes.create_string_buffer(self._BUFFER_SIZE)
 
             # int TLPM_getRsrcName(void *vi, int device_index, char resourceName[])
-            status = cls.instrument_lib.TLPM_getRsrcName(None, i, resource_name)
+            status = self.instrument_lib.TLPM_getRsrcName(None, i, resource_name)
             if status:
-                raise ImportError("TLPM: Failed when trying to find connected devices - '{}'".format(cls.get_error_message(status)))
+                raise ImportError("TLPM: Failed when trying to find connected devices - '{}'".format(self.get_error_message(status)))
 
             available_devices.append(resource_name.value.decode())
-
-        print(available_devices)
 
         return available_devices
 
@@ -117,7 +133,8 @@ class TLPM(PowerMeter):
         power = ctypes.c_double(0)
 
         # int TLPM_measPower(void *vi, double *power);
-        status = self.instrument_lib.TLPM_measPower(self.instrument, ctypes.byref(power))
+        # use ctypes.pointer() to allow the emulator to modify this parameter
+        status = self.instrument_lib.TLPM_measPower(self.instrument, ctypes.pointer(power))
         if status:
             raise RuntimeError("TLPM: Failed to get power - '{}'".format(self.get_error_message(status)))
 
