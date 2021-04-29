43cd4131266a9d63738db690f5ef192a7a877074
Emiel Por <epor@stsci.edu>
2021-04-29 12:18:02 -0400

    Add a stream_exposures() function to the camera interface.

--- catkit/interfaces/Camera.py
+++ catkit/interfaces/Camera.py
@@ -7,5 +7,9 @@ from catkit.interfaces.Instrument import Instrument
 class Camera(Instrument, ABC):
 
     @abstractmethod
-    def take_exposures(self, exposure_length, num_exposures, path=None, filename=None, *args, **kwargs):
+    def take_exposures(self, exposure_time, num_exposures, path=None, filename=None, *args, **kwargs):
         """Takes exposures and should be able to save fits and simply return the image data."""
+
+    @abstractmethod
+    def stream_exposures(self, exposure_time, num_exposures, *args, **kwargs):
+        """ Take a stream of exposures and yield individual images (ie. a generator)."""
