d0f3323fbd5d6880a55acee0608f1462fcb2b8d4
James Noss <jnoss@stsci.edu>
2022-03-07 19:38:18 -0500

    Fixes and name changes
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- catkit/hardware/starlight_xpress/filter_wheel.py
+++ catkit/hardware/starlight_xpress/filter_wheel.py
@@ -6,8 +6,8 @@ import usb
 
 
 class Report(enum.Enum):
-    GET_FILTER = bytearray([0, 0])
-    SET_FILTER = bytearray([0, 0])
+    GET_FILTER = (0, 0)
+    SET_FILTER = (1, 0)  # NOTE: 1 is a placeholder for the new filter position.
 
 
 class StandardUSBFilterWheel(FilterWheel):
@@ -15,14 +15,14 @@ class StandardUSBFilterWheel(FilterWheel):
     instrument_lib = usb
     ENDPOINT = 0x81
     N_BYTES = 2
-    MIN_FILTER = 1
+    MIN_POSITION = 1
 
     def initialize(self, idVendor=0x1278, idProduct=0x0920, timeout=8*1000):
         self.idVendor = idVendor
         self.idProduct = idProduct
         self.timeout = timeout
 
-        self.max_filters = None
+        self.max_position = None
 
     def _open(self):
         self.instrument = usb.core.find(idVendor=self.idVendor, idProduct=self.idProduct)
@@ -32,7 +32,7 @@ class StandardUSBFilterWheel(FilterWheel):
 
         self.instrument.set_configuration()
 
-        self.max_filters = self.get_max_filters()
+        self.max_position = self.get_max_filters()
 
         return self.instrument
 
@@ -45,38 +45,46 @@ class StandardUSBFilterWheel(FilterWheel):
         self.set_position(value)
 
     def _close(self):
+        """ Nothing to do. """
         ...
 
     def get_max_filters(self):
-        self.hid_set_report(Report.GET_FILTER.value)
-        return self.read()[1]
+        self._hid_set_report(Report.GET_FILTER)
+        return self._read()[1]
 
     def get_position(self):
-        filter_number = 0
-        while filter_number == 0:  # 0 := in motion.
-            self.hid_set_report(Report.GET_FILTER)
-            filter_number = self.read()[0]
+        filter_position = 0
+        while filter_position == 0:  # 0 := in motion.
+            self._hid_set_report(Report.GET_FILTER)
+            filter_number = self._read()[0]
+            time.sleep(1)
         return filter_number
 
     def set_position(self, new_position):
-        if self.MIN_FILTER < new_position > self.max_filters:
-            raise ValueError(f"Filter number must be in the range {self.MIN_FILTER}-{self.max_filters}")
+        if self.MIN_POSITION < new_position > self.max_position:
+            raise ValueError(f"Filter number must be in the range {self.MIN_POSITION}-{self.max_position}")
 
-        filter_number = 0
-        while filter_number == 0:  # 0 := in motion.
+        filter_position = 0
+        while filter_position == 0:  # 0 := in motion.
             report = Report.SET_FILTER.value
             report[0] = new_position
-            self.hid_set_report(report)
-            filter_number = self.read()[0]
-        return filter_number
+            self._hid_set_report(report)
+            filter_position = self._read()[0]
+            time.sleep(1)
 
-    def hid_set_report(self, report):
+        if filter_position != new_position:
+            raise RuntimeError(f"Failed to move to position: {new_position} and remains at position: {filter_position}.")
+
+    def _hid_set_report(self, report):
+        if isinstance(report, Report):
+            report = report.value
+
         self.instrument.ctrl_transfer(0x21,    # REQUEST_TYPE_CLASS | RECIPIENT_INTERFACE | ENDPOINT_OUT
                                       9,       # SET_REPORT
                                       0x200,   # "Vendor" Descriptor Type + 0 Descriptor Index
                                       0,       # USB interface.
-                                      report)  # the HID payload as a byte array
+                                      bytearray(report))  # the HID payload as a byte array
 
-    def read(self):
+    def _read(self):
         resp = self.instrument.read(self.ENDPOINT, self.N_BYTES, self.timeout)
         return resp[0], resp[1]
